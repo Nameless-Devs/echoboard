4d78345f2ab3db77edce641504ce4f11cda1431c
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-04 17:07:58 +0200

    removed unused import

--- src/main/java/echoboard/echoboard/Controller.java
+++ src/main/java/echoboard/echoboard/Controller.java
@@ -1,7 +1,6 @@
 package echoboard.echoboard;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController

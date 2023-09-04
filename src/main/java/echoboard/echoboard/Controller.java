e04864cf0eb2004919cd12f7a108ea8da1dd5f4c
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-04 17:02:17 +0200

    Added Status endpoint and removed test endpoint

--- src/main/java/echoboard/echoboard/Controller.java
+++ src/main/java/echoboard/echoboard/Controller.java
@@ -1,13 +1,15 @@
 package echoboard.echoboard;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/test")
-    public String returnHi(){
-        return "hi";
+    @GetMapping("/status")
+    public String getStatus() {
+        return "Server is up and running!";
     }
+
 }

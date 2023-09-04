e6b5a434395cc46ae34d99a0d8756e3205b98580
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-04 17:01:26 +0200

    added test that will fail if someone messes with the constructor

--- src/test/java/echoboard/echoboard/DiscussionTest.java
+++ src/test/java/echoboard/echoboard/DiscussionTest.java
@@ -1,4 +1,29 @@
+package echoboard.echoboard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DiscussionTest {
-  
+
+    private Discussion discussion;
+
+    @BeforeEach
+    void setUp() {
+        discussion = new Discussion("Test title", "Test content", "Author Name");
+    }
+
+    @Test
+    void testCreateDiscussion() {
+        assertNotNull(discussion);
+        assertEquals("Test title", discussion.getTitle());
+        assertEquals("Test content", discussion.getContent());
+        assertEquals("Author Name", discussion.getAuthor());
+        assertEquals(0, discussion.getUpvotes());
+        assertEquals(0, discussion.getDownvotes());
+    }
+
 }

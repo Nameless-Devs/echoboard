43167c783750ba6ef6ad1f80b7badfe0b94ab7bd
Vanessa Wingårdh <vwingardh@gmail.com>
2023-09-04 09:55:45 +0200

    Committing initial files

---
+++ src/main/java/echoboard/echoboard/EchoboardApplication.java
@@ -0,0 +1,13 @@
+package echoboard.echoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/echoboard/echoboard/EchoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package echoboard.echoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

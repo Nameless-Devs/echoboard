fa3bc6f9f3d92f46f48459e3371e005c7f3f2b3d
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-04 16:59:44 +0200

    created basic Dicussion class

---
+++ src/main/java/echoboard/echoboard/Discussion.java
@@ -0,0 +1,61 @@
+package echoboard.echoboard;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+public class Discussion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String author;
+    private int upvotes;
+    private int downvotes;
+    private LocalDateTime created;
+
+    public Discussion(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.upvotes = 0;
+        this.downvotes = 0;
+    }
+
+    protected Discussion() {
+    }
+
+    @PrePersist
+    private void onCreate() {
+        this.created = LocalDateTime.now();
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setUpvotes(int upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    public void setDownvotes(int downvotes) {
+        this.downvotes = downvotes;
+    }
+
+}
+

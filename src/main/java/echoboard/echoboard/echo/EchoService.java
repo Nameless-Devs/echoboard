b9b59ea69ba17054d8946bdb104811318b235e87
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-05 09:56:15 +0200

    optimized imports2

--- src/main/java/echoboard/echoboard/echo/EchoService.java
+++ src/main/java/echoboard/echoboard/echo/EchoService.java
@@ -1,7 +1,10 @@
-package echoboard.echoboard;
+package echoboard.echoboard.echo;
 
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -13,12 +16,17 @@ public class EchoService {
         this.echoRepository = echoRepository;
     }
 
-    public Iterable<Echo> getAllEchoes() {
-        return echoRepository.getAllEchoes();
+    public ArrayList<Echo> getAllEchoes() {
+        ArrayList<Echo> allEchoes = new ArrayList<>();
+        echoRepository.getAllEchoes().forEach(allEchoes::add);
+        return allEchoes;
     }
 
-    public void saveEcho(Echo echo) {
-        echoRepository.save(echo);
+    public Echo saveEcho(Echo echo) {
+        return echoRepository.save(echo);
     }
 
+    public Optional<Echo> getEchoById(long id) {
+        return echoRepository.getEchoById(id);
+    }
 }

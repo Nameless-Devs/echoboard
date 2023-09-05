41de72c7af98164ab5223470347961cb35203594
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-05 09:59:18 +0200

    optimized imports

--- src/test/java/echoboard/echoboard/EchoboardApplicationTests.java
+++ src/test/java/echoboard/echoboard/EchoboardApplicationTests.java
@@ -1,13 +1,57 @@
 package echoboard.echoboard;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import echoboard.echoboard.echo.Echo;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class EchoboardApplicationTests {
+@AutoConfigureMockMvc
+public class EchoboardApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private final MockMvc mockMvc;
+	private final ObjectMapper objectMapper;
+
+	@Autowired
+	public EchoboardApplicationTests(MockMvc mockMvc, ObjectMapper objectMapper) {
+		this.mockMvc = mockMvc;
+		this.objectMapper = objectMapper;
 	}
 
+	@Test
+	@Order(1)
+	public void testSaveEcho() throws Exception {
+		Echo echo = new Echo("Test Title", "Test Content", "Test Author");
+
+		String jsonRequest = objectMapper.writeValueAsString(echo);
+		System.out.println(jsonRequest);
+		mockMvc.perform(post("/echoes")
+						.contentType("application/json")
+						.content(jsonRequest))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	@Order(2)
+	void testGetEcho() throws Exception {
+
+		mockMvc.perform(get("/echoes"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].title").value("Test Title"))
+				.andExpect(jsonPath("$[0].content").value("Test Content"))
+				.andExpect(jsonPath("$[0].author").value("Test Author"))
+				.andExpect(jsonPath("$[0].upvotes").value(0))
+				.andExpect(jsonPath("$[0].downvotes").value(0))
+				.andExpect(jsonPath("$[0].id").value(1))
+				.andExpect(jsonPath("$[0].created").isNotEmpty())
+		;
+
+	}
 }

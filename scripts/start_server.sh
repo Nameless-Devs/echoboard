a6173b58aa0406ffbc5c824ac7fea96c8dcb7dd8
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-06 15:24:16 +0200

    now copies secrets before running mvn package

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,17 @@
+SOURCE_FILE="/home/ec2-user/env/secrets.yml"
+
+echo "Attempting to Start server..."
 cd /opt/codedeploy-agent/deployment-root/24553424-6f4c-4566-940a-5dcf5b9ca470/ || exit
+echo "Attempting find the latest directory..."
 cd "$(ls -td */ | head -n 1)" || exit
 cd deployment-archive/ || exit
+sudo mkdir "target"
+echo "Attempting to copy secrets..."
+cd target || exit
+cp $SOURCE_FILE ./
+cd ..
+echo "Attempting to create the package..."
 sudo mvn clean package
 cd target || exit
+echo "Staring the java file..."
 nohup java -jar echoboard-0.0.1-SNAPSHOT.jar > app.log 2>&1 &

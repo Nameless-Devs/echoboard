2e0d1424a803c89b9ac039c207b34b55b2edf91d
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-06 11:41:34 +0200

    added error handling for task not existing and logs

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,2 @@
-sudo pkill -f echoboard-0.0.1-SNAPSHOT.jar
+echo "Attempting to stop echoboard..."
+sudo pkill -f echoboard-0.0.1-SNAPSHOT.jar || { echo "Process not found or other error encountered"; true; }

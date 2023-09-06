92af38e824cf2c1bee208af15da680c48ee193e3
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-06 11:15:15 +0200

    added everything for cicd aws

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+sudo yum install -y java-17-amazon-corretto
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
---
+++ scripts/stop_server.sh
@@ -0,0 +1,1 @@
+pkill -f echoboard-0.0.1-SNAPSHOT.jar

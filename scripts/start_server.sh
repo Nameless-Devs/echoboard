68f8efb807ce67f6e75969b73b7a85152ff61172
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-06 12:09:27 +0200

    removed extra cd

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 cd /opt/codedeploy-agent/deployment-root/24553424-6f4c-4566-940a-5dcf5b9ca470/ || exit
-cd cd "$(ls -td */ | head -n 1)" || exit
+cd "$(ls -td */ | head -n 1)" || exit
 cd deployment-archive/ || exit
 sudo mvn clean package
 cd target || exit

402da36a61268967779c6674833efcb37bbcb571
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-09-06 11:40:17 +0200

    Created ProblemPostType

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+type ProblemPostData = {
+  title: string;
+  content: string;
+  author: string;
+};
 export default function Home() {
+  const [problemPost, setProblemPost] = useState<ProblemPostData>({
+    title: "",
+    content: "", 
+    author: "Anonymous" //change it later when we have user authentication
+  });
+
+  
   const [data, setData] = useState("")
   useEffect(() => {
     fetch('http://localhost:8080/api/status')
@@ -9,7 +21,7 @@ export default function Home() {
         if (!res.ok) {
           throw new Error('Network response was not ok');
         }
-        return res.text(); // Parse the response as plain text
+        return res.text(); 
       })
       .then(setData)
       .catch(error => {
@@ -21,6 +33,11 @@ console.log("this is data " + data);
     <main>
       <p>Hello</p>
       <p>{data}</p>
+      <form>
+        <input placeholder="Title" type="text" /> 
+        <input placeholder="Descride your problem" type="text" /> 
+        <input type="submit" />
+      </form>
     </main>
   )
 }

1e7b6ab10911e1258466cfb35241b5615d15749e
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-06 17:11:45 +0200

    updated the startup script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
+
 SOURCE_FILE="/home/ec2-user/env/secrets.yml"
 
 echo "Attempting to Start server..."
-cd /opt/codedeploy-agent/deployment-root/ff76af6c-87aa-4059-89ac-8e079c93601e/ || exit
-echo "Attempting find the latest directory..."
-cd "$(ls -td */ | head -n 1)" || exit
+
+DEPLOYMENT_DIR="/opt/codedeploy-agent/deployment-root/ff76af6c-87aa-4059-89ac-8e079c93601e/"
+cd "$DEPLOYMENT_DIR" || exit
+
+LATEST_DIR=$(ls -td */ | head -n 1)
+cd "$LATEST_DIR" || exit
+
 cd deployment-archive/ || exit
-cd /src/main/resources
-cp $SOURCE_FILE ./
-cd ..
-cd ..
-cd ..
+
+cp "$SOURCE_FILE" src/main/resources/
+
 echo "Attempting to create the package..."
 sudo mvn clean package
+
 cd target || exit
-echo "Staring the java file..."
+
+echo "Starting the Java application..."
 nohup java -jar echoboard-0.0.1-SNAPSHOT.jar > app.log 2>&1 &

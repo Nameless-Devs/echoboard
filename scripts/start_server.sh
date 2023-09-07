7d6d1ff7ef224489c65a196a41cc695382e4a5db
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-07 10:09:27 +0200

    now figures out the direcroty uuid and the jar file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,8 +4,16 @@ SOURCE_FILE="/home/ec2-user/env/secrets.yml"
 
 echo "Attempting to Start server..."
 
-DEPLOYMENT_DIR="/opt/codedeploy-agent/deployment-root/ff76af6c-87aa-4059-89ac-8e079c93601e/"
-cd "$DEPLOYMENT_DIR" || exit
+DEPLOYMENT_ROOT="/opt/codedeploy-agent/deployment-root"
+
+UUID_DIR=$(find "$DEPLOYMENT_ROOT" -maxdepth 1 -type d | grep -E "([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})")
+
+if [ -z "$UUID_DIR" ]; then
+    echo "No UUID directory found under $DEPLOYMENT_ROOT"
+    exit 1
+else
+    cd "$UUID_DIR" || exit
+fi
 
 LATEST_DIR=$(ls -td */ | head -n 1)
 cd "$LATEST_DIR" || exit
@@ -19,5 +27,15 @@ sudo mvn clean package
 
 cd target || exit
 
-echo "Starting the Java application..."
-nohup java -jar echoboard-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
+echo "Finding the .jar file..."
+
+JAR_FILE=$(find . -name "*.jar" -maxdepth 1)
+
+if [ -z "$JAR_FILE" ]; then
+    echo "No .jar file found in the target directory!"
+    exit 1
+fi
+
+# Running the .jar file
+echo "Starting the Java application $JAR_FILE..."
+nohup java -jar "$JAR_FILE" > app.log 2>&1 &

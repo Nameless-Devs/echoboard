e82fb5e9acdb00107152daad7ebf5e3946367ffc
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-08 14:23:45 +0200

    implemented tests for getallechoes endpoint

--- src/test/java/echoboard/echoboard/AllEchoesEndpointTest.java
+++ src/test/java/echoboard/echoboard/AllEchoesEndpointTest.java
@@ -1,6 +1,7 @@
 package echoboard.echoboard;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import echoboard.echoboard.echo.EchoBoard;
 import echoboard.echoboard.echo.EchoController;
 import echoboard.echoboard.echo.EchoService;
@@ -10,29 +11,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(EchoController.class)
-public class getAllEchoesTests {
+public class AllEchoesEndpointTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private EchoService echoService;
 
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public AllEchoesEndpointTest(ObjectMapper objectMapper, MockMvc mockMvc) {
+        this.objectMapper = objectMapper;
+        this.mockMvc = mockMvc;
+    }
+
+
     @Test
     public void testGetAllEchoesReturnsOk() throws Exception {
-        List<EchoBoard> mockEchoes = Collections.singletonList(new EchoBoard());
+        List<EchoBoard> mockEchoes = Arrays.asList(
+                new EchoBoard("as","asd","asdasd"),
+                new EchoBoard("as","asda","asdasd")
+        );
+
+        String expectedJson = objectMapper.writeValueAsString(mockEchoes);
 
         PaginatedScanList<EchoBoard> mockedScanList = mock(PaginatedScanList.class);
         when(mockedScanList.isEmpty()).thenReturn(false);
@@ -40,8 +55,9 @@ public class getAllEchoesTests {
 
         when(echoService.getAllEchoes(anyInt(), any())).thenReturn(mockedScanList);
 
-        mockMvc.perform(get("/echoes").param("limit", "1"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/echoes").param("limit", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
     }
 
     @Test
@@ -51,7 +67,7 @@ public class getAllEchoesTests {
 
         when(echoService.getAllEchoes(anyInt(), any())).thenReturn(emptyMockedList);
 
-        mockMvc.perform(get("/echoes").param("limit", "1"))
+        mockMvc.perform(get("/api/echoes").param("limit", "1"))
                 .andExpect(status().isNoContent());
     }
 

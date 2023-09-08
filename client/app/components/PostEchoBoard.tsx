5ba7a47f258f4721fc7eaffea25bc5220dce804b
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-09-08 10:48:07 +0200

    Extracted PostEchoBoard into separate component

---
+++ client/app/components/PostEchoBoard.tsx
@@ -0,0 +1,96 @@
+import React, {useState} from 'react'
+
+type ProblemPostData = {
+    title: string;
+    content: string;
+    author: string;
+  };
+
+const PostEchoBoard = () => {
+    const [problemPost, setProblemPost] = useState<ProblemPostData>({
+        title: "",
+        content: "", 
+        author: "" //change it later when we have user authentication
+      });
+      const [errorMessage, setErrorMessage] = useState("");
+    
+    
+      const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setProblemPost({ ...problemPost, author: event.target.value });
+      };
+    
+      const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setProblemPost({ ...problemPost, title: event.target.value });
+      };
+    
+      const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setProblemPost({ ...problemPost, content: event.target.value });
+      };
+    
+      const problemPostToSend: ProblemPostData = {
+        title: problemPost.title,
+        content: problemPost.content, 
+        author: problemPost.author || "Anonymous" 
+      }
+    
+      const handleProblemPost = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    
+    
+        fetch("http://localhost:8080/api/echoes", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(problemPostToSend),
+        })
+          .then((response) => { //do we want to do something with the response?
+            if (response.ok) {
+              setProblemPost({
+                title: "",
+                content: "",
+                author: "",
+              });
+              setErrorMessage("");
+            } else {
+              setErrorMessage("Error: Your post could not be sent.")
+              console.error(`HTTP Error! Status: ${response.status}`);
+            }
+          console.log(response); 
+          
+          })
+          .catch((error) => {
+            console.error("Fetch error:", error);
+            setErrorMessage("Error: There was a network issue. Your post wasn't sent");
+          });
+      }
+
+  return (
+    <div>
+        <h2>Create post with your problem</h2>
+        <form className="submitProblem__form" onSubmit={handleProblemPost}>
+      <input 
+        placeholder="Enter your name" 
+        type="text" 
+        value={problemPost.author} 
+        onChange={handleNameChange} /> 
+        <input 
+        placeholder="Title" 
+        type="text" 
+        value={problemPost.title} 
+        onChange={handleTitleChange} /> 
+        <textarea 
+        placeholder="Descride your problem" 
+        cols={50}
+        rows={5}
+        value={problemPost.content} 
+        onChange={handleContentChange} /> 
+        <input type="submit" />
+      </form>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+   
+    </div>
+  )
+}
+
+export default PostEchoBoard
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-type ProblemPostData = {
-  title: string;
-  content: string;
-  author: string;
-};
+import PostEchoBoard from "./components/PostEchoBoard";
+
+
 export default function Home() {
 
   /*This is just to test the connection */
@@ -25,90 +23,12 @@ export default function Home() {
   }, []);
 console.log("this is data " + data);
   /* Connection testing ends here*/
-
-
-  const [problemPost, setProblemPost] = useState<ProblemPostData>({
-    title: "",
-    content: "", 
-    author: "" //change it later when we have user authentication
-  });
-  const [errorMessage, setErrorMessage] = useState("");
-
-
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setProblemPost({ ...problemPost, author: event.target.value });
-  };
-
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setProblemPost({ ...problemPost, title: event.target.value });
-  };
-
-  const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setProblemPost({ ...problemPost, content: event.target.value });
-  };
-
-  const problemPostToSend: ProblemPostData = {
-    title: problemPost.title,
-    content: problemPost.content, 
-    author: problemPost.author || "Anonymous" 
-  }
-
-  const handleProblemPost = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-
-    fetch("http://localhost:8080/api/echoes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(problemPostToSend),
-    })
-      .then((response) => { //do we want to do something with the response?
-        if (response.ok) {
-          setProblemPost({
-            title: "",
-            content: "",
-            author: "",
-          });
-          setErrorMessage("");
-        } else {
-          setErrorMessage("Error: Your post could not be sent.")
-          console.error(`HTTP Error! Status: ${response.status}`);
-        }
-      console.log(response); 
-      
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-        setErrorMessage("Error: There was a network issue. Your post wasn't sent");
-      });
-  }
-
+  
   return (
     <main>
       <p>Hello</p>
       <p>{data}</p>
-      <form className="submitProblem__form" onSubmit={handleProblemPost}>
-      <input 
-        placeholder="Enter your name" 
-        type="text" 
-        value={problemPost.author} 
-        onChange={handleNameChange} /> 
-        <input 
-        placeholder="Title" 
-        type="text" 
-        value={problemPost.title} 
-        onChange={handleTitleChange} /> 
-        <textarea 
-        placeholder="Descride your problem" 
-        cols={50}
-        rows={5}
-        value={problemPost.content} 
-        onChange={handleContentChange} /> 
-        <input type="submit" />
-      </form>
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      <PostEchoBoard />
     </main>
   )
 }

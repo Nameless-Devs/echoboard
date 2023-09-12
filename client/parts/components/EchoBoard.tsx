c9d7587f65b7dca93ac44bfe60b804736dd81b96
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-12 13:53:41 +0200

    implimanting react-query

--- client/parts/components/EchoBoard.tsx
+++ client/parts/components/EchoBoard.tsx
@@ -1,37 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { EchoBoardResponseData, CommentResponseData } from '../Types'
-import { fetchEchoBoards } from '../Functions';
-import { SinglePost } from './SinglePost';
-import Link from 'next/link';
-import { Upvote } from './Upvote';
+import React, { useEffect, useState } from "react";
+import { EchoBoardResponseData, CommentResponseData } from "../Types";
+import { fetchEchoBoards } from "../Functions";
+import { SinglePost } from "./SinglePost";
+import Link from "next/link";
+import { Upvote } from "./Upvote";
+import { useQuery } from "@tanstack/react-query";
 
 export const EchoBoard = () => {
-    const [echoBoards, setEchoBoards] = useState<EchoBoardResponseData[]>([]);
-    useEffect(() => {
-        fetchEchoBoards()
-        .then(data => setEchoBoards(data))
-        .catch(error => console.error('Error fetching data', error));
-    }, []);
+  // const [echoBoards, setEchoBoards] = useState<EchoBoardResponseData[]>([]);
+  // useEffect(() => {
+  //   fetchEchoBoards()
+  //     .then((data) => setEchoBoards(data))
+  //     .catch((error) => console.error("Error fetching data", error));
+  // }, []);
 
+  const {
+    data: echoBoards,
+    isLoading,
+    isError,
+  } = useQuery<EchoBoardResponseData[]>(["echoBoards"], fetchEchoBoards);
 
   return (
-    <div>EchoBoard All Posts
-        {echoBoards.map((echoBoard, index) => (
-          <div key={index}>
-            <Link href={`/${echoBoard.id}`} >
-              <SinglePost 
-              id={echoBoard.id} 
-              title={echoBoard.title} 
-              content={echoBoard.content} 
-              author={echoBoard.author} 
-              upvote={echoBoard.upvote} 
-              created={echoBoard.created} 
-              comments={echoBoard.comments}  
-              />   
-            </Link>
-            <Upvote upvote={echoBoard.upvote} echoBoardId={echoBoard.id} />
-          </div>
-        ))}
+    <div>
+      <h1>Echo Board All Posts</h1>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error!</p>}
+      {echoBoards?.map((echoBoard, index) => 
+        <div key={index}>
+        <SinglePost {...echoBoard} />
+        </div>
+        )}
     </div>
-  )
-}
+    // <div>
+    //   EchoBoard All Posts
+    //   {echoBoards.map((echoBoard, index) => (
+    //     <div key={index}>
+    //       <Link href={`/${echoBoard.id}`}>
+    //         <SinglePost
+    //           id={echoBoard.id}
+    //           title={echoBoard.title}
+    //           content={echoBoard.content}
+    //           author={echoBoard.author}
+    //           upvote={echoBoard.upvote}
+    //           created={echoBoard.created}
+    //           comments={echoBoard.comments}
+    //         />
+    //       </Link>
+    //       <Upvote upvote={echoBoard.upvote} echoBoardId={echoBoard.id} />
+    //     </div>
+    //   ))}
+    // </div>
+  );
+};
--- client/parts/components/SinglePost.tsx
+++ client/parts/components/SinglePost.tsx
@@ -8,7 +8,7 @@ export const SinglePost: React.FC<EchoBoardResponseData> = ({ id, title, author,
         <h2>{author}</h2>
         <h3>{title}</h3>
         <p>{content}</p>
-        <p>comments:{comments.length}</p>
+        <p>comments:{comments?.length}</p>
         {/* <Upvote upvote={upvote} echoBoardId={id} /> */}
     </>
   )

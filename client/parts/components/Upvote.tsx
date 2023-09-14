50a89f476f9574f823a499bbda6dbd4d3b83354e
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-14 10:02:36 +0200

    Fixed upvote problem

--- client/parts/components/Upvote.tsx
+++ client/parts/components/Upvote.tsx
@@ -1,45 +1,26 @@
-import React, { useState } from "react";
-import { upvotePost } from "../Functions";
-import { UpvoteProps } from "../Types";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React from 'react';
+import { upvotePost, fetchEchoBoardById } from '../Functions';
+import { UpvoteProps } from '../Types';
+import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query';
 
-export const Upvote: React.FC<UpvoteProps> = ({ upvote, echoBoardId }) => {
-  // const [currentUpvote, setCurrentUpvote] = useState(upvote);
-
-  // const increaseUpvoteNumber = async () => {
-  //   const response = await upvotePost(echoBoardId);
-  //   if (response.ok) {
-  //     setCurrentUpvote(currentUpvote + 1);
-  //   } else {
-  //     throw new Error(`HTTP Error! Status: ${response.status}`);
-  //   }
-  // };
-
+export const Upvote: React.FC<UpvoteProps> = ({ echoBoardId }) => {
   const queryClient = useQueryClient();
 
-  const currentData = queryClient.getQueryData<{ upvote: number }>([
-    "echoBoard",
-    echoBoardId,
-  ]);
-  const currentUpvote = currentData ? currentData.upvote : 0; 
+  // Fetching the data for the echo directly in the Upvote component
+  const { data: echoData } = useQuery(['echoBoard', echoBoardId], () => fetchEchoBoardById(echoBoardId));
+  const upvoteCount = echoData ? echoData.upvote : 0;
 
   const mutation = useMutation(() => upvotePost(echoBoardId), {
     onSuccess: () => {
-
-      queryClient.invalidateQueries(["echoBoard", echoBoardId]);
-
+      queryClient.invalidateQueries(['echoBoard', echoBoardId]);
       queryClient.refetchQueries(['echoBoard', echoBoardId]);
-    },
+    }
   });
 
   return (
     <div>
-      {/* <button onClick={() => mutation.mutate()}>upvotes: {currentUpvote}</button> */}
-      {/* <button onClick={increaseUpvoteNumber}>upvotes:{currentUpvote}</button> */}
-
-      <button onClick={() => mutation.mutate()}>upvotes: {upvote}</button>
+      <button onClick={() => mutation.mutate()}>upvotes: {upvoteCount}</button>
       {mutation.isError ? <div>Error: {(mutation.error as Error).message}</div> : null}
-
     </div>
   );
 };

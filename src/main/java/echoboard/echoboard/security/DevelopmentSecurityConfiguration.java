a8eb83da970c2449b94f82f0fac5797c0edd200f
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-14 13:31:39 +0200

    created security config for dev profile2

--- src/main/java/echoboard/echoboard/security/DevelopmentSecurityConfiguration.java
+++ src/main/java/echoboard/echoboard/security/DevelopmentSecurityConfiguration.java
@@ -1,2 +1,26 @@
-package echoboard.echoboard.security;public class DevelopmentSecurityConfiguration {
+package echoboard.echoboard.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@Profile("dev")
+public class DevelopmentSecurityConfiguration {
+
+    @Bean
+    DefaultSecurityFilterChain defaultChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth ->
+                        auth
+                                .anyRequest().permitAll()
+                )
+                .cors(withDefaults())
+                .build();
+    }
 }

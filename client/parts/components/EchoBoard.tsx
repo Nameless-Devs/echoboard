c1ecab6d7a282de745cd10863b6778947fcb633c
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-15 12:14:23 +0200

    disabled linting for the file

--- client/parts/components/EchoBoard.tsx
+++ client/parts/components/EchoBoard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from "react";
 import { EchoBoardResponseData, CommentResponseData } from "../Types";
 import { fetchEchoBoards } from "../Functions";

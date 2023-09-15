d13d1dfb48fc6b93914bed9511668971d7e0c6d4
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-15 14:12:07 +0200

    Refactored code

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,23 +6,6 @@ import { EchoBoard } from "../parts/components/EchoBoard";
 
 
 export default function Home() {
-
-  /*This is just to test the connection */
-  const [data, setData] = useState("")
-  useEffect(() => {
-    fetch('http://localhost:8080/api/status')
-      .then(res => {
-        if (!res.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return res.text(); 
-      })
-      .then(setData)
-      .catch(error => {
-        console.error('Fetch error:', error);
-      });
-  }, []);
-  /* Connection testing ends here*/
   
   return (
     <main>

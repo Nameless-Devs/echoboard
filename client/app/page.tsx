377fa72ad8019a1598cfb454181dde9811016939
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-19 09:35:28 +0200

    Refactored app page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import PostEchoBoard from "../parts/components/PostEchoBoard";
-import { EchoBoard } from "../parts/components/EchoBoard";
-
+import PostEchoBoard from "../components/PostEchoBoard";
+import { EchoBoard } from "../components/EchoBoard";
 
 export default function Home() {
-  
   return (
     <main>
       <PostEchoBoard />
       <EchoBoard />
     </main>
-  )
+  );
 }

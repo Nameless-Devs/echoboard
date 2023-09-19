8dbaf3d10bbf74131520dcfc399cef3cd3934088
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-19 09:54:10 +0200

    modified app/page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import PostEchoBoard from "../components/PostEchoBoard";
-import { EchoBoard } from "../components/EchoBoard";
+import PostEchoBoard from "../components/postEchoBoard";
+import { EchoBoard } from "../components/echoBoard";
 
 export default function Home() {
   return (

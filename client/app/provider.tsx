47473cf2f28d35dd2f921a4f346fb29996191cb3
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-20 09:24:48 +0200

    Reverted back export method

--- client/app/provider.tsx
+++ client/app/provider.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   const [client] = useState(new QueryClient());
 
   return (

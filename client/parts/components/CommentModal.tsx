1e2a3ffcc11789ed765e2ca9e06c9eb0f23f5982
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-21 13:03:00 +0200

    Added CommentModal

---
+++ client/parts/components/CommentModal.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Modal, List, ListItem, ListItemText } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { EchoBoardResponseData } from "../Types";
+import { comment } from "postcss";
+
+interface CommentsModalProps {
+  post: EchoBoardResponseData;
+  handleClose: () => void;
+  isOpen: boolean;
+}
+
+const CommentsModal: React.FC<CommentsModalProps> = ({
+  post,
+  handleClose,
+  isOpen,
+}) => {
+  return (
+    <Modal open={isOpen} onClose={handleClose}>
+      <div
+        style={{
+          padding: "20px",
+          background: "#fff",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Typography gutterBottom variant="h5" component="div">
+          <h3>{post.title}</h3>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <p>{post.content}</p>
+          <h4>{post.author}</h4>
+        </Typography>
+        <List>
+          {post.echoBoardComments.map((comment, index) => (
+            <ListItem key={index}>
+              <ListItemText
+                secondary={comment.author}
+                primary={comment.comment}
+              ></ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </Modal>
+  );
+};
+
+export default CommentsModal;

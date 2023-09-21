a562f2c993badef0ce35cf87179556711a4c4bd2
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-21 15:09:31 +0200

    refactored dev initialising

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,14 +1,17 @@
+-- Create the echo_board table
+CREATE TABLE echo_board (
+                            id BIGINT NOT NULL AUTO_INCREMENT,  -- Primary Key with auto-increment
+                            title VARCHAR(255),
+                            content TEXT,
+                            author VARCHAR(255),
+                            upvote INT,
+                            created TIMESTAMP,
+                            PRIMARY KEY (id)
+);
+
+
 INSERT INTO echo_board (title, content, author, upvote, created)
 VALUES ('Project Deadline Delayed',
-        'We are facing challenges in meeting the project deadline due to resource constraints. Let's discuss possible solutions.',
-        'John Doe', 8, '2023-09-21T10:00:00Z');
+        'We are facing challenges in meeting the project deadline due to resource constraints. Let''s discuss possible solutions.','John Doe', 8, '2023-09-21T10:00:00Z');
 
-INSERT INTO echo_board (title, content, author, upvote, created)
-VALUES ('Technical Debt Accumulating',
-        'Our codebase is accumulating technical debt. How should we prioritize refactoring efforts?',
-        'Jane Smith', 12, '2023-09-22T12:30:00Z');
 
-INSERT INTO echo_board (title, content, author, upvote, created)
-VALUES ('Remote Work Challenges',
-        'Working remotely has presented communication challenges. Share your experiences and suggestions.',
-        'Alice Johnson', 6, '2023-09-23T09:45:00Z');

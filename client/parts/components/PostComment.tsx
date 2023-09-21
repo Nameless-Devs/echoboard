82b977bd1ecc338f49393615c568675d60079ef8
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-09-21 12:06:36 +0200

    Created form to write a comment

--- client/parts/components/PostComment.tsx
+++ client/parts/components/PostComment.tsx
@@ -1,9 +1,35 @@
 import React, { useState } from 'react'
+import { CommentToPost } from '../Types';
+import { Button, TextField } from '@mui/material';
 
 export const PostComment = () => {
-    const [commentToPost, setCommentToPost] = useState();
+    const [commentToPost, setCommentToPost] = useState<CommentToPost>({
+        author: "", 
+        comment:""
+    });
 
   return (
-    <div>PostComment</div>
+    <>
+    <form>
+    <TextField
+            label="Enter your name"
+            variant="outlined"
+            name="author"
+            value={commentToPost.author}
+            onChange={(e) =>
+              setCommentToPost({ ...commentToPost, author: e.target.value }) }/>
+     <TextField
+            label="comment"
+            variant="outlined"
+            name="comment"
+            value={commentToPost.comment}
+            onChange={(e) =>
+              setCommentToPost({ ...commentToPost, comment: e.target.value }) }/>
+    <Button variant="outlined" type="submit">
+            Comment
+          </Button>
+            
+    </form>
+    </>
   )
 }

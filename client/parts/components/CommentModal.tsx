ed3d55bf9452c0031ec1bf1aae993f26bdd55099
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-22 15:45:49 +0200

    Added basic Css

--- client/parts/components/CommentModal.tsx
+++ client/parts/components/CommentModal.tsx
@@ -7,7 +7,7 @@ import { PostComment } from "./PostComment";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import { fetchEchoBoardById } from "../Functions";
 import { upvoteComment } from "../Functions";
-
+import { Box } from "@mui/material";
 import Button from "@mui/material/Button";
 
 interface CommentsModalProps {
@@ -21,9 +21,8 @@ const CommentsModal: React.FC<CommentsModalProps> = ({
   handleClose,
   isOpen,
 }) => {
-
   const { data: updatedPost } = useQuery<EchoBoardResponseData>(
-    ["comments", post.id], 
+    ["comments", post.id],
     async () => {
       const result = await fetchEchoBoardById(post.id);
       return result;
@@ -53,21 +52,39 @@ const CommentsModal: React.FC<CommentsModalProps> = ({
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
+          borderRadius: "5px",
         }}
       >
-        <Typography gutterBottom variant="h5" component="div">
-          <h3>{post.title}</h3>
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          <p>{post.content}</p>
-          <h4>{post.author}</h4>
-        </Typography>
+        <Box mb={1}>
+          <Typography variant="body2" color="text.secondary">
+            {post.author}
+          </Typography>
+        </Box>
+        <Box mb={1}>
+          <Typography variant="h6" color="text.secondary">
+            {post.title}
+          </Typography>
+        </Box>
+        <Box style={{ borderBottom: "1px solid #e0e0e0" }}>
+          <Typography mb={1} variant="h6">
+            {post.content}
+          </Typography>
+          <Upvote upvote={displayPost.upvote} echoBoardId={displayPost.id} />
+        </Box>
         <List>
           {displayPost.echoBoardComments.map((comment, index) => (
             <ListItem key={index}>
               <ListItemText
-                secondary={comment.author}
-                primary={comment.comment}
+                primary={
+                  <Typography variant="body2" color="textSecondary">
+                    {comment.author}
+                  </Typography>
+                }
+                secondary={
+                  <Typography variant="body1" color="textPrimary">
+                    {comment.comment}
+                  </Typography>
+                }
               ></ListItemText>
               <Button onClick={() => mutation.mutate(comment.id)}>
                 Upvote: {comment.upvote}
@@ -75,7 +92,6 @@ const CommentsModal: React.FC<CommentsModalProps> = ({
             </ListItem>
           ))}
         </List>
-        <Upvote upvote={displayPost.upvote} echoBoardId={displayPost.id} />
         <PostComment echoBoardId={displayPost.id} />
       </div>
     </Modal>

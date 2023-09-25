59d44a33711d84118bff86fc30721cf1f30e8146
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-09-25 09:52:22 +0200

    Changed application-dev.yml

--- src/main/java/se/salt/echoboard/model/EchoBoardSolution.java
+++ src/main/java/se/salt/echoboard/model/EchoBoardSolution.java
@@ -1,37 +1,37 @@
-//package se.salt.echoboard.model;
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//import lombok.ToString;
-//import java.time.Instant;
-//
-//
-//@Entity
+package se.salt.echoboard.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import java.time.Instant;
+
+
+@Entity
 //@Data
-//@ToString
-//@Table(name = "echo_board_solution")
-//public class EchoBoardSolution {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-//    private Long id;
-//
-//    private String author;
-//    private String content;
-//    private Long upvote;
-//
-//    @Column(columnDefinition = "TIMESTAMP")
-//    private Instant created;
-//
-//    public EchoBoardSolution(String author, String content) {
-//        this.author = author;
-//        this.content = content;
-//        this.upvote = 0L;
-//        this.created = Instant.now();
-//    }
-//
-//    public EchoBoardSolution() {
-//        this.upvote = 0L;
-//        this.created = Instant.now();
-//    }
-//}
+@ToString
+@Table(name = "echo_board_solution")
+public class EchoBoardSolution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String author;
+    private String content;
+    private Long upvote;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private Instant created;
+
+    public EchoBoardSolution(String author, String content) {
+        this.author = author;
+        this.content = content;
+        this.upvote = 0L;
+        this.created = Instant.now();
+    }
+
+    public EchoBoardSolution() {
+        this.upvote = 0L;
+        this.created = Instant.now();
+    }
+}

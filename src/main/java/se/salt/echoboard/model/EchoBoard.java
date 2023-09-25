1247c07fe03cd28bcf6bc02711bf655847cac523
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-25 16:56:16 +0200

    Changed from Sequence to Identity due to primary key overlap

--- src/main/java/se/salt/echoboard/model/EchoBoard.java
+++ src/main/java/se/salt/echoboard/model/EchoBoard.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class EchoBoard {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String title;

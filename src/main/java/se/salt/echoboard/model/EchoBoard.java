4b8cf203ee345d6a31cf1c2bb3555da94dd5cf9f
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-26 14:25:04 +0200

    Set content field to have a maximum length of 1000 characters using @Column annotation

--- src/main/java/se/salt/echoboard/model/EchoBoard.java
+++ src/main/java/se/salt/echoboard/model/EchoBoard.java
@@ -22,6 +22,8 @@ public class EchoBoard {
     private long id;
 
     private String title;
+
+    @Column(length = 1000)
     private String content;
     private String author;
     private int upvote;

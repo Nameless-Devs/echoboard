2c406425c2749e16ef279b9358c6397fe130035c
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-09-26 10:03:41 +0200

    Added solution types

--- client/parts/Types.tsx
+++ client/parts/Types.tsx
@@ -30,3 +30,16 @@ export type UpvoteProps = {
   author: string; 
   content: string;
  }
+
+ //Solution types are the same as comment type, should we refactor? 
+ export type SolutionToPost = {
+  author: string; 
+  content: string;
+ }
+ export type SolutionResponseData = {
+  id: string;
+  author: string;
+  content: string;
+  upvote: number;
+  created: string;
+};

89b3364683615ee5fa9b0514c7af42311e30df87
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-26 20:48:12 +0200

    updated echoboardsample

--- src/test/java/util/TestUtilities.java
+++ src/test/java/util/TestUtilities.java
@@ -7,14 +7,6 @@ import se.salt.echoboard.model.EchoBoard;
 
 public class TestUtilities {
 
-
-    public static EchoBoard echoBoardSample() {
-        String title = "Sample title";
-        String content = "Sample content";
-        String author = "Sample author";
-        return new EchoBoard(title, content, author);
-    }
-
     public static void assertEchoBoardEqual(EchoBoard expected, EchoBoard actual) {
         Assertions.assertNotNull(actual, "Actual EchoBoard should not be null");
         Assertions.assertEquals(expected.getId(), actual.getId());
@@ -23,6 +15,12 @@ public class TestUtilities {
         Assertions.assertEquals(expected.getAuthor(), actual.getAuthor());
     }
 
+    public static EchoBoard echoBoardSample() {
+        String title = "Ineffective Cross-Departmental Meetings";
+        String content = "Cross-departmental meetings often lack direction and effectiveness. Participants from different departments may struggle to communicate their needs and expectations, resulting in lengthy, unproductive discussions. Improving meeting structures and agendas can enhance inter-departmental collaboration. ";
+        String author = "Meeting Facilitator";
+        return new EchoBoard(title, content, author);
+    }
     public static String convertJsonString(final Object object) {
         try {
             return new ObjectMapper().writeValueAsString(object);

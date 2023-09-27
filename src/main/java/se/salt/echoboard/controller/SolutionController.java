2e7d9390832cf5ddd6d2bf6599d9e448cc4fcd2b
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-27 11:46:53 +0200

    encapsulated solution methods into its own controller

---
+++ src/main/java/se/salt/echoboard/controller/SolutionController.java
@@ -0,0 +1,32 @@
+package se.salt.echoboard.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.salt.echoboard.model.EchoBoardSolution;
+import se.salt.echoboard.service.EchoBoardService;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/solutions")
+public class SolutionController {
+
+    private final EchoBoardService echoService;
+
+    @GetMapping("{echoBoardSolutionId}")
+    public ResponseEntity<EchoBoardSolution> getEchoBoardSolution(@PathVariable long echoBoardSolutionId) {
+        return ResponseEntity.of(echoService.getSolutionById(echoBoardSolutionId));
+    }
+
+    @PatchMapping("{echoBoardSolutionId}")
+    public ResponseEntity<EchoBoardSolution> updateSolutionStatus(@PathVariable long echoBoardSolutionId
+            , @RequestParam EchoBoardSolution.SolutionStatus updateToStage) {
+
+        var echoBoardSolution = echoService.getSolutionById(echoBoardSolutionId)
+                .map(solution -> solution.updateSolutionStatus(updateToStage))
+                .map(echoService::saveSolution);
+
+        return ResponseEntity.of(echoBoardSolution);
+    }
+}

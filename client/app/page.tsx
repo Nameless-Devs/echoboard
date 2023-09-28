de08bfb1dd6c04f55039064bb2f7366644c10f1a
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-28 16:08:17 +0200

    Removed the Jwt rendering

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
 import PostEchoBoard from "../parts/components/PostEchoBoard";
 import { EchoBoard } from "../parts/components/EchoBoard";
+import JwtAuth from "@/parts/components/JwtAuth";
+import { useCookies } from "react-cookie";
+
 
 
 export default function Home() {
-  
+
   return (
     <main>
       <PostEchoBoard />

fea06f776e300ce342cf32c323eb14949ff4c0a5
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-28 12:21:00 +0200

    updated config

--- src/main/java/se/salt/echoboard/security/config/SecurityConfiguration.java
+++ src/main/java/se/salt/echoboard/security/config/SecurityConfiguration.java
@@ -1,11 +1,18 @@
 package se.salt.echoboard.security.config;
 
+import jakarta.servlet.Filter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import se.salt.echoboard.security.CustomAuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,33 +30,47 @@ import java.util.List;
 public class SecurityConfiguration {
 
     private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomBearerTokenFilter customBearerTokenFilter;
 
     @Bean
     DefaultSecurityFilterChain defaultChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth ->
+        return http
+                .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers("/api/status").permitAll()
+                                .requestMatchers("api/status").permitAll()
                                 .anyRequest().authenticated()
                 )
-//                .cors().disable
-//                .csrf().disable
+                .cors(Customizer.withDefaults())
                 .oauth2Login(oauth2 -> oauth2
                         .successHandler(customAuthenticationSuccessHandler)
                         )
-                .cors(corsCustomizer())
-                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(customBearerTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable()
                 .build();
     }
 
+//    @Bean
+//    public Customizer<CorsConfigurer<HttpSecurity>> corsCustomizer() {
+//        return cors -> cors
+//                .configurationSource(request -> {
+//                    CorsConfiguration config = new CorsConfiguration();
+//                    config.setAllowedOrigins(List.of("https://echoboard.vercel.app/",
+//                                                        "http://localhost:3000/"));
+//                    config.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH"));
+//                    config.setAllowCredentials(true);
+//                    return config;
+//                });
+//    }
+
     @Bean
-    public Customizer<CorsConfigurer<HttpSecurity>> corsCustomizer() {
-        return cors -> cors
-                .configurationSource(request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("https://echoboard.vercel.app/"));
-                    config.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH"));
-                    config.setAllowCredentials(true);
-                    return config;
-                });
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000/"); // Replace with the actual origin of your frontend
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }

10351bbae6787e12c7736ce5d5fb6ef187b438a7
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-09-29 14:59:06 +0200

    Created user endpoint that returnes mocked data so i can work on FE

---
+++ src/main/java/se/salt/echoboard/controller/UserController.java
@@ -0,0 +1,21 @@
+package se.salt.echoboard.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.salt.echoboard.model.EchoBoardUser;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<EchoBoardUser> getAuthenticatedUser(){
+        EchoBoardUser user = EchoBoardUser.builder().name("Mikey Tester")
+                .id(12345)
+                .email("mikey.mike@gmail.com").build();
+        return ResponseEntity.ok(user);
+    }
+
+}

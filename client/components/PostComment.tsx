11c1e4312c244055e751f3840c7ae7349eef80be
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-02 10:55:18 +0200

    Moved out commoponent folder to root

---
+++ client/components/PostComment.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { CommentToPost } from "../service/Types";
+import { Button, TextField } from "@mui/material";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { postComment } from "../service/Functions";
+import { useCookies } from "react-cookie";
+import "../app/styles/PostCommentStyles.css"
+
+type CommentProps = {
+  echoBoardId: string;
+};
+
+export const PostComment: React.FC<CommentProps> = ({ echoBoardId }) => {
+  const [commentToPost, setCommentToPost] = useState<CommentToPost>({
+    author: "",
+    content: "",
+  });
+
+  const [cookies] = useCookies();
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation((data: CommentToPost) =>
+    postComment(data, echoBoardId, cookies.JwtToken)
+  );
+
+  const handleCommentPost = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!commentToPost.author.trim() || !commentToPost.content.trim()) {
+      return;
+    }
+
+    mutation.mutate(commentToPost, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["echoBoards"]);
+        queryClient.refetchQueries(["comments", echoBoardId]);
+        setCommentToPost({
+          author: "",
+          content: "",
+        });
+      },
+      onError: (error) => {
+        console.error("Error:", error);
+      },
+    });
+  };
+
+  return (
+    <>
+      <form className="post-comment__form" onSubmit={handleCommentPost}>
+        <TextField
+          className="post-comment__name-input"
+          label="Enter your name"
+          variant="outlined"
+          name="author"
+          size="small"
+          value={commentToPost.author}
+          onChange={(e) =>
+            setCommentToPost({ ...commentToPost, author: e.target.value })
+          }
+        />
+        <TextField
+          className="post-comment__comment"
+          label="Comment"
+          variant="outlined"
+          name="comment"
+          multiline
+          rows={2}
+          value={commentToPost.content}
+          onChange={(e) =>
+            setCommentToPost({ ...commentToPost, content: e.target.value })
+          }
+        />
+        <Button
+          className="post-comment__button"
+          variant="outlined"
+          type="submit"
+        >
+          Comment
+        </Button>
+      </form>
+    </>
+  );
+};
---
+++ client/components/SinglePost.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { EchoBoardResponseData } from "../service/Types";
+import Typography from "@mui/material/Typography";
+
+export const SinglePost: React.FC<EchoBoardResponseData> = ({
+  id,
+  title,
+  author,
+  content,
+  upvote,
+  echoBoardComment: comments,
+}) => {
+  return (
+    <>
+      <Typography gutterBottom variant="h5" component="div">
+        <h3>{title}</h3>
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        <p>{content}</p>
+        <h4>{author}</h4>
+      </Typography>
+    </>
+  );
+};

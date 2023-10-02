5214558f0a6eb6078741a2a653bb917b3069dd2b
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-02 10:55:52 +0200

    Changed import statement

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,15 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import PostEchoBoard from "../parts/components/PostEchoBoard";
-import { EchoBoard } from "../parts/components/EchoBoard";
-import JwtAuth from "@/parts/components/JwtAuth";
+import React from "react";
+import PostEchoBoard from "../components/PostEchoBoard";
+import { EchoBoard } from "../components/EchoBoard";
+import JwtAuth from "@/components/JwtAuth";
 
 export default function Home() {
-
   return (
     <main>
       <PostEchoBoard />
       <EchoBoard />
-      <JwtAuth /> 
+      <JwtAuth />
     </main>
-  )
+  );
 }

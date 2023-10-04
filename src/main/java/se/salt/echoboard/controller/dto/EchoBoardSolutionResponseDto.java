25ea9cc119b211689c0562df119fb59543a589a7
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-04 15:02:35 +0200

    created EchoBoard solution dto

---
+++ src/main/java/se/salt/echoboard/controller/dto/EchoBoardSolutionResponseDto.java
@@ -0,0 +1,13 @@
+package se.salt.echoboard.controller.dto;
+
+import se.salt.echoboard.model.EchoBoardSolution;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * DTO for {@link EchoBoardSolution}
+ */
+public record EchoBoardSolutionResponseDto(long id, String author, String content, int upvote,
+                                           EchoBoardSolution.SolutionStatus status, Instant created) implements Serializable {
+}

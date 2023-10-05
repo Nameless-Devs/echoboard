793d64428d667d13363f96fb97c9b665bbb8e08c
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-05 13:09:52 +0200

    Implemented header to home page

--- client/app/home/Header.tsx
+++ client/app/home/Header.tsx
@@ -1,56 +1,44 @@
-import * as React from 'react';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import * as React from "react";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Image from "next/image";
+import EchoLogo from "../image/EchoLogo.png";
 
 interface HeaderProps {
-  sections: ReadonlyArray<{
-    title: string;
-    url: string;
-  }>;
   title: string;
 }
 
 export default function Header(props: HeaderProps) {
-  const { sections, title } = props;
+  const { title } = props;
 
   return (
-    <React.Fragment>
-      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Button size="small">Subscribe</Button>
+    <>
+      <Toolbar
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          bgcolor: "#E5E4E2",
+          borderRadius: "8px",
+        }}
+      >
+        <Image src={EchoLogo} alt="EchoBoard Logo" width={40} />
         <Typography
           component="h2"
           variant="h5"
           color="inherit"
-          align="center"
           noWrap
           sx={{ flex: 1 }}
         >
           {title}
         </Typography>
         <Button variant="outlined" size="small">
-          Sign up
+          <Link href={"http://localhost:8080/echoes"}>
+            Join the Community
+          </Link>
         </Button>
       </Toolbar>
-      <Toolbar
-        component="nav"
-        variant="dense"
-        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-      >
-        {sections.map((section) => (
-          <Link
-            color="inherit"
-            noWrap
-            key={section.title}
-            variant="body2"
-            href={section.url}
-            sx={{ p: 1, flexShrink: 0 }}
-          >
-            {section.title}
-          </Link>
-        ))}
-      </Toolbar>
-    </React.Fragment>
+    </>
   );
 }

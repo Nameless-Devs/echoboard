871608609c770281b9e0a35f4659dd6aeddc9b5f
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-05 13:09:40 +0200

    Implemented footer to home page

--- client/app/home/Footer.tsx
+++ client/app/home/Footer.tsx
@@ -1,25 +1,29 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
-interface SidebarProps {
+interface FooterProps {
   description: string;
   title: string;
 }
 
-export default function Sidebar(props: SidebarProps) {
+export default function Footer(props: FooterProps) {
   const { description, title } = props;
 
   return (
-    <Grid item xs={12} md={4} style={{ position: 'fixed', bottom: 0, width: '100%' }}>
-      <Paper elevation={0} sx={{ p: 2, bgcolor: 'grey.200' }}>
+    <Grid
+      item
+      xs={12}
+      md={4}
+      style={{ position: "fixed", bottom: 0, width: "100%" }}
+    >
+      <Paper elevation={0} sx={{ p: 2, bgcolor: "grey.200" }}>
         <Typography variant="h6" gutterBottom>
           {title}
         </Typography>
         <Typography>{description}</Typography>
       </Paper>
-    
     </Grid>
   );
 }

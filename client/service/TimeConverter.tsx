e825114369b2a0c161ffa1193081906d0a3a208c
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-05 12:19:12 +0200

    Added time converter function

---
+++ client/service/TimeConverter.tsx
@@ -0,0 +1,35 @@
+ export function timeconverter(timestamp: string): string {
+    const date = new Date(timestamp);
+    const months = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12; // Convert to 12-hour format
+  
+    // Function to add the appropriate suffix to the day
+    const getDayWithSuffix = (day: number): string => {
+      if (day >= 11 && day <= 13) {
+        return `${day}th`;
+      }
+      switch (day % 10) {
+        case 1:
+          return `${day}st`;
+        case 2:
+          return `${day}nd`;
+        case 3:
+          return `${day}rd`;
+        default:
+          return `${day}th`;
+      }
+    };
+  
+    return `${month} ${getDayWithSuffix(day)} at ${formattedHours}:${minutes.toString().padStart(2, '0')} ${ampm}`;
+  }
+  
+  

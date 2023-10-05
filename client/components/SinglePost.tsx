3d3c267df7b28c7367ab12c7daf52a418243a6b2
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-05 12:15:14 +0200

    Rendering created property

--- client/components/SinglePost.tsx
+++ client/components/SinglePost.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { EchoBoardResponseData } from "@/service/Types";
 import Typography from "@mui/material/Typography";
+import { Avatar, Box } from "@mui/material";
 
 export const SinglePost: React.FC<EchoBoardResponseData> = ({
   id,
@@ -9,16 +10,26 @@ export const SinglePost: React.FC<EchoBoardResponseData> = ({
   content,
   anonymous,
   upvote,
+  created,
   echoBoardComment: comments,
 }) => {
   return (
     <>
-      <Typography variant="subtitle2"></Typography>
-        <h4>{anonymous ? "Anonymous" : author}</h4>
+      <Box>
+        <Avatar />
+        <Box>
+          <Typography variant="subtitle1">
+            {anonymous ? "Anonymous" : author}
+          </Typography>
+          <Typography variant="caption">
+            {created}
+          </Typography>
+        </Box>
+      </Box>
       <Typography gutterBottom variant="h5" component="div">
-         {title}
+        {title}
       </Typography>
-      <Typography variant="body2" color="text.secondary">
+      <Typography variant="body1" color="text.secondary">
         <p>{content}</p>
       </Typography>
     </>

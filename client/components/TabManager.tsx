85a21c7949d1f349839e028c8f8bfb3ea2dcc339
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-10 09:46:21 +0200

    Added TabManger for SPR

---
+++ client/components/TabManager.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box, List, ListItem, ListItemText, Modal, Tab, Tabs} from "@mui/material";
+
+
+const TabManager = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TabManager

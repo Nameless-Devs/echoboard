6a11705c2389392045a6319c1f8abf2bf128a15c
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-10 13:17:46 +0200

    Reformat document with VSCode formatter

--- client/components/JwtAuth.tsx
+++ client/components/JwtAuth.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import Cookies from "js-cookie";
-import jwtDecode from "jwt-decode"; 
+import jwtDecode from "jwt-decode";
 
 type DecodedToken = {
   exp: number;
@@ -11,14 +11,13 @@ const JwtAuth = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
   const existingToken = Cookies.get("JwtToken");
-  
+
   const isTokenExpired = !existingToken || checkIfTokenExpired(existingToken);
 
   if (token && (isTokenExpired || !existingToken)) {
     const tokenString = Array.isArray(token) ? token[0] : token;
     Cookies.set("JwtToken", tokenString, { expires: 1 });
   }
-
 
   function checkIfTokenExpired(token: string) {
     const decodedToken: DecodedToken = jwtDecode(token);

6efa38b72582212ff5bac74eeee458bc14d2167c
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-11 12:06:57 +0200

    Updated endpoints

--- client/service/config.ts
+++ client/service/config.ts
@@ -6,6 +6,8 @@ export const ENDPOINTS = {
   POST_COMMENT: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}/comments`,
   POST_SOLUTION: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}/solutions`,
   UPVOTE_SOLUTION: `${process.env.NEXT_PUBLIC_API_BASE_URL}/solutions/{solutionId}/upvote`,
+  USER: `${process.env.NEXT_PUBLIC_API_BASE_URL}/user`,
+  UPDATE_SOLUTION_STATUS: `${process.env.NEXT_PUBLIC_API_BASE_URL}/solutions/{solutionId}?updateToStage={status}`
 };
 
 // example from Marcus

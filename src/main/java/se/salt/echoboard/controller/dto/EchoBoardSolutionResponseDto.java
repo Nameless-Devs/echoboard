503d125cd63a2378ebacdd37bd5505cb72b8bf5f
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-11 11:09:34 +0200

    upvotes are now a set

--- src/main/java/se/salt/echoboard/controller/dto/EchoBoardSolutionResponseDto.java
+++ src/main/java/se/salt/echoboard/controller/dto/EchoBoardSolutionResponseDto.java
@@ -3,11 +3,12 @@ package se.salt.echoboard.controller.dto;
 import se.salt.echoboard.model.EchoBoardSolution;
 
 import java.time.Instant;
+import java.util.Set;
 
 /**
  * DTO for {@link EchoBoardSolution}
  */
-public record EchoBoardSolutionResponseDto(long id, String content, int upvote, boolean anonymous,
+public record EchoBoardSolutionResponseDto(long id, String content, Set<String> upvote, boolean anonymous,
                                            EchoBoardSolution.SolutionStatus status, Instant created,
                                            EchoBoardUserResponseDto echoBoardUser) {
 

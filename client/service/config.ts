5552d5b99979e2a00830c1dfb9b0591519f9df2a
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-11 11:10:44 +0200

    Added enviroment variables

--- client/service/config.ts
+++ client/service/config.ts
@@ -1,15 +1,14 @@
-//Development Base Url
-const API_BASE_URL = "http://localhost:8080/api";
-
-//Deployment Base Url
-// const API_BASE_URL = "https://api.echoboard.site/api"
-
 export const ENDPOINTS = {
-  POST_ECHO: `${API_BASE_URL}/echoes`,
-  UPVOTE_POST: `${API_BASE_URL}/echoes/{echoBoardId}/upvote`,
-  UPVOTE_COMMENT: `${API_BASE_URL}/echoes/{echoBoardId}/comments/{commentId}/upvote`,
-  ECHO: `${API_BASE_URL}/echoes/{echoBoardId}`,
-  POST_COMMENT: `${API_BASE_URL}/echoes/{echoBoardId}/comments`,
-  POST_SOLUTION: `${API_BASE_URL}/echoes/{echoBoardId}/solutions`,
-  UPVOTE_SOLUTION: `${API_BASE_URL}/echoes/{echoBoardId}/solutions/{solutionId}/upvote`,
+  POST_ECHO: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes`,
+  UPVOTE_POST: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}/upvote`,
+  UPVOTE_COMMENT: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}/comments/{commentId}/upvote`,
+  ECHO: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}`,
+  POST_COMMENT: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}/comments`,
+  POST_SOLUTION: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}/solutions`,
+  UPVOTE_SOLUTION: `${process.env.NEXT_PUBLIC_API_BASE_URL}/echoes/{echoBoardId}/solutions/{solutionId}/upvote`,
 };
+
+// example from Marcus
+// if(process.env.NEXT_PUBLIC_FF_MY_FEATURE === "ON"){
+// alert("WIP");
+// } 

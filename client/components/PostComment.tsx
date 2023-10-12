a844565d30a8f24e06036dd76dde0d12bad121ab
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-12 11:29:08 +0200

    Extracted TextField from PostComment to follow SRP

--- client/components/PostComment.tsx
+++ client/components/PostComment.tsx
@@ -1,78 +1,49 @@
 import React, { useState } from "react";
 import { CommentToPost, UserResponseData } from "@/service/Types";
-import { Avatar, Box, IconButton, TextField } from "@mui/material";
+import { Avatar, Box } from "@mui/material";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postComment } from "@/service/Functions";
-import "../app/styles/PostComment.css";
 import { useCookies } from "react-cookie";
-import { StyledBadge } from "./StyledBadge";
-import SendIcon from "@mui/icons-material/Send";
+import { CommentForm } from "./CommentForm"; 
+import { StyledBadge } from "./StyledBadge"; 
+import "../app/styles/PostComment.css";
 
-type CommentProps = {
+interface CommentProps {
   echoBoardId: string;
   user: UserResponseData;
-};
+}
 
 export const PostComment: React.FC<CommentProps> = ({ echoBoardId, user }) => {
-  const [commentToPost, setCommentToPost] = useState<CommentToPost>({
-    author: user.name,
-    content: "",
-  });
-  const [numberOfRows, setNumberOfRows] = useState<number>(1);
   const [isSuccess, setIsSuccess] = useState(false);
-  const [isSendButtonVisible, setIsSendButtonVisible] = useState(false);
   const [cookies] = useCookies();
+
   const queryClient = useQueryClient();
 
   const mutation = useMutation((data: CommentToPost) =>
     postComment(data, echoBoardId, cookies.JwtToken)
   );
 
-  const handleTextAreaFocus = () => {
-    setNumberOfRows(3);
-    setIsSendButtonVisible(true);
-  };
-  const handleTextAreaBlur = () => {
-    if (!commentToPost.content.trim()) {
-      setNumberOfRows(1);
-      setIsSendButtonVisible(false);
-    }
-    handleTextAreaFocus;
-  };
-  const handleCommentPost = (event?: React.FormEvent<HTMLFormElement>) => {
-    if (event) {
-      event.preventDefault();
-    }
-
-    if (!commentToPost.content.trim()) {
+  const handleCommentPost = (content: string) => {
+    if (!content.trim()) {
       return;
     }
+
+    const commentToPost = {
+      author: user.name,
+      content,
+    };
 
     mutation.mutate(commentToPost, {
       onSuccess: () => {
         queryClient.invalidateQueries(["echoBoards"]);
         queryClient.refetchQueries(["comments", echoBoardId]);
-        setCommentToPost({
-          author: user.name,
-          content: "",
-        });
         setIsSuccess(true);
         setTimeout(() => setIsSuccess(false), 1500);
-        setNumberOfRows(1);
-        setIsSendButtonVisible(false);
       },
       onError: (error) => {
         console.error("Error:", error);
       },
     });
-  };
-
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === "Enter" && event.shiftKey === false) {
-
-      event.preventDefault();
-      handleCommentPost();
-    }
   };
 
   return (
@@ -82,7 +53,7 @@ export const PostComment: React.FC<CommentProps> = ({ echoBoardId, user }) => {
           Your comment was successfully posted
         </h3>
       )}
-      <form className="post-comment__form" onSubmit={handleCommentPost}>
+      <form className="post-comment__form" onSubmit={(e) => e.preventDefault()}>
         <Box className="post-comment__box">
           <Box>
             <StyledBadge
@@ -97,33 +68,7 @@ export const PostComment: React.FC<CommentProps> = ({ echoBoardId, user }) => {
               />
             </StyledBadge>
           </Box>
-          <TextField
-            className="post-comment__comment"
-            label="Comment"
-            variant="outlined"
-            name="comment"
-            multiline
-            rows={numberOfRows}
-            value={commentToPost.content}
-            onChange={(e) =>
-              setCommentToPost({ ...commentToPost, content: e.target.value })
-            }
-            onFocus={handleTextAreaFocus}
-            onBlur={handleTextAreaBlur}
-            onKeyDown={(event) => handleKeyPress(event)}
-            InputProps={{
-              endAdornment: isSendButtonVisible && (
-                <IconButton
-                  type="submit"
-                  color="primary"
-                  className="post-comment__send-icon"
-                  style={{ position: "absolute", bottom: "0", right: "0" }}
-                >
-                  <SendIcon />
-                </IconButton>
-              ),
-            }}
-          />
+          <CommentForm onCommentSubmit={handleCommentPost} />
         </Box>
       </form>
     </>

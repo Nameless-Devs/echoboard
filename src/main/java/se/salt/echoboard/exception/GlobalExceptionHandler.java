f7153c91eac7c9cc33c056bad232a698fd1a49ac
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-13 10:34:38 +0200

    refactored code

--- src/main/java/se/salt/echoboard/exception/GlobalExceptionHandler.java
+++ src/main/java/se/salt/echoboard/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package se.salt.echoboard.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,9 +14,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleEntityNotFoundException(NoSuchElementException ex){
+    public String handleEntityNotFoundException(NoSuchElementException ex){
         log.error("Entity not found", ex);
-        String errorMessage = ex.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        return ex.getMessage();
     }
 }

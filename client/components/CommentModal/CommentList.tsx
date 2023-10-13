a56cb7716bb51b9cd852ad1284963965e84f27d0
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 13:38:58 +0200

    Added props for comment list component

--- client/components/CommentModal/CommentList.tsx
+++ client/components/CommentModal/CommentList.tsx
@@ -1,4 +1,10 @@
+import { EchoBoardResponseData } from '@/service/Types';
 import React from 'react'
+
+type CommentsListProps {
+    comments: EchoBoardResponseData[];
+    onCommentUpvote: (commentId: string) => void;
+  }
 
 export const CommentList = () => {
   return (

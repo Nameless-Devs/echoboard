e365c3b724865d0c729c862f098121948d340e08
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 11:51:07 +0200

    Extracted elements into solution item

--- client/components/CommentModal/SolutionItem.tsx
+++ client/components/CommentModal/SolutionItem.tsx
@@ -1,14 +1,35 @@
 import { SolutionResponseData } from '@/service/Types'
+import { Avatar, ListItem, ListItemText, Typography } from '@mui/material';
 import React from 'react'
+import { SolutionStatusButton } from '../SolutionStatusButton';
+import UpvoteButton from '../UpvoteButton';
 
 type SolutionItemProps = {
     solution: SolutionResponseData;
-    onUpvote: () => void; 
+    onUpvote: (solutionId: string) => void;
 }
 
 export const SolutionItem: React.FC<SolutionItemProps> = ({ solution, onUpvote }) => {
-    
-  return (
-    <div>SolutionItem</div>
-  )
+
+    return (
+        <ListItem className="comment-display__individual-comment">
+            <Avatar src={solution.echoBoardUser.picture} style={{ marginRight: "15px" }} />
+            <ListItemText
+                primary={
+                    <div>
+                        <Typography variant="body2" color="textSecondary">
+                            {solution.echoBoardUser.name}
+                        </Typography>
+                    </div>
+                }
+                secondary={
+                    <Typography variant="body1" color="textPrimary">
+                        {solution.content}
+                    </Typography>
+                }
+            ></ListItemText>
+            <SolutionStatusButton status={solution.status} solutionId={solution.id} />
+            <UpvoteButton count={solution.upvote.length} onUpvote={() => onUpvote(solution.id)} />
+        </ListItem>
+    )
 }

89235975d882ae856e2ca04de8b1d0a17999b957
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 13:49:12 +0200

    Extracted solutionsList logic

--- client/components/CommentModal/SolutionsLIst.tsx
+++ client/components/CommentModal/SolutionsLIst.tsx
@@ -1,5 +1,7 @@
 import { SolutionResponseData } from '@/service/Types';
+import { List } from '@mui/material';
 import React from 'react'
+import { SolutionItem } from './SolutionItem';
 
 type SolutionsListProps =  {
     solutions: SolutionResponseData[];
@@ -8,6 +10,16 @@ type SolutionsListProps =  {
 
 export const SolutionsList: React.FC<SolutionsListProps> = ({ solutions, onSolutionUpvote }) => {
   return (
-    <div>SolutionsLIst</div>
+    <List>
+    {solutions
+      .sort((a, b) => b.upvote.length - a.upvote.length)
+      .map((solution, index) => (
+        <SolutionItem
+          key={index}
+          solution={solution}
+          onUpvote={() => onSolutionUpvote(solution.id)}
+        />
+      ))}
+  </List>
   )
 }

29f93a4d428340f731bb082d44dfa1d58895f432
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 14:55:14 +0200

    Added props for EchoBoard form

--- client/components/PostEchoBoard/EchoBoardForm.tsx
+++ client/components/PostEchoBoard/EchoBoardForm.tsx
@@ -1,6 +1,26 @@
+import { PostEchoBoardData, UserResponseData } from '@/service/Types'
 import React from 'react'
 
-export const EchoBoardForm = () => {
+type EchoBoardFormProps = {
+  echoBoardPost: PostEchoBoardData;
+  setProblemPost: (post: PostEchoBoardData) => void; 
+  ifAnonymous: boolean; 
+  setIfAnonymous: (anonymous: boolean) => void;
+  handleProblemPost: (event?: React.FormEvent<HTMLFormElement>) => void;
+  handleKeyPress: (event: React.KeyboardEvent<HTMLDivElement>) => void;
+  user: UserResponseData;
+}
+
+export const EchoBoardForm: React.FC<EchoBoardFormProps> = ({
+    echoBoardPost,
+    setProblemPost,
+    ifAnonymous,
+    setIfAnonymous,
+    handleProblemPost,
+    handleKeyPress,
+    user,
+  }
+) => {
   return (
     <div>EchoBoardFrom</div>
   )

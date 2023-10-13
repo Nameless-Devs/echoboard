86c051716817b213d1c2c018b6b1001bb3342db6
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 13:40:32 +0200

    Extracted rendering logic of comment list

--- client/components/CommentModal/CommentList.tsx
+++ client/components/CommentModal/CommentList.tsx
@@ -1,13 +1,26 @@
 import { EchoBoardResponseData } from '@/service/Types';
+import { List } from '@mui/material';
 import React from 'react'
+import { CommentItem } from './CommentItem';
 
-type CommentsListProps {
+type CommentsListProps = {
     comments: EchoBoardResponseData[];
     onCommentUpvote: (commentId: string) => void;
   }
 
-export const CommentList = () => {
+export const CommentList: React.FC<CommentsListProps> = ({ comments, onCommentUpvote }) => {
+    
   return (
-    <div>CommentList</div>
+    <List>
+    {comments
+      .sort((a, b) => b.upvote.length - a.upvote.length)
+      .map((comment, index) => (
+        <CommentItem
+          key={index}
+          comment={comment}
+          onUpvote={() => onCommentUpvote(comment.id)}
+        />
+      ))}
+  </List>
   )
 }

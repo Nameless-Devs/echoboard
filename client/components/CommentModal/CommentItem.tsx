bf72394e2f2ac656e0e219779160922b03824819
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 11:41:10 +0200

    Added elements inside CommentItem

--- client/components/CommentModal/CommentItem.tsx
+++ client/components/CommentModal/CommentItem.tsx
@@ -1,5 +1,7 @@
 import { CommentResponseData } from '@/service/Types'
+import { Avatar, ListItem, ListItemText, Typography } from '@mui/material'
 import React from 'react'
+import UpvoteButton from '../UpvoteButton'
 
 type CommentItemProps = {
 comment: CommentResponseData,
@@ -7,8 +9,23 @@ onUpvote: () => void,
 }
 
 export const CommentItem: React.FC <CommentItemProps> = ({ comment, onUpvote }) => {
-    
+
   return (
-    <div>CommentItem</div>
+      <ListItem className="comment-display__individual-comment">
+      <Avatar src={comment.echoBoardUser.picture} style={{ marginRight: "15px" }} />
+      <ListItemText
+        primary={
+          <Typography variant="body2" color="textSecondary">
+            {comment.echoBoardUser.name}
+          </Typography>
+        }
+        secondary={
+          <Typography variant="body1" color="textPrimary">
+            {comment.content}
+          </Typography>
+        }
+      ></ListItemText>
+      <UpvoteButton count={comment.upvote.length} onUpvote={onUpvote} />
+    </ListItem>
   )
 }

d4c19415a48270f59e7b1ccefc3ba1430ecbfd6e
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 13:48:33 +0200

    Added props for solutins list

--- client/components/CommentModal/SolutionsLIst.tsx
+++ client/components/CommentModal/SolutionsLIst.tsx
@@ -1,6 +1,12 @@
+import { SolutionResponseData } from '@/service/Types';
 import React from 'react'
 
-export const SolutionsLIst = () => {
+type SolutionsListProps =  {
+    solutions: SolutionResponseData[];
+    onSolutionUpvote: (solutionId: string) => void;
+  }
+
+export const SolutionsList: React.FC<SolutionsListProps> = ({ solutions, onSolutionUpvote }) => {
   return (
     <div>SolutionsLIst</div>
   )

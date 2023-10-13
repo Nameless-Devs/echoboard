3577cf31ef2d243ae0eb59b51a2bf96a70766f81
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-13 11:42:13 +0200

    Refactored StyledBadge.tsx in a PostComment folder

---
+++ client/components/PostComment/StyledBadge.tsx
@@ -0,0 +1,11 @@
+import { Badge, styled } from "@mui/material";
+
+export const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    bottom: 0, 
+    right: 0, 
+    transform: "none",
+  },
+}));

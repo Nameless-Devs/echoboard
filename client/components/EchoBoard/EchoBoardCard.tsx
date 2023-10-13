fdc2e7756f6e8ae7e3ae58c12d5b496bc358bd29
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-13 15:49:13 +0200

    Added props for EchoBoardCard

--- client/components/EchoBoard/EchoBoardCard.tsx
+++ client/components/EchoBoard/EchoBoardCard.tsx
@@ -1,6 +1,21 @@
+import { EchoBoardResponseData, UserResponseData } from '@/service/Types'
 import React from 'react'
 
-const EchoBoardCard = () => {
+type EchoBoardCardPros = {
+echoBoard: EchoBoardResponseData;
+user: UserResponseData;
+handleOpen: (echoBoard: EchoBoardResponseData) => void; 
+handleOpenSolutionForm: (echoBoard: EchoBoardResponseData) => void; 
+index: number;
+}
+
+const EchoBoardCard: React.FC<EchoBoardCardPros> = ({ 
+    echoBoard, 
+    user, 
+    handleOpen, 
+    handleOpenSolutionForm, 
+    index 
+}) => {
   return (
     <div>EchoBoardCard</div>
   )

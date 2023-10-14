fc896180703eb063d287f5bc99b4c3a0309b4219
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-14 20:57:16 +0200

    refactored to send created object with response

--- src/main/java/se/salt/echoboard/controller/CommentController.java
+++ src/main/java/se/salt/echoboard/controller/CommentController.java
@@ -1,15 +1,15 @@
 package se.salt.echoboard.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import se.salt.echoboard.model.EchoBoardComment;
 import se.salt.echoboard.service.EchoBoardService;
 
-import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/comments")
@@ -19,28 +19,23 @@ public class CommentController {
     private final EchoBoardService echoService;
 
     @GetMapping("{commentId}")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<EchoBoardComment> getCommentById(@PathVariable long commentId) {
         return ResponseEntity.of(echoService.getCommentById(commentId));
     }
 
     @PatchMapping("{commentId}/upvote")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<Integer> upvoteComment(@PathVariable long commentId, @AuthenticationPrincipal OidcUser user) {
         return ResponseEntity.of(echoService.upvoteComment(commentId, user.getSubject()));
     }
 
+
     @PostMapping("{commentId}")
-    public ResponseEntity<Void> addCommentToEchoBoardComment(@PathVariable long commentId,
-                                                      @RequestBody EchoBoardComment echoBoardComment,
-                                                      @AuthenticationPrincipal OidcUser user) {
-
-        var id = echoService.addCommentToComment(commentId, echoBoardComment, user.getSubject()).getId();
-
-        URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/api/comments")
-                .path("/{id}")
-                .buildAndExpand(id)
-                .toUri();
-        return ResponseEntity.created(location).build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public Optional<EchoBoardComment> addCommentToEchoBoardComment(@PathVariable long commentId,
+                                                                   @RequestBody EchoBoardComment echoBoardComment,
+                                                                   @AuthenticationPrincipal OidcUser user) {
+        return echoService.addCommentToComment(commentId, echoBoardComment, user.getSubject());
     }
-
 }

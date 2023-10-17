e2c99bfb05dbace82b6c0b054f8c671f44685211
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-17 14:20:25 +0200

    renamed to industry standard

--- src/test/java/se/salt/echoboard/controller/CommentControllerTest.java
+++ src/test/java/se/salt/echoboard/controller/CommentControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import se.salt.echoboard.exception.GlobalExceptionHandler;
+import se.salt.echoboard.exception.GeneralExceptionHandler;
 import se.salt.echoboard.service.EchoBoardService;
 import util.mock.AuthenticationPrincipalResolver;
 import util.mock.WithMockOidcUser;

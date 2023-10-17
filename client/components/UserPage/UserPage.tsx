be5b2d96b5e4c395c16c8899b888ef84eeaa3f80
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-17 15:38:42 +0200

    Added chip elements to show case some statistics

--- client/components/UserPage/UserPage.tsx
+++ client/components/UserPage/UserPage.tsx
@@ -1,5 +1,5 @@
 import { UserResponseData } from '@/service/Types'
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, Chip, Typography } from '@mui/material';
 import React from 'react'
 
 type UserPageProps = {
@@ -8,11 +8,24 @@ type UserPageProps = {
 export const UserPage: React.FC<UserPageProps> = ({ user }) => {
     return (
         <>
-            <Box>
-                <Typography variant="h3" color="textPrimary">
-                    {user.name}
-                </Typography>
-                <Avatar src={user.picture} style={{ }} />
+            <Box sx={{ display: "flex" }}>
+                <Box>
+                    <Typography variant="h3" color="textPrimary">
+                        {user.name}
+                    </Typography>
+                    <Chip label={"Problems solved: " + user.name.length} color="success" /> 
+                    <Chip label="Volunteered: 5" color="warning" />
+                    {/* number of solved problems and volunteered are just placeholder for now,
+                    real funtionality will be implemented later on */}
+
+                </Box>
+                <Avatar
+                    alt={user.name + "avatar"}
+                    src={user.picture}
+                    style={{
+                        width: "150px",
+                        height: "150px"
+                    }} />
             </Box>
         </>
     )

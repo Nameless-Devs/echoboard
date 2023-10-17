d97614aa85f61a8ccbc6f72a661970249175c3e0
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-17 11:58:46 +0200

    Made A chat room entity

---
+++ src/main/java/se/salt/echoboard/model/ChatRoom.java
@@ -0,0 +1,27 @@
+package se.salt.echoboard.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "chat_rooms")
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "problem_id", nullable = false)
+    private Long problemId;
+
+    @Column(name = "solution_id", nullable = false)
+    private Long solutionId;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Message> messages = new ArrayList<>();
+}
--- src/main/java/se/salt/echoboard/websocket/WebSocketEventListener.java
+++ src/main/java/se/salt/echoboard/websocket/WebSocketEventListener.java
@@ -1,31 +1,31 @@
-package se.salt.echoboard.websocket;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import se.salt.echoboard.model.ChatMessage;
-import se.salt.echoboard.model.MessageType;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class WebSocketEventListener {
-
-    private final SimpMessageSendingOperations messageSendingOperations;
-
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String userName = (String) headerAccessor.getSessionAttributes().get("username");
-
-        if (userName != null) {
-            log.info("user disconnected {}", userName);
-            var chatMessage = ChatMessage.builder().type(MessageType.LEAVER)
-                    .sender(userName)
-                    .build();
-            messageSendingOperations.convertAndSend("/topic/public", chatMessage);
-        }
-    }
-}
+//package se.salt.echoboard.websocket;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.messaging.simp.SimpMessageSendingOperations;
+//import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+//import se.salt.echoboard.model.ChatRoom;
+//import se.salt.echoboard.model.Message;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class WebSocketEventListener {
+//
+//    private final SimpMessageSendingOperations messageSendingOperations;
+//
+//    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+//        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+//        String userName = (String) headerAccessor.getSessionAttributes().get("username");
+//
+//        if (userName != null) {
+//            log.info("user disconnected {}", userName);
+//            var chatMessage = ChatRoom.builder().type(Message.LEAVER)
+//                    .sender(userName)
+//                    .build();
+//            messageSendingOperations.convertAndSend("/topic/public", chatMessage);
+//        }
+//    }
+//}

74cca0f1cc09d3b67e500b802f3765e5929928de
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-17 16:20:09 +0200

    refactored code

--- src/main/java/se/salt/echoboard/exception/custom/SolutionNotFoundException.java
+++ src/main/java/se/salt/echoboard/exception/custom/SolutionNotFoundException.java
@@ -1,14 +1,14 @@
 package se.salt.echoboard.exception.custom;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-public class SolutionNotFoundException  extends ResponseStatusException {
+public class SolutionNotFoundException extends ResponseStatusException {
     public SolutionNotFoundException() {
         super(NOT_FOUND);
     }
+
     public SolutionNotFoundException(long detail) {
         super(NOT_FOUND, "Solution with id %s not found".formatted(detail));
     }

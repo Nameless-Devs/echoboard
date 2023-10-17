d50c290cf67d0c1216c73c1eef9c300460c116af
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-17 11:08:14 +0200

    Added rendering logic and types inside of VolunteeringModal component

--- client/components/CommentModal/commentModalComponents/VolunteeringModal.tsx
+++ client/components/CommentModal/commentModalComponents/VolunteeringModal.tsx
@@ -1,7 +1,37 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import React from 'react'
 
-export const VolunteeringModal = () => {
+type VolunteeringModalProps = {
+  isConfirmationModalOpen: boolean;
+  setIsConfirmationModalOpen: (isOpen: boolean) => void;
+  setVolunteerConfirmed: (isConfirmed: boolean) => void;
+ } 
+
+export const VolunteeringModal: React.FC<VolunteeringModalProps> = ({
+  isConfirmationModalOpen,
+  setIsConfirmationModalOpen,
+  setVolunteerConfirmed,
+}) => {
   return (
-    <div>VolunteeringModal</div>
+    <Dialog open={isConfirmationModalOpen} onClose={() => setIsConfirmationModalOpen(false)}>
+    <DialogTitle>Confirm Volunteer Action</DialogTitle>
+    <DialogContent>
+        <DialogContentText>
+            Are you sure you want to volunteer for solution testing?
+        </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+        <Button onClick={() => setIsConfirmationModalOpen(false)}>Cancel</Button>
+        <Button
+            onClick={() => {
+                setIsConfirmationModalOpen(false);
+                setVolunteerConfirmed(true);
+            }}
+            color="primary"
+        >
+            Confirm
+        </Button>
+    </DialogActions>
+</Dialog>
   )
 }

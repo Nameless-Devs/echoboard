9cd804d48dea05fd8c880dff72e79d6baeafc369
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-17 12:01:15 +0200

    commented out for now

--- src/main/java/se/salt/echoboard/controller/ChatController.java
+++ src/main/java/se/salt/echoboard/controller/ChatController.java
@@ -1,26 +1,26 @@
-package se.salt.echoboard.controller;
-
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.stereotype.Controller;
-import se.salt.echoboard.model.ChatMessage;
-
-@Controller
-public class ChatController {
-
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        return chatMessage;
-    }
-
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        return chatMessage;
-    }
-
-}
+//package se.salt.echoboard.controller;
+//
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+//import org.springframework.stereotype.Controller;
+//import se.salt.echoboard.model.ChatRoom;
+//
+//@Controller
+//public class ChatController {
+//
+//    @MessageMapping("/chat.sendMessage")
+//    @SendTo("/topic/public")
+//    public ChatRoom sendMessage(@Payload ChatRoom chatRoom) {
+//        return chatRoom;
+//    }
+//
+//    @MessageMapping("/chat.sendMessage")
+//    @SendTo("/topic/public")
+//    public ChatRoom addUser(@Payload ChatRoom chatRoom, SimpMessageHeaderAccessor headerAccessor){
+//        headerAccessor.getSessionAttributes().put("username", chatRoom.getSender());
+//        return chatRoom;
+//    }
+//
+//}

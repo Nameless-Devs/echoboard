cf1831f97f144386e530fa55775bbb1096943e97
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-17 15:20:10 +0200

    Added name and avatar randering to UserPage component

--- client/components/UserPage/UserPage.tsx
+++ client/components/UserPage/UserPage.tsx
@@ -1,11 +1,19 @@
 import { UserResponseData } from '@/service/Types'
+import { Avatar, Box, Typography } from '@mui/material';
 import React from 'react'
 
 type UserPageProps = {
     user: UserResponseData;
 }
-export const UserPage: React.FC<UserPageProps> = ({user}) => {
-  return (
-    <div>UserPage</div>
-  )
+export const UserPage: React.FC<UserPageProps> = ({ user }) => {
+    return (
+        <>
+            <Box>
+                <Typography variant="h3" color="textPrimary">
+                    {user.name}
+                </Typography>
+                <Avatar src={user.picture} style={{ }} />
+            </Box>
+        </>
+    )
 }

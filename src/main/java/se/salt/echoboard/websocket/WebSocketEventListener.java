ae03f586dec491422c4792ace542552eeb2cbc56
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-20 15:11:24 +0200

    Implemented WebSocketEventListener

--- src/main/java/se/salt/echoboard/websocket/WebSocketEventListener.java
+++ src/main/java/se/salt/echoboard/websocket/WebSocketEventListener.java
@@ -1,31 +1,37 @@
-//package se.salt.echoboard.websocket;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.messaging.simp.SimpMessageSendingOperations;
-//import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-//import se.salt.echoboard.model.ChatRoom;
-//import se.salt.echoboard.model.Message;
-//
-//@Component
-//@RequiredArgsConstructor
-//@Slf4j
-//public class WebSocketEventListener {
-//
-//    private final SimpMessageSendingOperations messageSendingOperations;
-//
-//    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-//        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-//        String userName = (String) headerAccessor.getSessionAttributes().get("username");
-//
-//        if (userName != null) {
-//            log.info("user disconnected {}", userName);
-//            var chatMessage = ChatRoom.builder().type(Message.LEAVER)
-//                    .sender(userName)
-//                    .build();
-//            messageSendingOperations.convertAndSend("/topic/public", chatMessage);
-//        }
-//    }
-//}
+package se.salt.echoboard.websocket;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import se.salt.echoboard.model.ChatRoom;
+import se.salt.echoboard.model.Message;
+
+import java.security.Principal;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageSendingOperations;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        String sessionId = event.getMessage().getHeaders().get("simpSessionId", String.class);
+        Principal principal = event.getUser();
+        log.info("Session connect " + sessionId + ": " + principal);
+//        sessions.put(sessionId, principal);
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        log.info("User Disconnected!");
+    }
+}

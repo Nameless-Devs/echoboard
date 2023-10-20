2c4275378c3ac44a9c6672c735d0e0d85a0e3d0e
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-20 10:35:00 +0200

    Added a getters and setters

--- src/main/java/se/salt/echoboard/model/Message.java
+++ src/main/java/se/salt/echoboard/model/Message.java
@@ -1,10 +1,14 @@
 package se.salt.echoboard.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "messages")
 public class Message {
     @Id

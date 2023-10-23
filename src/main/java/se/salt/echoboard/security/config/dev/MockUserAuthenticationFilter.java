8265d29a6b44b0dbc818efb0d94df786683fe571
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-23 12:01:27 +0200

    updated to use jwt cookie

--- src/main/java/se/salt/echoboard/security/config/dev/MockUserAuthenticationFilter.java
+++ src/main/java/se/salt/echoboard/security/config/dev/MockUserAuthenticationFilter.java
@@ -1,27 +1,41 @@
 package se.salt.echoboard.security.config.dev;
 
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTParser;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import se.salt.echoboard.service.EchoBoardService;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static se.salt.echoboard.security.config.JwtValidation.*;
+
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 @Profile({"dev", "test"})
 public class MockUserAuthenticationFilter extends OncePerRequestFilter implements MockUserSecurityContextFactory{
 
     private final EchoBoardService service;
+    private JWTParser parser;
 
     @Value("${frontend-details.base-url}")
     private String baseUrl;
@@ -31,17 +45,42 @@ public class MockUserAuthenticationFilter extends OncePerRequestFilter implement
                                     @NonNull HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        DefaultOidcUser mockUser = createMockUser(request.getSession().getId());
-        SecurityContextHolder.setContext(setMockUserInSecurityContext(mockUser));
-        createUserIfTheyDoNotExist(mockUser);
-        response.setHeader("Access-Control-Allow-Origin", baseUrl);
+        var jwtTokenString = getJwtTokenFromRequestCookie(request);
+
+        if (jwtTokenString.isPresent()) {
+            log.info("Received JWT token: {}", jwtTokenString.get());
+            JWT jwt;
+            try {
+                jwt =  JWTParser.parse(jwtTokenString.get());
+                OidcUser user = createOidcUserFromJwt(jwt);
+                SecurityContextHolder.setContext(setMockUserInSecurityContext(user));
+                log.info(String.valueOf(jwt));
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+
+        } else {
+            log.info("JWT not received");
+            var user = createMockUser();
+            service.createUser(user);
+            response.addCookie(createNewCookie(user.getIdToken().getTokenValue()));
+        }
         filterChain.doFilter(request, response);
     }
+    private Cookie createNewCookie(String tokenValue) {
+        Cookie cookie = new Cookie("JwtToken", tokenValue);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(3500);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setDomain("localhost");
+        return cookie;
+    }
 
-    private void createUserIfTheyDoNotExist(DefaultOidcUser oidcUser) {
-        if (service.getUserBySubject(oidcUser.getSubject()).isEmpty()) {
-            service.createUser(oidcUser);
-        }
+    private OidcUser createOidcUserFromJwt(JWT jwt) throws ParseException {
+        OidcIdToken oidcIdToken =
+                new OidcIdToken(Arrays.toString(jwt.getParsedParts()), null, null, jwt.getJWTClaimsSet().getClaims());
+        return new DefaultOidcUser(Collections.emptyList(), oidcIdToken);
     }
 
 }

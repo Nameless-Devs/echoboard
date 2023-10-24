884fc66b172b61bc7cf76f7f6bee6e3f5f27e1c8
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-24 16:09:59 +0200

    Added contect info to footer

--- client/app/home/Footer.tsx
+++ client/app/home/Footer.tsx
@@ -2,14 +2,13 @@ import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import { Box, Icon } from "@mui/material";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-interface FooterProps {
-  description: string;
-  title: string;
-}
 
 export default function Footer() {
-  
+
 
   return (
     <Grid
@@ -19,10 +18,43 @@ export default function Footer() {
       style={{ position: "fixed", bottom: 0, width: "100%" }}
     >
       <Paper elevation={0} sx={{ p: 2, bgcolor: "#6c9bd3" }}>
-        <Typography variant="h6" gutterBottom>
-          title
-        </Typography>
-        <Typography>description</Typography>
+        <Box>
+          <Box></Box>
+          <Box>
+            <Typography variant="subtitle1">
+              Nameless_devs
+            </Typography>
+            <Typography variant="body2">Oslo/Stockholm</Typography>
+            <Typography variant="body2">
+              <a href="mailto:nameless.devs.salt@gmail.com"
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                  display: "flex",
+                  alignItems: "center"
+                }}
+              >
+                <MailOutlineIcon style={{ width: "20px" }} />
+                nameless.devs.salt@gmail.com
+              </a>
+            </Typography>
+            <Typography variant="body2">
+              <a href="https://github.com/Nameless-Devs/echoboard"
+                target="_blank"
+                style={{
+                textDecoration: "none",
+                color: "black",
+                display: "flex",
+                alignItems: "center"
+              }}
+              >
+                <GitHubIcon />
+                /echoboard
+              </a>
+            </Typography>
+          </Box>
+          <Box></Box>
+        </Box>
       </Paper>
     </Grid>
   );

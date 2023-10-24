482b273e672d9341fc567d077fe71f5ee54364ad
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-24 15:48:38 +0200

    updated method to use authentication

--- src/main/java/se/salt/echoboard/controller/ChatController.java
+++ src/main/java/se/salt/echoboard/controller/ChatController.java
@@ -6,10 +6,12 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import se.salt.echoboard.model.Message;
 import se.salt.echoboard.service.WebSocketService;
 
+import java.time.Instant;
 import java.util.List;
 
 @Controller
@@ -21,18 +23,17 @@ public class ChatController {
 
     @MessageMapping("/chat/sendMessage")
     @SendTo("/topic/chatrooms")
-    public Message sendMessage(@Payload Message message, @Header(value = "simpUser") Object user) {
+    public Message sendMessage(@Payload Message message, @Header(value = "simpUser") Authentication user) {
         if (message.getContent() == null || message.getContent().isEmpty()) {
             throw new IllegalArgumentException("Message content cannot be null or empty");
         }
-
-
-        log.info(String.valueOf(user));
-
-//        DefaultOidcUser user1 = (DefaultOidcUser) user;
-//        log.info(String.valueOf(user1));
-//        log.info(String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()));
-        return webSocketService.saveMessage(message);
+        log.info("User: "+ user + "sent a message");
+        return webSocketService.saveMessage(Message.builder()
+                .content(message.getContent())
+                .sender(user.getName())
+                .picture(message.getPicture())
+                        .timestamp(Instant.now())
+                .build());
     }
     public List<Message> getChatHistory(Long chatRoomId) {
         return webSocketService.getChatHistory(chatRoomId);

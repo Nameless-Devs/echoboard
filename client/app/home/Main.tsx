28acaf533a7894e9dd25abb19bdf3bd80c47aac3
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-24 16:58:12 +0200

    Added picture

--- client/app/home/Main.tsx
+++ client/app/home/Main.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
+import Example from "../image/Example.png";
+import Image from "next/image";
 
 interface MainFeaturedProp {
   title: string;
@@ -9,18 +11,21 @@ interface MainFeaturedProp {
 
 export default function MainFeatured({ title, description }: MainFeaturedProp) {
   return (
-    <Box>
-      <Box
-        sx={{
-          marginTop: "30px",
-          height: 400,
-          p: { xs: 4, md: 4 },
-          pr: { md: 0 },
-          backgroundColor: "#fff",
-          color: "black",
-          borderRadius: '15px'
-        }}
-      >
+
+    <Box
+      sx={{
+        marginTop: "30px",
+        height: 500,
+        p: { xs: 4, md: 4 },
+        pr: { md: 0 },
+        backgroundColor: "#fff",
+        color: "black",
+        borderRadius: '15px',
+        display: "flex", 
+        flexWrap: "wrap"
+      }}
+    >
+      <Box>
         <Typography
           variant="h2"
         >
@@ -32,8 +37,15 @@ export default function MainFeatured({ title, description }: MainFeaturedProp) {
           {description}
         </Typography>
       </Box>
-      <Box>
-       
+      <Box sx={{alignSelf: "center", margin: "auto"}}>
+          <Image
+            src={Example}
+            alt={"EchoBoard solution screenshot"}
+            width={400}
+            style={{
+              boxShadow: "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+            }}
+          />
       </Box>
     </Box>
   );

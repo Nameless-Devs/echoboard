81ee286d45df3787018a76f83c7ca01ff7e22ca9
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-24 15:42:04 +0200

    refactored code

--- src/main/java/se/salt/echoboard/websocket/WebSocketConfig.java
+++ src/main/java/se/salt/echoboard/websocket/WebSocketConfig.java
@@ -1,38 +1,14 @@
 package se.salt.echoboard.websocket;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
-import org.ietf.jgss.Oid;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-import org.springframework.web.util.WebUtils;
-
-import java.security.Principal;
-import java.util.List;
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@Slf4j
+//@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -44,56 +20,56 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/w")
+//                .addInterceptors(httpSessionHandshakeInterceptor())
                 .setAllowedOrigins("http://localhost:3000");
+
     }
 
-    //TODO make this more readable
-    @Bean
-    public HandshakeInterceptor httpSessionHandshakeInterceptor() {
-        return new HandshakeInterceptor() {
-            @Override
-            public boolean beforeHandshake(@NonNull ServerHttpRequest request,
-                                           @NonNull ServerHttpResponse response,
-                                           @NonNull WebSocketHandler wsHandler,
-                                           @NonNull Map<String, Object> attributes) {
-                if (request instanceof ServletServerHttpRequest servletServerRequest) {
-                    HttpServletRequest servletRequest = servletServerRequest.getServletRequest();
-                    Cookie token = WebUtils.getCookie(servletRequest, "JwtToken");
-                    log.info("Cookie with value: " + token);
-                    assert token != null;
-                    attributes.put("JwtToken", token.getValue());
-                }
-                return true;
-            }
-            @Override
-            public void afterHandshake(ServerHttpRequest request,
-                                       ServerHttpResponse response,
-                                       WebSocketHandler wsHandler,
-                                       Exception exception) {
-            }
-        };
-    }
-
-    //TODO make this more readable
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(
-        new ChannelInterceptor() {
-            @Override
-            public Message<?> preSend(Message<?> message, MessageChannel channel) {
-                StompHeaderAccessor accessor =
-                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-//                    Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
-//                    List<String> authorization = accessor.getNativeHeader("Authorization");
-                    Principal user = (Principal) accessor.getHeader("simpUser");
-                    accessor.setUser( user);
-                    log.info(String.valueOf(user));
-                }
-
-                return message;
-            }
-        });
-    }
+//    @Bean
+//    public HandshakeInterceptor httpSessionHandshakeInterceptor() {
+//        return new HandshakeInterceptor() {
+//            @Override
+//            public boolean beforeHandshake(@NonNull ServerHttpRequest request,
+//                                           @NonNull ServerHttpResponse response,
+//                                           @NonNull WebSocketHandler wsHandler,
+//                                           @NonNull Map<String, Object> attributes) {
+//                log.info(String.valueOf(request.getPrincipal()));
+//                if (request instanceof ServletServerHttpRequest servletServerRequest) {
+//                    HttpServletRequest servletRequest = servletServerRequest.getServletRequest();
+//                    Cookie token = WebUtils.getCookie(servletRequest, "JwtToken");
+//                    assert token != null;
+//                    log.info("Cookie with value in Http Session Handshake: " + token.getValue());
+//                    attributes.put("JwtToken", token.getValue());
+//                    SecurityContextHolder.getContext().setAuthentication((Authentication) request.getPrincipal());
+//                }
+//                return true;
+//            }
+//            @Override
+//            public void afterHandshake(ServerHttpRequest request,
+//                                       ServerHttpResponse response,
+//                                       WebSocketHandler wsHandler,
+//                                       Exception exception) {
+//            }
+//        };
+//    }
+//
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(
+//        new ChannelInterceptor() {
+//            @Override
+//            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+//                StompHeaderAccessor accessor =
+//                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+////                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+//                    log.info(String.valueOf(accessor.getUser()));
+////                }
+////                System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+//                SecurityContextHolder.getContext().setAuthentication((Authentication) accessor.getUser());
+//
+//                return message;
+//            }
+//        });
+//    }
 
 }

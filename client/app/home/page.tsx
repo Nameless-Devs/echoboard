f0d037f2fb4fc7be27833d22e20833785b48e0e6
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-25 14:03:19 +0200

    Deleted old landing page

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,37 +1,11 @@
 import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
-import Header from "./Header";
-import Footer from "./Footer";
-import MainFeatured from "./Main";
 
-const echoBoardHeader = {
-  title: "EchoBoard",
-};
 
-const mainFeature = {
-  title: "EchoBoard",
-  description: "Where Your Voice Amplifies Change!",
-};
 
 const home = () => {
   return (
-    
-    <main style={{ minHeight: "100vh", backgroundColor: "#4581C7" }}>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <Header title={echoBoardHeader.title} />
-        <Grid>
-          <MainFeatured
-            title={mainFeature.title}
-            description={mainFeature.description}
-          />
-        </Grid>
-      </Container>
-      <Footer
-      />
-    </main>
+    <>
+    </>
   );
 };
 

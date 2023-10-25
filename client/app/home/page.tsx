85434056096f3a53dc1c19aabc6efc081080d08c
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-25 15:59:44 +0200

    Added background picture to the landing page

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,10 +1,15 @@
+import { Box } from "@mui/material";
 import * as React from "react";
+import "../styles/LandingPage.css"
 
 
 
 const home = () => {
   return (
     <>
+    <Box className="main__background-picture"> 
+hi
+    </Box>
     </>
   );
 };

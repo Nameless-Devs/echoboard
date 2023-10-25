5e47aa59d165aec8bddd58c4a8528fcce757b35d
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-25 16:34:59 +0200

    Added logo and name to the nav bar, fixed opacity issue

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,21 +1,42 @@
-import { AppBar, Box, Container, Toolbar } from "@mui/material";
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
 import * as React from "react";
 import "../styles/LandingPage.css"
-
+import EchoLogoWhite from "../image/EchoBoard_logo_white.png"
+import Image from "next/image";
 
 
 const home = () => {
   return (
     <>
-    <Box className="main__background-picture"> 
-     <AppBar sx={{backgroundColor: "black", opacity: "30%"}}>
-     <Container maxWidth="xl">
-     <Toolbar disableGutters></Toolbar>
+      <Box className="main__background-picture">
+        <AppBar sx={{ backgroundColor: 'rgba(0, 0, 0, 0.3)'}} >
+          <Container maxWidth="xl" >
+            <Toolbar disableGutters >
+            <Image src={EchoLogoWhite} alt="EchoBoard logo white" width={40} />
+            <Typography
+            variant="h5"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 800,
+              letterSpacing: '.3rem',
+              color: 'white',
+              textDecoration: 'none',
+              opacity: 1,
+              marginLeft: "20px"
+            }}
+          >
+            EchoBoard
+          </Typography>
 
-     </Container>
-     </AppBar>
 
-    </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+
+      </Box>
     </>
   );
 };

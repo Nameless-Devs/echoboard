86f2554feca5763faf16c63dceb24efceb0c88b7
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-26 20:42:52 +0200

    refactored method

--- src/main/java/se/salt/echoboard/controller/UserController.java
+++ src/main/java/se/salt/echoboard/controller/UserController.java
@@ -1,21 +1,15 @@
 package se.salt.echoboard.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import se.salt.echoboard.controller.dto.EchoBoardDTO;
-import se.salt.echoboard.model.EchoBoardUser;
+import se.salt.echoboard.controller.dto.EchoBoardUserInfo;
 import se.salt.echoboard.controller.dto.EchoBoardUserResponse;
-import se.salt.echoboard.model.EchoBoardUser;
 import se.salt.echoboard.service.EchoBoardService;
-
-import java.util.List;
-import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -34,9 +28,9 @@ public class UserController {
     }
 
     @GetMapping("/echoboard")
-    public ResponseEntity<EchoBoardDTO> getEchoBoardWithCommentsAndSolutions (@AuthenticationPrincipal OidcUser user) {
-        var echoBoardUser = echoBoardService.getEchoBoardUserWithCommentsAndSolutions(user.getSubject());
-        return ResponseEntity.ok(echoBoardUser);
+    @ResponseStatus(OK)
+    public EchoBoardUserInfo getEchoBoardWithCommentsAndSolutions (@AuthenticationPrincipal OidcUser user) {
+        return echoBoardService.getEchoBoardUserWithCommentsAndSolutions(user.getSubject());
     }
 
 }

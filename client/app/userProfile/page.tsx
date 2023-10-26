8aac2ec216cc91001595a00e67bb15dc80a18c2c
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-26 09:21:59 +0200

    implementing user page layout

--- client/app/userProfile/page.tsx
+++ client/app/userProfile/page.tsx
@@ -1,12 +1,21 @@
-"use client"
-import { UserPage } from '@/components/UserPage/UserPage';
-import { getUserInfo } from '@/service/Functions';
-import { useQuery } from '@tanstack/react-query';
-import React from 'react'
+"use client";
+import { UserPage } from "@/components/UserPage/UserPage";
+import { getUserInfo } from "@/service/Functions";
+import {
+  AppBar,
+  Button,
+  Grid,
+  Paper,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import Box from "@mui/material/Box";
+import React from "react";
 
 export default function UserProfile() {
-
-  const { data: user, error, isLoading } = useQuery(['userInfo'], getUserInfo)
+  const { data: user, error, isLoading } = useQuery(["userInfo"], getUserInfo);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -18,15 +27,25 @@ export default function UserProfile() {
 
   if (user) {
     return (
-      <div>
-        <UserPage user={user} />
-      </div>
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+        {/* Nav Bar */}
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Logo
+            </Typography>
+            <Stack direction="row" spacing={2}>
+              <Button color="inherit">Home</Button>
+              <Button color="inherit">Chat</Button>
+            </Stack>
+          </Toolbar>
+        </AppBar>
+
+        {/* Main */}
+        <Box sx={{ flexGrow: 1, backgroundColor: "#FAF9F6", overflow: "auto" }}>
+          <UserPage user={user} />
+        </Box>
+      </Box>
     );
-
   }
-};
-
-
-
-
-
+}

abfc6cdafb4f8ac8d682c7d8351816efc9959e40
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-27 10:58:26 +0200

    Added grid component

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,8 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Typography} from "@mui/material";
 import * as React from "react";
 import "../styles/LandingPage.css"
 import NavBar from "./NavBar";
 import Main from "./Main";
+import Grid from '@mui/material/Grid';
 
 const pages = ['Home', 'About', 'Features', 'Contact'];
 const home = () => {
@@ -10,10 +11,32 @@ const home = () => {
     <>
       <Box className="main__background-picture">
         <NavBar />
-        <Main title="EchoBoard" description="Where Your Voice Amplifies Change!"/>
+        <Main title="EchoBoard" description="Where Your Voice Amplifies Change!" />
       </Box>
-      <Box> 
-        Something here
+      <Box>
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: "center",
+            mt: "2rem",
+            fontSize: { md: "2.5rem" }
+          }}
+        >Features
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <Typography>xs=6 md=8</Typography>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Typography>xs=6 md=4</Typography>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Typography>xs=6 md=4</Typography>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Typography>xs=6 md=8</Typography>
+          </Grid>
+        </Grid>
       </Box>
     </>
   );

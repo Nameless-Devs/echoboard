3e6f8d0a1d860903d6335027a7649649bd22fd16
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-27 12:33:20 +0200

    Added second feature box in grid

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -5,6 +5,7 @@ import NavBar from "./NavBar";
 import Main from "./Main";
 import Grid from '@mui/material/Grid';
 import AddCommentIcon from '@mui/icons-material/AddComment';
+import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
 
 const pages = ['Home', 'About', 'Features', 'Contact'];
 const home = () => {
@@ -25,6 +26,7 @@ const home = () => {
           }}
         >Features
         </Typography>
+
         <Box sx={{
           display: "flex",
           m: { xs: "10px", md: "auto" },
@@ -32,18 +34,24 @@ const home = () => {
           maxWidth: { md: "1100px" },
         }}>
           <Grid container spacing={1}>
+
+
             <Grid sx={{ border: "black 2px solid" }} item xs={12} md={6}>
               <Box sx={{ 
+                textAlign: { xs: "left", md: "right"},
                 display: "flex", 
                 padding: "1rem", 
-                // border: "1px solid black",
                 m: { xs: "0.5rem" , md: "1rem 2rem 1rem 0"},
                 borderRadius: "10px", 
                 backgroundColor: "#f9e8dd",
                 boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
-                // boxShadow: "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
                 }}>
-                <AddCommentIcon sx={{ fontSize: {xs: "3rem", md: "3.5rem"}, mr: "10px" }} />
+                <AddCommentIcon 
+                sx={{ 
+                  fontSize: "3rem",
+                  mr: "10px",
+                  display: {xs: "block", md: "none"}
+                   }} />
                 <Box>
                   <Typography variant="h5" sx={{ fontSize: {md: "2rem"}}}>
                     Make posts
@@ -52,17 +60,46 @@ const home = () => {
                     Share your problem with others and ask for help
                     </Typography>
                 </Box>
+                <AddCommentIcon 
+                sx={{ 
+                  fontSize: "4rem",
+                  ml: "10px",
+                  display: {xs: "none", md: "block"},
+                   }} />
               </Box>
             </Grid>
+
+
             <Grid sx={{ border: "black 2px solid", display: { xs: "none", md: "block" } }} item xs={0} md={6}>
               <Typography></Typography>
             </Grid>
             <Grid sx={{ border: "black 2px solid", display: { xs: "none", md: "block" } }} item xs={0} md={6}>
               <Typography></Typography>
             </Grid>
+
+
             <Grid sx={{ border: "black 2px solid" }} item xs={12} md={6}>
-              <Typography>Content</Typography>
+            <Box sx={{ 
+                display: "flex", 
+                padding: "1rem", 
+                m: { xs: "0.5rem" , md: "1rem 0.5rem 1rem 1.5rem"},
+                borderRadius: "10px", 
+                backgroundColor: "#f9e8dd",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+                }}>
+                 <PsychologyAltIcon sx={{ fontSize: {xs: "3.5rem", md: "4rem"}, mr: "10px" }} />
+                <Box>
+                  <Typography variant="h5" sx={{ fontSize: {md: "2rem"}}}>
+                    Anonymity
+                    </Typography>
+                  <Typography variant="body1" sx={{ fontSize: {md: "1.2rem"}}}>
+                    Choose to share your struggles anonymously if you wish not to disclose your identity
+                    </Typography>
+                </Box>
+              </Box>
             </Grid>
+
+
             <Grid sx={{ border: "black 2px solid" }} item xs={12} md={6}>
               <Typography>Content</Typography>
             </Grid>

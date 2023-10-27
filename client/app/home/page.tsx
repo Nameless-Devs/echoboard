6941c58065c49612b44be5474f0373d6faf7f4b9
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-27 13:57:49 +0200

    Added the rest of feateres

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -7,6 +7,10 @@ import Grid from '@mui/material/Grid';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import RecommendIcon from '@mui/icons-material/Recommend';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import ForumIcon from '@mui/icons-material/Forum';
+import HistoryIcon from '@mui/icons-material/History';
 
 const pages = ['Home', 'About', 'Features', 'Contact'];
 const home = () => {
@@ -141,23 +145,123 @@ const home = () => {
             <Grid sx={{ border: "black 2px solid", display: { xs: "none", md: "block" } }} item xs={0} md={6}>
               <Typography></Typography>
             </Grid>
+          
             <Grid sx={{ border: "black 2px solid" }} item xs={12} md={6}>
-              <Typography>Content</Typography>
+            <Box sx={{ 
+                display: "flex", 
+                padding: "1rem", 
+                m: { xs: "0.5rem" , md: "1rem 0.5rem 1rem 1.5rem"},
+                borderRadius: "10px", 
+                backgroundColor: "#f9e8dd",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+                }}>
+                 <RecommendIcon sx={{ fontSize: {xs: "3rem", md: "4rem"}, mr: "10px" }} />
+                <Box>
+                  <Typography variant="h5" sx={{ fontSize: {md: "2rem"}}}>
+                    React
+                    </Typography>
+                  <Typography variant="body1" sx={{ fontSize: {md: "1.2rem"}}}>
+                    Let other know that you care about disscussed issues
+                    </Typography>
+                </Box>
+              </Box>
             </Grid>
+
             <Grid sx={{ border: "black 2px solid" }} item xs={12} md={6}>
-              <Typography>Content</Typography>
+            <Box sx={{ 
+                textAlign: { xs: "left", md: "right"},
+                display: "flex", 
+                padding: "1rem", 
+                m: { xs: "0.5rem" , md: "1rem 2rem 1rem 0"},
+                borderRadius: "10px", 
+                backgroundColor: "#f9e8dd",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+                }}>
+                <VolunteerActivismIcon 
+                sx={{ 
+                  fontSize: "3rem",
+                  mr: "10px",
+                  display: {xs: "block", md: "none"}
+                   }} />
+                <Box>
+                  <Typography variant="h5" sx={{ fontSize: {md: "2rem"}}}>
+                    Volunteer
+                    </Typography>
+                  <Typography variant="body1" sx={{ fontSize: {md: "1.2rem"}}}>
+                    Step forward to help your peers implementing the solutions
+                    </Typography>
+                </Box>
+                <VolunteerActivismIcon  
+                sx={{ 
+                  fontSize: "4rem",
+                  ml: "10px",
+                  display: {xs: "none", md: "block"},
+                   }} />
+              </Box>
             </Grid>
+
+
             <Grid sx={{ border: "black 2px solid", display: { xs: "none", md: "block" } }} item xs={0} md={6}>
               <Typography></Typography>
             </Grid>
             <Grid sx={{ border: "black 2px solid", display: { xs: "none", md: "block" } }} item xs={0} md={6}>
               <Typography></Typography>
             </Grid>
+
+
             <Grid sx={{ border: "black 2px solid" }} item xs={12} md={6}>
-              <Typography>Content</Typography>
+                <Box sx={{ 
+                display: "flex", 
+                padding: "1rem", 
+                m: { xs: "0.5rem" , md: "1rem 0.5rem 1rem 1.5rem"},
+                borderRadius: "10px", 
+                backgroundColor: "#f9e8dd",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+                }}>
+                 <ForumIcon sx={{ fontSize: {xs: "3rem", md: "4rem"}, mr: "10px" }} />
+                <Box>
+                  <Typography variant="h5" sx={{ fontSize: {md: "2rem"}}}>
+                    Communicate 
+                    </Typography>
+                  <Typography variant="body1" sx={{ fontSize: {md: "1.2rem"}}}>
+                    Chat with other volunteers in assigned group chat rooms  
+                    </Typography>
+                </Box>
+              </Box>
             </Grid>
+
+
             <Grid sx={{ border: "black 2px solid" }} item xs={12} md={6}>
-              <Typography>Content</Typography>
+                   <Box sx={{ 
+                textAlign: { xs: "left", md: "right"},
+                display: "flex", 
+                padding: "1rem", 
+                m: { xs: "0.5rem" , md: "1rem 2rem 1rem 0"},
+                borderRadius: "10px", 
+                backgroundColor: "#f9e8dd",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+                }}>
+                <HistoryIcon
+                sx={{ 
+                  fontSize: "3rem",
+                  mr: "10px",
+                  display: {xs: "block", md: "none"}
+                   }} />
+                <Box>
+                  <Typography variant="h5" sx={{ fontSize: {md: "2rem"}}}>
+                    History
+                    </Typography>
+                  <Typography variant="body1" sx={{ fontSize: {md: "1.2rem"}}}>
+                    Access your personal archives thought your profile
+                    </Typography>
+                </Box>
+                <HistoryIcon 
+                sx={{ 
+                  fontSize: "4rem",
+                  ml: "10px",
+                  display: {xs: "none", md: "block"},
+                   }} />
+              </Box>
             </Grid>
           </Grid>
         </Box>

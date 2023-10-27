5420aab9741f6697cb786cacc29ec8fec6b5dc38
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-27 11:50:29 +0200

    removed redundant code

--- src/main/java/se/salt/echoboard/model/ChatRoom.java
+++ src/main/java/se/salt/echoboard/model/ChatRoom.java
@@ -2,7 +2,6 @@ package se.salt.echoboard.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,12 +14,6 @@ public class ChatRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-//    @Column(name = "problem_id", nullable = false)
-//    private Long problemId;
-//
-//    @Column(name = "solution_id", nullable = false)
-//    private Long solutionId;
 
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Message> messages = new ArrayList<>();

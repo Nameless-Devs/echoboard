d0982b405314d8321085cfe7a75afb60ecef1d25
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-27 15:20:30 +0200

    Formated doc

--- client/app/home/Feature.tsx
+++ client/app/home/Feature.tsx
@@ -9,67 +9,65 @@ type FeatureProps = {
 }
 
 export const Feature: React.FC<FeatureProps> = ({
-  icon,
-  title,
-  description,
-  reverseOnDesktop,
+    icon,
+    title,
+    description,
+    reverseOnDesktop,
 }) => {
 
-  return (
+    return (
         <Box
-          sx={{
-            textAlign: { xs: "left", md: "right" },
-            display: "flex",
-            padding: "1rem",
-            m: { xs: "0.5rem", md: reverseOnDesktop ?  "0 1.5rem 0 0" : "0 0 0 1.5rem" },
-            borderRadius: "10px",
-            backgroundColor: "#f9e8dd",
-            boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
-          }}
+            sx={{
+                textAlign: { xs: "left", md: "right" },
+                display: "flex",
+                padding: "1rem",
+                m: { xs: "0.5rem", md: reverseOnDesktop ? "0 1.5rem 0 0" : "0 0 0 1.5rem" },
+                borderRadius: "10px",
+                backgroundColor: "#f9e8dd",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+            }}
         >
-          {reverseOnDesktop && (
-            <>
-             <Box sx={{ display: {xs: "flex", md: "none"} }}  >
-              {icon}
-              <Box ml={"10px"}>
-                <Typography variant="h5" sx={{ fontSize: { md: "2rem" } }}>
-                  {title}
-                </Typography>
-                <Typography variant="body1" sx={{ fontSize: { md: "1.2rem" } }}>
-                  {description}
-                </Typography>
-              </Box>
-              </Box>
+            {reverseOnDesktop && (
+                <>
+                    <Box sx={{ display: { xs: "flex", md: "none" } }}  >
+                        {icon}
+                        <Box ml={"10px"}>
+                            <Typography variant="h5" sx={{ fontSize: { md: "2rem" } }}>
+                                {title}
+                            </Typography>
+                            <Typography variant="body1" sx={{ fontSize: { md: "1.2rem" } }}>
+                                {description}
+                            </Typography>
+                        </Box>
+                    </Box>
 
-              <Box sx={{ display: {xs: "none", md: "flex"} }}>
-              <Box mr={"20px"}>
-                <Typography variant="h5" sx={{ fontSize: { md: "2rem" } }}>
-                  {title}
-                </Typography>
-                <Typography variant="body1" sx={{ fontSize: { md: "1.2rem" } }}>
-                  {description}
-                </Typography>
-              </Box>
-             {icon}
-              </Box>
-
-            </>
-    
-          )}
-          {!reverseOnDesktop && (
-            <>
-              <Box>
-                <Typography variant="h5" sx={{ fontSize: { md: "2rem" } }}>
-                  {title}
-                </Typography>
-                <Typography variant="body1" sx={{ fontSize: { md: "1.2rem" } }}>
-                  {description}
-                </Typography>
-              </Box>
-              {icon}
-            </>
-          )}
+                    <Box sx={{ display: { xs: "none", md: "flex" } }}>
+                        <Box mr={"20px"}>
+                            <Typography variant="h5" sx={{ fontSize: { md: "2rem" } }}>
+                                {title}
+                            </Typography>
+                            <Typography variant="body1" sx={{ fontSize: { md: "1.2rem" } }}>
+                                {description}
+                            </Typography>
+                        </Box>
+                        {icon}
+                    </Box>
+                </>
+            )}
+            {!reverseOnDesktop && (
+                <>
+                    <Box>
+                        <Typography variant="h5" sx={{ fontSize: { md: "2rem" } }}>
+                            {title}
+                        </Typography>
+                        <Typography variant="body1" sx={{ fontSize: { md: "1.2rem" } }}>
+                            {description}
+                        </Typography>
+                    </Box>
+                    {icon}
+                </>
+            )}
         </Box>
-  );
+    );
 };
 

2b6c1a41eb7353b214537df44daa6a05ce168fb1
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-27 11:16:03 +0200

    Added basic responsive grid structure

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import * as React from "react";
 import "../styles/LandingPage.css"
 import NavBar from "./NavBar";
@@ -18,25 +18,60 @@ const home = () => {
           variant="h5"
           sx={{
             textAlign: "center",
-            mt: "2rem",
+            m: "2rem 0",
+
             fontSize: { md: "2.5rem" }
           }}
         >Features
         </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={6} md={8}>
-            <Typography>xs=6 md=8</Typography>
+        <Box sx={{
+          display: "flex", 
+          m: {xs: "10px", md: "auto"}, 
+          alignItems: "center",
+          maxWidth: {md: "1000px"},
+          }}>
+          <Grid container spacing={1}>
+            <Grid sx={{border: "black 2px solid"}} item xs={12} md={6}>
+              <Typography>Content</Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid", display: {xs: "none", md: "block"}}} item xs={0} md={6}>
+              <Typography></Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid", display: {xs: "none", md: "block"}}} item xs={0} md={6}>
+              <Typography></Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid"}} item xs={12} md={6}>
+              <Typography>Content</Typography>
+            </Grid>
+            <Grid sx={{border: "black 2px solid"}} item xs={12} md={6}>
+              <Typography>Content</Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid", display: {xs: "none", md: "block"}}} item xs={0} md={6}>
+              <Typography></Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid", display: {xs: "none", md: "block"}}} item xs={0} md={6}>
+              <Typography></Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid"}} item xs={12} md={6}>
+              <Typography>Content</Typography>
+            </Grid>
+            <Grid sx={{border: "black 2px solid"}} item xs={12} md={6}>
+              <Typography>Content</Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid", display: {xs: "none", md: "block"}}} item xs={0} md={6}>
+              <Typography></Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid", display: {xs: "none", md: "block"}}} item xs={0} md={6}>
+              <Typography></Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid"}} item xs={12} md={6}>
+              <Typography>Content</Typography>
+            </Grid>
+            <Grid  sx={{border: "black 2px solid"}} item xs={12} md={6}>
+              <Typography>Content</Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={6} md={4}>
-            <Typography>xs=6 md=4</Typography>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Typography>xs=6 md=4</Typography>
-          </Grid>
-          <Grid item xs={6} md={8}>
-            <Typography>xs=6 md=8</Typography>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
     </>
   );

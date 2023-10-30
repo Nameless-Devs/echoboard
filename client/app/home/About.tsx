52af93bbbf9b41480d58685731f62b1edf7ee102
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-30 11:11:13 +0100

    Added some gradient as background

--- client/app/home/About.tsx
+++ client/app/home/About.tsx
@@ -1,7 +1,16 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 export const About = () => {
   return (
-    <div>About section </div>
+    <>
+      <Box sx={{
+        height: "50vh",
+        background: "linear-gradient(135deg, hsla(215, 91%, 18%, 1) 2%, hsla(187, 61%, 28%, 1) 100%)"
+      }}>
+ hello 
+      </Box>
+
+    </>
   )
 }

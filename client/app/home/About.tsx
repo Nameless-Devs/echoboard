4ed569470751953af82fe20bd382cb7419fe50a9
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-30 13:05:06 +0100

    Styled about section

--- client/app/home/About.tsx
+++ client/app/home/About.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import React from 'react';
 import PhonePicture from "../image/Phone_example.png";
 import Image from "next/image";
@@ -23,30 +23,56 @@ export const About = () => {
                 </Typography>
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={6}>
-                        <Box>
+                        <Box 
+                        display={'flex'} 
+                        flexDirection={"column"}
+                        sx={{
+                            textAlign: "left",
+                            margin: { xs: "0 1.5rem", md: "0 0 0 3rem"},
+                            alignItems: "center",
+                            justifyContent: "center",
+                            height: "100%"
+                    
+                        }}>
                             <Typography variant='h6'
-                            sx={{color: "white"}}>
-                            EchoBoard is a platform designed to address the challenge of effectively sharing and solving problems within the workplace. 
-                            It aims to bridge the gap between employees and upper management by providing a platform for employees to voice their concerns, 
-                            propose solutions, and collaborate on addressing issues that impact the organisation.
+                                sx={{ 
+                                    color: "white",
+                        
+                                     }}>
+                                EchoBoard is a platform designed to address the challenge of effectively sharing and solving problems within the workplace.
                             </Typography>
+                            <Typography variant='h6'
+                                sx={{ 
+                                    color: "white",
+                                    marginTop: "1.5rem",
+                                     }}>
+                                It aims to bridge the gap between employees and upper management by providing a platform for employees to voice their concerns,
+                                propose solutions, and collaborate on addressing issues that impact the organisation.
+                            </Typography>
+                            <Button component="label" variant="contained" sx={{margin: "2rem"}}>
+                                Watch video
+                            </Button>
                         </Box>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <Box display={{ xs: "none", md: "block" }}>
-                            <Image 
-                            src={PhonePicture} 
-                            alt="Picture of mobile phone with EchoBoard application"
-                            // layout="responsive" 
-                            width={250}
+                        <Box display={{ xs: "none", md: "flex" }} justifyContent={"center"}>
+                            <Image
+                                src={PhonePicture}
+                                alt="Picture of mobile phone with EchoBoard application"
+                                width={250}
+                                style={{
+                                    marginBottom: "5rem"
+                                }}
                             />
                         </Box>
-                        <Box display={{ xs: "block", md: "none" }}>
-                            <Image 
-                            src={PhonePicture} 
-                            alt="Picture of mobile phone with EchoBoard application"
-                            // layout="responsive" 
-                            width={200}
+                        <Box display={{ xs: "flex", md: "none" }} justifyContent={"center"}>
+                            <Image
+                                src={PhonePicture}
+                                alt="Picture of mobile phone with EchoBoard application"
+                                width={200}
+                                style={{
+                                    marginBottom: "3rem"
+                                }}
                             />
                         </Box>
                     </Grid>

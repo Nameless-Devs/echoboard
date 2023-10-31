d4e3ef5df7fe23e7a0904e4094a14fef94d61adf
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-31 18:10:57 +0100

    optimized imports

--- src/main/java/se/salt/echoboard/security/config/deploy/CustomAuthenticationSuccessHandler.java
+++ src/main/java/se/salt/echoboard/security/config/deploy/CustomAuthenticationSuccessHandler.java
@@ -6,13 +6,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 import se.salt.echoboard.security.config.JwtValidation;
 import se.salt.echoboard.service.repository.EchoBoardUserRepository;
 

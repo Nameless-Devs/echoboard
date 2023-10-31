5c5113bd315df78d08cad0920b318ece11ff8b96
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-31 14:27:39 +0100

    Extracted chat history to a new component

---
+++ client/components/Chat/ChatRoomHistory.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Message } from "@/service/Types";
+import { fetchChatRoomHistory } from "@/service/Functions";
+import { ChatMessage } from "./ChatMessage";
+
+interface ChatRoomHistoryProp {
+  chatRoomId: number;
+}
+
+function ChatRoomHistory({ chatRoomId }: ChatRoomHistoryProp) {
+  const { data: chatHistory } = useQuery<Message[]>(
+    ["messages", chatRoomId],
+    async () => {
+      return await fetchChatRoomHistory(chatRoomId);
+    }
+  );
+
+  if (chatHistory) {
+    return (
+      <div>
+        {chatHistory.map((message, index) => (
+          <div key={index}> 
+            <ChatMessage index={index} msg={message}/>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export default ChatRoomHistory

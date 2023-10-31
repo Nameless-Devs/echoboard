4b6159c45a936433bd5abe636d2c3baa6011d573
Ibrahim <Ibrahim@ebs.net.pk>
2023-10-31 18:05:27 +0100

    optimized imports

--- src/main/java/se/salt/echoboard/security/config/CustomLogoutSuccessHandler.java
+++ src/main/java/se/salt/echoboard/security/config/CustomLogoutSuccessHandler.java
@@ -1,9 +1,8 @@
 package se.salt.echoboard.security.config;
 
-import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;

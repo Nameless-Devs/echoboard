74e71ca5aa4ce30db73a508b40a0ba58fb7b69d0
NateAra <nate.arafayne@appliedtechnology.se>
2023-11-03 14:40:44 +0100

    implemented modern ui

--- client/app/chatroom/page.tsx
+++ client/app/chatroom/page.tsx
@@ -7,7 +7,7 @@ import {
   getUserInfo,
 } from "@/service/Functions";
 import { Message } from "@/service/Types";
-import {Button, Grid, Input, ListItemButton, Paper} from "@mui/material";
+import { Button, Grid, Input, ListItemButton, Paper } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import React, { useState, useEffect } from "react";
 import subscribeToUserChatRooms from "@/service/chatRoomService";
@@ -23,15 +23,15 @@ export default function UserChat() {
 
   const { data: user, error, isLoading } = useQuery(["userInfo"], getUserInfo);
 
-  
-
-  const { data: chatHistory } = useQuery<Message[]>(["messages", selectedChatRoomId], async () => {
-    if (selectedChatRoomId) {
-      return await fetchChatRoomHistory(selectedChatRoomId);
+  const { data: chatHistory } = useQuery<Message[]>(
+    ["messages", selectedChatRoomId],
+    async () => {
+      if (selectedChatRoomId) {
+        return await fetchChatRoomHistory(selectedChatRoomId);
+      }
+      return [];
     }
-    return [];
-
-  });
+  );
 
   useEffect(() => {
     const newClient = Stomp.client(WEBSOCKET.BASE_URL);
@@ -42,7 +42,8 @@ export default function UserChat() {
     newClient.onConnect = () => {
       if (chatRooms) {
         subscribeToUserChatRooms(newClient, chatRooms, onMessageReceived);
-      }}
+      }
+    };
     newClient.activate();
     setClient(newClient);
     if (chatHistory) setMessages(chatHistory);
@@ -75,7 +76,7 @@ export default function UserChat() {
         timestamp: new Date(),
       };
       client.publish({
-        destination: "/app/chat/sendMessage/" +selectedChatRoomId,
+        destination: "/app/chat/sendMessage/" + selectedChatRoomId,
         body: JSON.stringify(message),
       });
       setInput("");
@@ -91,51 +92,67 @@ export default function UserChat() {
   };
 
   return (
-      <Grid container style={{ position: 'absolute', height: '100%', width: '100%' }}>
-        {/*Left Grid*/}
-        <Grid item xs={2} sx={{ height: '100%', backgroundColor: "#f1f1f1"}}>
-             {chatRooms?.map((chatroom, index) => (
-                 <ListItemButton
-                     key={index}
-                     onClick={() => handleChatRoomChange(chatroom)}
-                     style={{ borderRadius: "10px", padding: "16px" }}
-                 >
-                   {chatroom}
-                 </ListItemButton>
-             ))}
+    <Grid
+      container
+      style={{ position: "absolute", height: "100%", width: "100%" }}
+    >
+      {/*Left Grid*/}
+      <Grid item xs={2} sx={{ height: "100%", backgroundColor: "#292b2f" }}>
+        {chatRooms?.map((chatroom, index) => (
+          <ListItemButton
+            key={index}
+            onClick={() => handleChatRoomChange(chatroom)}
+            style={{ borderRadius: "10px", padding: "16px", color: "#f1f1f1" }}
+          >
+            {chatroom}
+          </ListItemButton>
+        ))}
+      </Grid>
+      <Grid item xs={10} sx={{ height: "100%", backgroundColor: "#424549" }}>
+        {/*Top Right*/}
+        <Grid item xs={12} sx={{ height: "90%", overflowY: "scroll"}}>
+          {messages.map((msg, index) => (
+            <div key={index}>
+              <ChatMessage index={index} msg={msg} />
+            </div>
+          ))}
         </Grid>
-        <Grid item xs={10} sx={{ height: '100%'}}>
-          {/*Top Right*/}
-          <Grid item xs={12} sx={{ height: '90%', overflowY: "scroll" }}>
-            {messages.map((msg, index) => (
-                <div key={index}>
-                  <ChatMessage index={index} msg={msg} />
-                </div>
-            ))}
-          </Grid>
-          {/*Top Left*/}
-          <Grid item xs={12} sx={{
-            height: '10%',
-            outline: '10px blue',
-            backgroundColor: "#f1f1f1",
-            paddingLeft: "1rem",
-          }}>
-            <div style={{display:"flex",
+        {/*Top Left*/}
+        <Grid
+          item
+          xs={12}
+          sx={{
+            height: "10%",
+            outline: "10px blue",
+            backgroundColor: "#424549",
+            padding: "1rem",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
               flexDirection: "row",
-              justifyContent:"space-between",
-              paddingTop: "2rem",
-              paddingRight: "1rem"}}>
+              justifyContent: "space-between",
+              padding: "1rem",
+              backgroundColor: "#4a4c51",
+              borderRadius: "5px"
+            }}
+          >
             <Input
-                type="text"
-                placeholder="Enter a message"
-                value={input}
-                disableUnderline={true}
-                onChange={handleMessageInput}
+              sx={{
+                width: "80%",
+                color: "#f1f1f1"
+              }}
+              type="text"
+              placeholder="Enter a message"
+              value={input}
+              disableUnderline={true}
+              onChange={handleMessageInput}
             />
             <Button onClick={handleSendMessage}>Send</Button>
-            </div>
-          </Grid>
+          </div>
         </Grid>
       </Grid>
+    </Grid>
   );
 }

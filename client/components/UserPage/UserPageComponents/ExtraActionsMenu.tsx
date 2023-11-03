c6637ee5bd6846443d50fbc82603a336fdaca231
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-03 16:17:58 +0100

    Closing menu when confirmation window is open

--- client/components/UserPage/UserPageComponents/ExtraActionsMenu.tsx
+++ client/components/UserPage/UserPageComponents/ExtraActionsMenu.tsx
@@ -72,6 +72,7 @@ export default function ExtraActionsMenu( {echoBoard}: ExtraActionsMenuProps) {
 
   const handleDelete = () => {
    setIsDeleteModalOpen(true);
+   handleClose();
   };
 
   const handleCloseDeleteWindow = () => {

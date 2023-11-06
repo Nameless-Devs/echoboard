14bbf96e9117e0617cdd5ec0f6e9884ec96205a4
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-06 10:12:24 +0100

    added loading page

---
+++ client/components/Shared/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,24 @@
+import '@/components/Shared/LoadingPage/Loading.css'
+
+export function LoadingPage(){
+
+    return (
+        <>
+            <div className="title">
+                <h1>Loading </h1>
+            </div>
+            <div className="center">
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+            </div>
+        </>
+    );
+}

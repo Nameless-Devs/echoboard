064437c216530c4c4fbfe142dbea0a5fc2922a2d
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-07 10:55:18 +0100

    extracted solution logic into its own service

--- src/main/java/se/salt/echoboard/controller/SolutionController.java
+++ src/main/java/se/salt/echoboard/controller/SolutionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import se.salt.echoboard.controller.dto.EchoBoardSolutionResponse;
 import se.salt.echoboard.model.EchoBoardSolution;
 import se.salt.echoboard.service.EchoBoardService;
+import se.salt.echoboard.service.SolutionService;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
@@ -17,12 +18,12 @@ import static org.springframework.http.HttpStatus.OK;
 @RequiredArgsConstructor
 public class SolutionController {
 
-    private final EchoBoardService echoService;
+    private final SolutionService solutionService;
 
     @GetMapping("{solutionId}")
     @ResponseStatus(OK)
-    public EchoBoardSolution getEchoBoardSolution(@PathVariable long solutionId) {
-        return echoService.getSolutionById(solutionId);
+    public EchoBoardSolutionResponse getEchoBoardSolution(@PathVariable long solutionId) {
+        return solutionService.getSolutionById(solutionId);
     }
 
     @PatchMapping("{solutionId}")
@@ -30,19 +31,19 @@ public class SolutionController {
     public EchoBoardSolutionResponse updateSolutionStatus(@PathVariable long solutionId
             , @RequestParam EchoBoardSolution.SolutionStatus updateToStage) {
 
-        return echoService.updateSolutionStatus(solutionId, updateToStage);
+        return solutionService.updateSolutionStatus(solutionId, updateToStage);
     }
 
     @PatchMapping("{solutionId}/upvote")
     @ResponseStatus(OK)
     public Integer upvoteSolution(@PathVariable long solutionId, @AuthenticationPrincipal OidcUser user) {
-        return echoService.upvoteSolution(solutionId, user.getSubject());
+        return solutionService.upvoteSolution(solutionId, user.getSubject());
     }
 
     @PostMapping("{solutionId}/volunteer")
     @ResponseStatus(CREATED)
     public EchoBoardSolutionResponse volunteerForSolutionTesting(@PathVariable long solutionId
             , @AuthenticationPrincipal OidcUser user) {
-        return echoService.addVolunteerToSolution(solutionId, user);
+        return solutionService.addVolunteerToSolution(solutionId, user);
     }
 }

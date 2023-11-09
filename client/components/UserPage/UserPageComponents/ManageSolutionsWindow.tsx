712f8fd642708e16de685161c364044b657b060c
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-09 13:48:08 +0100

    Added props that will be passed to ManageSolution window

--- client/components/UserPage/UserPageComponents/ManageSolutionsWindow.tsx
+++ client/components/UserPage/UserPageComponents/ManageSolutionsWindow.tsx
@@ -1,7 +1,25 @@
+import { EchoBoardResponseData, UserResponseData } from '@/service/Types';
+import { Box, Modal } from '@mui/material'
 import React from 'react'
 
-export const ManageSolutionsWindow = () => {
+type ManageSolutionsWindowProps = {
+    open: boolean; 
+    onClose: () => void; 
+    user: UserResponseData; 
+    echoBoard: EchoBoardResponseData; 
+}
+
+export const ManageSolutionsWindow: React.FC<ManageSolutionsWindowProps> = ({
+    open,
+    onClose,
+    user,
+    echoBoard
+}) => {
   return (
-    <div>ManageSolutionsWindow</div>
+    <Modal open={open} onClose={onClose}>
+        <Box> 
+            
+        </Box>
+    </Modal>
   )
 }

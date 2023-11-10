1d7e6843bbd3dc8c56a5b7071086997fe36be50e
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-10 15:07:11 +0100

    Made Manage button look nice in mobile view

--- client/components/UserPage/UserPageComponents/ManageSolutionsWindow.tsx
+++ client/components/UserPage/UserPageComponents/ManageSolutionsWindow.tsx
@@ -77,8 +77,10 @@ export const ManageSolutionsWindow: React.FC<ManageSolutionsWindowProps> = ({
                                                 alignItems: "center", 
                                                 justifyContent: "flex-end",
                                                 marginRight: { xs: "1rem", md: "2rem"}, 
+                                                marginBottom: { xs: "0.5rem", md: "0"},
                                                 color: "#1976d2",
-                                                gap: "1rem"
+                                                gap: "1rem",
+                                                textAlign: "right",
                                                 }}>
                                                 <Typography>
                                                     Volunteers: 5, Accepted: 2

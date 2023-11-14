ff872084d4e107fbf5a2fc056acfd6eadd6ce0c2
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-14 16:07:28 +0100

    Created a new method in JPA repo to find echoboard by solution

--- src/main/java/se/salt/echoboard/service/repository/JPAEchoBoardRepository.java
+++ src/main/java/se/salt/echoboard/service/repository/JPAEchoBoardRepository.java
@@ -3,11 +3,17 @@ package se.salt.echoboard.service.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import se.salt.echoboard.controller.dto.EchoBoardPreview;
+import se.salt.echoboard.controller.dto.EchoBoardResponse;
 import se.salt.echoboard.model.EchoBoard;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JPAEchoBoardRepository extends JpaRepository<EchoBoard, Long> {
     @Query("select e from EchoBoard e order by e.created DESC")
     List<EchoBoard> findByOrderByCreatedDesc(Pageable pageable);
+
+    Optional<EchoBoard> findByEchoBoardSolutions_Id(long id);
 }
+

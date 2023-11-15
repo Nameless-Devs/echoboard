1d2a359ff51d125a7478db3af6c9fe9b8e7caadf
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-15 15:19:08 +0100

    Refactored component - added more elements

--- client/components/UserPage/UserPageComponents/EchoBoardPreviewDisplay.tsx
+++ client/components/UserPage/UserPageComponents/EchoBoardPreviewDisplay.tsx
@@ -4,27 +4,36 @@ import { EchoBoardPreviewResponseData } from '@/service/Types';
 
 type EchoBoardPreviewDisplayProps = {
     isLoading: boolean;
-    data: EchoBoardPreviewResponseData;
+    data: EchoBoardPreviewResponseData | undefined;
 }
 
 export const EchoBoardPreviewDisplay: React.FC<EchoBoardPreviewDisplayProps> = ({ isLoading, data }) => (
-    <Grid item xs={12} md="auto">
-        {isLoading ? (
-            <Skeleton variant="rectangular" width={200} height={24} />
-        ) : (
-            data && (
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <Typography color="textSecondary">to:</Typography>
-                    <Avatar
-                        src={data.echoBoardUser.picture}
-                        alt={data.echoBoardUser.name + " avatar picture"}
-                        sx={{ margin: "0 0.5rem", width: 24, height: 24 }}
-                    />
-                    <Typography color="textSecondary" sx={{ mr: "0.5rem" }}>
-                        {data.echoBoardUser.name}&rsquo;s problem
-                    </Typography>
-                </Box>
-            )
-        )}
-    </Grid>
+    <>
+        <Grid item xs={12} md="auto">
+            {isLoading ? (
+                <Skeleton variant="rectangular" width={200} height={24} />
+            ) : (
+                data && (
+                    <Box sx={{ display: "flex", alignItems: "center" }}>
+                        <Typography color="textSecondary">to:</Typography>
+                        <Avatar
+                            src={data.echoBoardUser.picture}
+                            alt={data.echoBoardUser.name + " avatar picture"}
+                            sx={{ margin: "0 0.5rem", width: 24, height: 24 }}
+                        />
+                        <Typography color="textSecondary" sx={{ mr: "0.5rem" }}>
+                            {data.echoBoardUser.name}&rsquo;s problem
+                        </Typography>
+                    </Box>
+                )
+            )}
+        </Grid>
+        <Grid item xs={12} md='auto' >
+            {isLoading ? (
+                <Skeleton variant="rectangular" width={150} height={24} />
+            ) : (
+                data && <Typography color="textSecondary">&quot;{data.title}&quot;</Typography>
+            )}
+        </Grid>
+    </>
 );

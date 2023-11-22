5af0158c9428a2cfbaf852cfee46a550efda0688
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-22 14:35:54 +0100

    Passing User data to SolutionItemUserPage

--- client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
+++ client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
@@ -14,6 +14,7 @@ import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 type SolutionItemProps = {
     solution: SolutionResponseData;
     onUpvote: (solutionId: string) => void;
+    user: UserResponseData;
 }
 
 export const SolutionItemUserPage: React.FC<SolutionItemProps> = ({ solution, onUpvote, user }) => {

347a2994332f502cdd9878d1006520f5bc76f1d9
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-22 14:37:36 +0100

    Making grid component into clickble element that triggers opening of CommentModal

--- client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
+++ client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
@@ -56,26 +56,47 @@ export const SolutionItemUserPage: React.FC<SolutionItemProps> = ({ solution, on
                     margin: "1rem 0",
                     borderRadius: "1rem",
                     backgroundColor: "white",
-                }}>
-                <EchoBoardPreviewDisplay isLoading={isLoading} data={echoBoardPreview} />
+                }}
+
+            >
+                <EchoBoardPreviewDisplay isLoading={previewLoading} data={echoBoardPreview} />
                 <Grid item xs={12}>
                     <Box sx={{ display: "flex", justifyContent: "flex-end", margin: "0.2rem 0.5rem 0 0 " }}>
                         <SolutionStatusBadge status={solution.status} solutionId={solution.id} />
                     </Box>
                 </Grid>
-                <Grid item xs={12} >
-                    <Typography variant="body1" color="textPrimary" sx={{ margin: "0.5rem 2rem 0.5rem 0" }}>
-                        {solution.content}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sx={{ marginTop: "-0.4rem" }}>
-                    <Typography variant="caption" color="textSecondary">
-                        {timeConverter(solution.created)}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <UpvoteButton count={solution.upvote.length} onUpvote={() => onUpvote(solution.id)} />
-                </Grid>
+                <Box sx={{
+                    width: '100%',
+                    cursor: 'pointer',
+                    borderRadius: '0.5rem',
+                    transition: 'box-shadow 0.3s',
+                    padding: "0 0.5rem",
+                    '&:hover': {
+                        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px;', 
+                    },
+                }}
+                    onClick={() => setIsOpen(true)} >
+                    <Grid item xs={12} >
+                        <Typography variant="body1" color="textPrimary" sx={{ margin: "0.5rem 2rem 0.5rem 0" }}>
+                            {solution.content}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sx={{ marginTop: "-0.4rem" }}>
+                        <Typography variant="caption" color="textSecondary">
+                            {timeConverter(solution.created)}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>   
+                    <Button sx={{
+                        padding: 0, 
+                        minWidth: 0, 
+                        margin: "0.5rem 0 1rem 0",
+                        '&:hover': { backgroundColor: 'transparent' },
+                        }}>
+                        <ThumbUpIcon /> {solution.upvote.length}
+                        </Button >     
+                    </Grid>
+                </Box>
             </Grid>
             {echoBoardExtended && <CommentModal
                 post={echoBoardExtended}

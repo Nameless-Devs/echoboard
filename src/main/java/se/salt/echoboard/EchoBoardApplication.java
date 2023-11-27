8ba5690829c56f409784cd782bed84c7de2f5ab2
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-27 14:42:38 +0100

    added ConfigurationPropertiesScan

--- src/main/java/se/salt/echoboard/EchoBoardApplication.java
+++ src/main/java/se/salt/echoboard/EchoBoardApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EchoBoardApplication {
 
     public static void main(String[] args) {

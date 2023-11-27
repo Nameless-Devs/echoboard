96523191e196374c46e21085b885045e6691b928
Ibrahim <Ibrahim@ebs.net.pk>
2023-11-27 13:31:47 +0100

    added testconatiner to test the complete application

---
+++ src/test/java/se/salt/echoboard/TestContainersConfig.java
@@ -0,0 +1,18 @@
+package se.salt.echoboard;
+
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestContainersConfig {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:15");
+    }
+}

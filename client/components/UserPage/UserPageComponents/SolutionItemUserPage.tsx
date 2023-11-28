0b757b96cec6af6cd6932207b67949fc326b1b64
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-11-28 10:34:39 +0100

    Optimized imports

--- client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
+++ client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
@@ -1,17 +1,13 @@
 import { EchoBoardPreviewResponseData, EchoBoardResponseData, SolutionResponseData, UserResponseData } from '@/service/Types';
-import { Box, Button, Grid, ListItem, Typography } from '@mui/material';
+import { Box, Grid, ListItem } from '@mui/material';
 import React, { useState } from 'react';
-import { timeConverter } from '@/service/TimeConverter';
 import { SolutionStatusBadge } from '@/components/CommentModal/commentModalComponents/SolutionStatusBadge';
 import { useQuery } from '@tanstack/react-query';
 import { editSolution, fetchEchoBoardById, fetchEchoBoardBySolutionId } from '@/service/Functions';
 import { EchoBoardPreviewDisplay } from './EchoBoardPreviewDisplay';
 import CommentModal from '@/components/CommentModal/CommentModal';
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ExtraActionsMenu from './ExtraActionsMenu';
 import { ClickableContentElement } from './ClickableContentElement';
-import { Solitreo } from 'next/font/google';
-
 
 type SolutionItemProps = {
     solution: SolutionResponseData;

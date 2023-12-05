aa471f7f08f9a4781eabe84c35d76d88f3ce7959
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-05 11:02:55 +0100

    Implement recursive rendering of nested comments in CommentItem
    - Added logic to recursively render child comments within each CommentItem component.
    - This allows the display of a hierarchical comment structure, enabling users to view replies directly under each comment.

--- client/components/CommentModal/commentModalComponents/CommentItem.tsx
+++ client/components/CommentModal/commentModalComponents/CommentItem.tsx
@@ -12,38 +12,30 @@ type CommentItemProps = {
   user: UserResponseData;
 };
 
-export const CommentItem: React.FC<CommentItemProps> = ({
-  comment,
-  onUpvote,
-  user,
-}) => {
-  return (
-    <ListItem className="comment-display__individual-comment">
-      <Grid container sx={{ marginTop: "1rem" }}>
-        <ItemHeader
-          pictureUrl={comment.echoBoardUser.picture}
-          userName={comment.echoBoardUser.name}
-          created={comment.created}
-        />
-        <ItemContent
-          content={comment.content}
-          upvote={comment.upvote.length}
-          onUpvote={onUpvote}
-        />
-        <PostCommentOnComment echoBoardId={comment.id} user={user} />
-        {comment.echoBoardComments && comment.echoBoardComments.length > 0 && (
-          <div className="nested-comments">
-            {comment.echoBoardComments.map((childComment) => (
-              <CommentItem
-                key={childComment.id}
-                comment={childComment}
-                onUpvote={onUpvote}
-                user={user}
-              />
-            ))}
-          </div>
-        )}
-      </Grid>
-    </ListItem>
-  );
-};
+export const CommentItem: React.FC<CommentItemProps> = ({ comment, onUpvote, user }) => {
+
+    return (
+        <ListItem className="comment-display__individual-comment">
+             <Grid container sx={{marginTop: "1rem"}}>
+                <ItemHeader
+                    pictureUrl={comment.echoBoardUser.picture}
+                    userName={comment.echoBoardUser.name}
+                    created={comment.created}
+                />
+                <ItemContent
+                    content={comment.content}
+                    upvote={comment.upvote.length}
+                    onUpvote={onUpvote}
+                />
+                <PostCommentOnComment echoBoardId={comment.id} user={user} />
+                {comment.echoBoardComments && comment.echoBoardComments.length > 0 && (
+                    <div className='nested-comments'>
+                        {comment.echoBoardComments.map((childComment) => (
+                            <CommentItem key={childComment.id} comment={childComment} onUpvote={onUpvote} user={user} />
+                        ))}
+                    </div>
+                )}
+            </Grid>
+        </ListItem>
+    )
+}

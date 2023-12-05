37ca8309073725b9b9949b5dc0c7180ca1d79ab6
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-05 11:12:21 +0100

    Reformatted Document. - Fixed Indentation & Cleaned up the code

--- client/components/Upvote.tsx
+++ client/components/Upvote.tsx
@@ -13,15 +13,12 @@ export const Upvote: React.FC<UpvoteProps> = ({ upvote, echoBoardId }) => {
   );
   const upvoteCount = echoData ? echoData.upvote.length : upvote.length;
 
-  const mutation = useMutation(
-    () => upvotePost(echoBoardId),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(["echoBoard", echoBoardId]);
-        queryClient.refetchQueries(["echoBoard", echoBoardId]);
-      },
-    }
-  );
+  const mutation = useMutation(() => upvotePost(echoBoardId), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["echoBoard", echoBoardId]);
+      queryClient.refetchQueries(["echoBoard", echoBoardId]);
+    },
+  });
 
   return (
     <div>

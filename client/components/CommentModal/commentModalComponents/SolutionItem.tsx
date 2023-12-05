6b9532e74421fc82dc301617b8c398fc0d7742d3
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-05 10:58:40 +0100

    Reformatted Document. - Fixed Indentation & Cleaned up the code

--- client/components/CommentModal/commentModalComponents/SolutionItem.tsx
+++ client/components/CommentModal/commentModalComponents/SolutionItem.tsx
@@ -1,39 +1,51 @@
-import { SolutionResponseData, UserResponseData } from '@/service/Types'
-import { Avatar, Box, Grid, ListItem, Typography } from '@mui/material';
-import React from 'react'
-import UpvoteButton from '../../UpvoteButton';
-import { SolutionStatusBadge } from './SolutionStatusBadge';
-import { timeConverter } from '@/service/TimeConverter';
-import { ItemContent } from './ItemContent';
-import { ItemHeader } from './ItemHeader';
+import { SolutionResponseData, UserResponseData } from "@/service/Types";
+import { Avatar, Box, Grid, ListItem, Typography } from "@mui/material";
+import React from "react";
+import UpvoteButton from "../../UpvoteButton";
+import { SolutionStatusBadge } from "./SolutionStatusBadge";
+import { timeConverter } from "@/service/TimeConverter";
+import { ItemContent } from "./ItemContent";
+import { ItemHeader } from "./ItemHeader";
 
 type SolutionItemProps = {
-    solution: SolutionResponseData;
-    onUpvote: (solutionId: string) => void;
-    user?: UserResponseData;
-}
+  solution: SolutionResponseData;
+  onUpvote: (solutionId: string) => void;
+  user?: UserResponseData;
+};
 
-export const SolutionItem: React.FC<SolutionItemProps> = ({ solution, onUpvote, user }) => {
-
-    return (
-        <ListItem className="comment-display__individual-comment">
-            <Grid container>
-                <Grid item xs={12}>
-                    <Box sx={{ display: "flex", justifyContent: "flex-end", margin: "0.2rem 0.5rem 0 0 " }}>
-                        <SolutionStatusBadge status={solution.status} solutionId={solution.id} />
-                    </Box>
-                </Grid>
-                <ItemHeader
-                    pictureUrl={solution.echoBoardUser.picture}
-                    userName={solution.echoBoardUser.name}
-                    created={solution.created}
-                />
-                <ItemContent
-                    content={solution.content}
-                    upvote={solution.upvote.length}
-                    onUpvote={onUpvote}
-                />
-            </Grid>
-        </ListItem>
-    )
-}
+export const SolutionItem: React.FC<SolutionItemProps> = ({
+  solution,
+  onUpvote,
+  user,
+}) => {
+  return (
+    <ListItem className="comment-display__individual-comment">
+      <Grid container>
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              margin: "0.2rem 0.5rem 0 0 ",
+            }}
+          >
+            <SolutionStatusBadge
+              status={solution.status}
+              solutionId={solution.id}
+            />
+          </Box>
+        </Grid>
+        <ItemHeader
+          pictureUrl={solution.echoBoardUser.picture}
+          userName={solution.echoBoardUser.name}
+          created={solution.created}
+        />
+        <ItemContent
+          content={solution.content}
+          upvote={solution.upvote.length}
+          onUpvote={onUpvote}
+        />
+      </Grid>
+    </ListItem>
+  );
+};

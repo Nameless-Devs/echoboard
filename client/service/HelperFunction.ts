c5c5be381302578bbdc5b49ea3e6b1bf12af1f44
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-05 11:11:15 +0100

    Reformatted Document. - Fixed Indentation & Cleaned up the code

--- client/service/HelperFunction.ts
+++ client/service/HelperFunction.ts
@@ -1,10 +1,16 @@
 import { SolutionResponseData, UserResponseData } from "./Types";
 
-export const calulateNumberOfSolvedSolution = (solutions: SolutionResponseData[]) => {
-    const solvedSolutions = solutions.filter(solution => solution.status === 'SOLVED');
-    return solvedSolutions.length;
-}
+export const calulateNumberOfSolvedSolution = (
+  solutions: SolutionResponseData[]
+) => {
+  const solvedSolutions = solutions.filter(
+    (solution) => solution.status === "SOLVED"
+  );
+  return solvedSolutions.length;
+};
 
 export const calculateNumberOfVolunteering = (user: UserResponseData) => {
-    return user.pendingVolunteeredSolutions.length + user.volunteeredSolutions.length;
-}
+  return (
+    user.pendingVolunteeredSolutions.length + user.volunteeredSolutions.length
+  );
+};

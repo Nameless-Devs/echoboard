b091f15a26c31b65f050cbbfcccf8e17f0673849
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-05 11:12:32 +0100

    Reformatted Document. - Fixed Indentation & Cleaned up the code

--- client/hooks/useScrollToLatestMessage.ts
+++ client/hooks/useScrollToLatestMessage.ts
@@ -1,11 +1,11 @@
-import {Message} from "@/service/Types";
-import {useEffect, useRef} from "react";
+import { Message } from "@/service/Types";
+import { useEffect, useRef } from "react";
 
-export const useScrollToLatestMessage = ( messages: Message[] ) => {
-    const dummy = useRef<HTMLDivElement>(null);
+export const useScrollToLatestMessage = (messages: Message[]) => {
+  const dummy = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        dummy.current?.scrollIntoView({behavior: "smooth"});
-    }, [messages]);
-    return dummy;
-}
+  useEffect(() => {
+    dummy.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+  return dummy;
+};

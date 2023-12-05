91a2a8e6422422ce4c8e319e422373739700f3a5
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-05 10:59:32 +0100

    Reformatted Document. - Fixed Indentation & Cleaned up the code

--- client/components/CommentModal/commentModalComponents/ItemContent.tsx
+++ client/components/CommentModal/commentModalComponents/ItemContent.tsx
@@ -1,30 +1,34 @@
-import UpvoteButton from '@/components/UpvoteButton'
-import { Grid, Typography } from '@mui/material'
-import React from 'react'
+import UpvoteButton from "@/components/UpvoteButton";
+import { Grid, Typography } from "@mui/material";
+import React from "react";
 
 type ItemContentProps = {
-    content: string; 
-    upvote: number; 
-    onUpvote: (itemId: string) => void; 
-}
+  content: string;
+  upvote: number;
+  onUpvote: (itemId: string) => void;
+};
 
 export const ItemContent: React.FC<ItemContentProps> = ({
-    content,
-    upvote,
-    onUpvote
+  content,
+  upvote,
+  onUpvote,
 }) => {
-    return (
-        <>
-            <Grid item xs={2} md={1}></Grid>
-            <Grid item xs={10} md={11}>
-                <Typography variant="body1" color="textPrimary" sx={{ margin: "0.5rem 2rem 0.5rem 0" }}>
-                    {content}
-                </Typography>
-            </Grid>
-            <Grid item xs={2} md={1}></Grid>
-            <Grid item xs={2} md={1}>
-                <UpvoteButton count={upvote} onUpvote={() => onUpvote} />
-            </Grid>
-        </>
-    )
-}
+  return (
+    <>
+      <Grid item xs={2} md={1}></Grid>
+      <Grid item xs={10} md={11}>
+        <Typography
+          variant="body1"
+          color="textPrimary"
+          sx={{ margin: "0.5rem 2rem 0.5rem 0" }}
+        >
+          {content}
+        </Typography>
+      </Grid>
+      <Grid item xs={2} md={1}></Grid>
+      <Grid item xs={2} md={1}>
+        <UpvoteButton count={upvote} onUpvote={() => onUpvote} />
+      </Grid>
+    </>
+  );
+};

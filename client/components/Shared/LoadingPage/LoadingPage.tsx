6eff2e1c61bbf41600e0f70f86c50484c83fd291
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:21:46 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/components/Shared/LoadingPage/LoadingPage.tsx
+++ client/components/Shared/LoadingPage/LoadingPage.tsx
@@ -1,24 +1,23 @@
-import '@/components/Shared/LoadingPage/Loading.css'
+import "@/components/Shared/LoadingPage/Loading.css";
 
-export function LoadingPage(){
-
-    return (
-        <>
-            <div className="title">
-                <h1>Loading </h1>
-            </div>
-            <div className="center">
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-                <div className="wave"></div>
-            </div>
-        </>
-    );
+export function LoadingPage() {
+  return (
+    <>
+      <div className="title">
+        <h1>Loading </h1>
+      </div>
+      <div className="center">
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+      </div>
+    </>
+  );
 }

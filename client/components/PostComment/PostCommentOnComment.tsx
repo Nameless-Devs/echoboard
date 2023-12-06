827b0ffd7263ad226cb5938f9ef6061c3140c266
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 16:23:51 +0100

    Added a new PostCommentOnComment that imitates PostComment.tsx

---
+++ client/components/PostComment/PostCommentOnComment.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { CommentToPost, UserResponseData } from "@/service/Types";
+import { Avatar, Box, Dialog, DialogContentText } from "@mui/material";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { postComment, postingCommentOnComment } from "@/service/Functions";
+import { CommentForm } from "./PostCommentTextField";
+import { StyledBadge } from "./StyledBadge";
+import "../../app/styles/PostComment.css";
+
+interface CommentProps {
+  echoBoardId: string;
+  user: UserResponseData;
+}
+
+export const PostCommentOnComment: React.FC<CommentProps> = ({ echoBoardId, user }) => {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation((data: CommentToPost) =>
+  postingCommentOnComment(data, echoBoardId)
+  );
+
+  const handlePostCommentOnComment = (content: string) => {
+    if (!content.trim()) {
+      return;
+    }
+
+    const commentToPost = {
+      author: user.name,
+      content,
+    };
+
+    mutation.mutate(commentToPost, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["echoBoards"]);
+        queryClient.refetchQueries(["comments", echoBoardId]);
+        setIsSuccess(true);
+        setTimeout(() => setIsSuccess(false), 1500);
+      },
+      onError: (error) => {
+        console.error("Error:", error);
+      },
+    });
+  };
+
+  return (
+    <>
+      {isSuccess && (
+        <Dialog open={isSuccess}>
+          <DialogContentText style={{padding: "40px", color: "green", fontSize: "20px", textAlign: "center"}}>
+          Your comment was successfully posted
+          </DialogContentText>
+        </Dialog>
+      )}
+      <form className="post-comment__form" onSubmit={(e) => e.preventDefault()}>
+        <Box className="post-comment__box">
+          <Box>
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar
+                className="post-comment__avatar"
+                alt={user.name + "avatar"}
+                src={user.picture}
+              />
+            </StyledBadge>
+          </Box>
+          <CommentForm onCommentSubmit={handlePostCommentOnComment} />
+        </Box>
+      </form>
+    </>
+  );
+};

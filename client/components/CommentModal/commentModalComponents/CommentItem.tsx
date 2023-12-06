27c3e0c8fbda22cc289e7ac1de1cd5b6ecbcd0f1
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 16:22:41 +0100

    Added logic to recursively render child comments within each CommentItem component.

--- client/components/CommentModal/commentModalComponents/CommentItem.tsx
+++ client/components/CommentModal/commentModalComponents/CommentItem.tsx
@@ -1,17 +1,20 @@
-import { CommentResponseData } from "@/service/Types";
+import { CommentResponseData, UserResponseData } from "@/service/Types";
 import { Grid, ListItem } from "@mui/material";
 import React from "react";
 import { ItemContent } from "./ItemContent";
 import { ItemHeader } from "./ItemHeader";
+import { PostCommentOnComment } from "@/components/PostComment/PostCommentOnComment";
 
 type CommentItemProps = {
   comment: CommentResponseData;
   onUpvote: () => void;
+  user: UserResponseData;
 };
 
 export const CommentItem: React.FC<CommentItemProps> = ({
   comment,
   onUpvote,
+  user,
 }) => {
   return (
     <ListItem className="comment-display__individual-comment">
@@ -27,6 +30,19 @@ export const CommentItem: React.FC<CommentItemProps> = ({
           onUpvote={onUpvote}
           id={comment.id}
         />
+        <PostCommentOnComment echoBoardId={comment.id} user={user} />
+        {comment.echoBoardComments && comment.echoBoardComments.length > 0 && (
+          <div className="nested-comments">
+            {comment.echoBoardComments.map((childComment) => (
+              <CommentItem
+                key={childComment.id}
+                comment={childComment}
+                onUpvote={onUpvote}
+                user={user}
+              />
+            ))}
+          </div>
+        )}
       </Grid>
     </ListItem>
   );

9a31c8345a86515c7d3465c3e2712cf9a95aa959
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:21:36 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/components/CommentModal/commentModalComponents/ItemHeader.tsx
+++ client/components/CommentModal/commentModalComponents/ItemHeader.tsx
@@ -1,33 +1,37 @@
-import { timeConverter } from '@/service/TimeConverter'
-import { Grid, Box, Avatar, Typography } from '@mui/material'
-import React from 'react'
+import { timeConverter } from "@/service/TimeConverter";
+import { Grid, Box, Avatar, Typography } from "@mui/material";
+import React from "react";
 
 type ItemHeaderProps = {
-    pictureUrl: string; 
-    userName: string; 
-    created: string;
-}
+  pictureUrl: string;
+  userName: string;
+  created: string;
+};
 
 export const ItemHeader: React.FC<ItemHeaderProps> = ({
-    pictureUrl,
-    userName,
-    created
+  pictureUrl,
+  userName,
+  created,
 }) => {
-    return (
-        <>
-            <Grid item xs={2} md={1} sx={{ marginTop: "-0.5rem" }}>
-                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                    <Avatar src={pictureUrl} />
-                </Box>
-            </Grid>
-            <Grid item xs={10} md={11} sx={{ marginTop: "-0.5rem" }}>
-                <Typography sx={{ marginBottom: "-0.4rem" }}>
-                    {userName}
-                </Typography>
-                <Typography variant="caption" color="textSecondary">
-                    {timeConverter(created)}
-                </Typography>
-            </Grid>
-        </>
-    )
-}
+  return (
+    <>
+      <Grid item xs={2} md={1} sx={{ marginTop: "-0.5rem" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Avatar src={pictureUrl} />
+        </Box>
+      </Grid>
+      <Grid item xs={10} md={11} sx={{ marginTop: "-0.5rem" }}>
+        <Typography sx={{ marginBottom: "-0.4rem" }}>{userName}</Typography>
+        <Typography variant="caption" color="textSecondary">
+          {timeConverter(created)}
+        </Typography>
+      </Grid>
+    </>
+  );
+};

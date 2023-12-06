86654b00db15251c0b96f149047abb2376c99681
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:22:37 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
+++ client/components/UserPage/UserPageComponents/SolutionItemUserPage.tsx
@@ -1,85 +1,121 @@
-import { EchoBoardPreviewResponseData, EchoBoardResponseData, SolutionResponseData, UserResponseData } from '@/service/Types';
-import { Box, Grid, ListItem } from '@mui/material';
-import React, { useState } from 'react';
-import { SolutionStatusBadge } from '@/components/CommentModal/commentModalComponents/SolutionStatusBadge';
-import { useQuery } from '@tanstack/react-query';
-import { editSolution, fetchEchoBoardById, fetchEchoBoardBySolutionId } from '@/service/Functions';
-import { EchoBoardPreviewDisplay } from './EchoBoardPreviewDisplay';
-import CommentModal from '@/components/CommentModal/CommentModal';
-import ExtraActionsMenu from './ExtraActionsMenu';
-import { ClickableContentElement } from './ClickableContentElement';
+import {
+  EchoBoardPreviewResponseData,
+  EchoBoardResponseData,
+  SolutionResponseData,
+  UserResponseData,
+} from "@/service/Types";
+import { Box, Grid, ListItem } from "@mui/material";
+import React, { useState } from "react";
+import { SolutionStatusBadge } from "@/components/CommentModal/commentModalComponents/SolutionStatusBadge";
+import { useQuery } from "@tanstack/react-query";
+import {
+  editSolution,
+  fetchEchoBoardById,
+  fetchEchoBoardBySolutionId,
+} from "@/service/Functions";
+import { EchoBoardPreviewDisplay } from "./EchoBoardPreviewDisplay";
+import CommentModal from "@/components/CommentModal/CommentModal";
+import ExtraActionsMenu from "./ExtraActionsMenu";
+import { ClickableContentElement } from "./ClickableContentElement";
 
 type SolutionItemProps = {
-    solution: SolutionResponseData;
-    user: UserResponseData;
-}
+  solution: SolutionResponseData;
+  user: UserResponseData;
+};
 
-export const SolutionItemUserPage: React.FC<SolutionItemProps> = ({ solution, user }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const defaultTabIndex = 1;
+export const SolutionItemUserPage: React.FC<SolutionItemProps> = ({
+  solution,
+  user,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const defaultTabIndex = 1;
 
-    const {
-        data: echoBoardPreview,
-        isLoading: previewLoading,
-        isError: previewError,
-    } = useQuery<EchoBoardPreviewResponseData>(["echoBoardPreviewSolution", solution.id], async () => {
-        return await fetchEchoBoardBySolutionId(solution.id);
-    });
+  const {
+    data: echoBoardPreview,
+    isLoading: previewLoading,
+    isError: previewError,
+  } = useQuery<EchoBoardPreviewResponseData>(
+    ["echoBoardPreviewSolution", solution.id],
+    async () => {
+      return await fetchEchoBoardBySolutionId(solution.id);
+    }
+  );
 
-    const {
-        data: echoBoardExtended,
-        isLoading: extendedLoading,
-        isError: extendedError,
-    } = useQuery<EchoBoardResponseData>(
-        ["echoBoardExtendedSolution", echoBoardPreview?.id],
-        async () => {
-            return await fetchEchoBoardById(echoBoardPreview?.id || '');
-        },
-        {
-            enabled: !!echoBoardPreview?.id,
-        }
-    );
+  const {
+    data: echoBoardExtended,
+    isLoading: extendedLoading,
+    isError: extendedError,
+  } = useQuery<EchoBoardResponseData>(
+    ["echoBoardExtendedSolution", echoBoardPreview?.id],
+    async () => {
+      return await fetchEchoBoardById(echoBoardPreview?.id || "");
+    },
+    {
+      enabled: !!echoBoardPreview?.id,
+    }
+  );
 
-    const handleClose = () => {
-        setIsOpen(false);
-    };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
-    return (
-        <ListItem sx={{ padding: 0 }}>
-            <Grid
-                container sx={{
-                    border: "solid black 1px",
-                    padding: "1rem",
-                    margin: "1rem 0",
-                    borderRadius: "1rem",
-                    backgroundColor: "white",
-                }}
-
-            >
-                <Box sx={{ display: "flex", justifyContent: "space-between", width: '100%' }}>
-                    <Box sx={{ marginLeft: 'auto', width: '100%' }}>
-                        <EchoBoardPreviewDisplay isLoading={previewLoading} data={echoBoardPreview} />
-                    </Box>
-                    <ExtraActionsMenu solution={solution} onEdit={editSolution} />
-                </Box>
-                <Grid item xs={12}>
-                    <Box sx={{ display: "flex", justifyContent: "flex-end", margin: "0.2rem 0.5rem 0 0 " }}>
-                        <SolutionStatusBadge status={solution.status} solutionId={solution.id} />
-                    </Box>
-                </Grid>
-                <ClickableContentElement
-                    content={solution.content}
-                    created={solution.created}
-                    upvoteLength={solution.upvote.length}
-                    setIsOpen={setIsOpen} />
-            </Grid>
-            {echoBoardExtended && <CommentModal
-                post={echoBoardExtended}
-                handleClose={handleClose}
-                isOpen={isOpen}
-                user={user}
-                defaultTabIndex={defaultTabIndex}
-            />}
-        </ListItem>
-    )
-}
+  return (
+    <ListItem sx={{ padding: 0 }}>
+      <Grid
+        container
+        sx={{
+          border: "solid black 1px",
+          padding: "1rem",
+          margin: "1rem 0",
+          borderRadius: "1rem",
+          backgroundColor: "white",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <Box sx={{ marginLeft: "auto", width: "100%" }}>
+            <EchoBoardPreviewDisplay
+              isLoading={previewLoading}
+              data={echoBoardPreview}
+            />
+          </Box>
+          <ExtraActionsMenu solution={solution} onEdit={editSolution} />
+        </Box>
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              margin: "0.2rem 0.5rem 0 0 ",
+            }}
+          >
+            <SolutionStatusBadge
+              status={solution.status}
+              solutionId={solution.id}
+            />
+          </Box>
+        </Grid>
+        <ClickableContentElement
+          content={solution.content}
+          created={solution.created}
+          upvoteLength={solution.upvote.length}
+          setIsOpen={setIsOpen}
+        />
+      </Grid>
+      {echoBoardExtended && (
+        <CommentModal
+          post={echoBoardExtended}
+          handleClose={handleClose}
+          isOpen={isOpen}
+          user={user}
+          defaultTabIndex={defaultTabIndex}
+        />
+      )}
+    </ListItem>
+  );
+};

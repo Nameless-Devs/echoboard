8150c7d324878b923235c1575dde13c67c30b5ee
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:20:06 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/components/CommentModal/commentModalComponents/CommentItem.tsx
+++ client/components/CommentModal/commentModalComponents/CommentItem.tsx
@@ -1,31 +1,33 @@
-import { CommentResponseData } from '@/service/Types'
-import { Grid, ListItem } from '@mui/material'
-import React from 'react'
-import { ItemContent } from './ItemContent'
-import { ItemHeader } from './ItemHeader'
+import { CommentResponseData } from "@/service/Types";
+import { Grid, ListItem } from "@mui/material";
+import React from "react";
+import { ItemContent } from "./ItemContent";
+import { ItemHeader } from "./ItemHeader";
 
 type CommentItemProps = {
-    comment: CommentResponseData,
-    onUpvote: () => void,
-}
+  comment: CommentResponseData;
+  onUpvote: () => void;
+};
 
-export const CommentItem: React.FC<CommentItemProps> = ({ comment, onUpvote }) => {
-
-    return (
-        <ListItem className="comment-display__individual-comment">
-             <Grid container sx={{marginTop: "1rem"}}>
-                <ItemHeader
-                    pictureUrl={comment.echoBoardUser.picture}
-                    userName={comment.echoBoardUser.name}
-                    created={comment.created}
-                />
-                <ItemContent
-                    content={comment.content}
-                    upvote={comment.upvote.length}
-                    onUpvote={onUpvote}
-                    id={comment.id}
-                />
-            </Grid>
-        </ListItem>
-    )
-}
+export const CommentItem: React.FC<CommentItemProps> = ({
+  comment,
+  onUpvote,
+}) => {
+  return (
+    <ListItem className="comment-display__individual-comment">
+      <Grid container sx={{ marginTop: "1rem" }}>
+        <ItemHeader
+          pictureUrl={comment.echoBoardUser.picture}
+          userName={comment.echoBoardUser.name}
+          created={comment.created}
+        />
+        <ItemContent
+          content={comment.content}
+          upvote={comment.upvote.length}
+          onUpvote={onUpvote}
+          id={comment.id}
+        />
+      </Grid>
+    </ListItem>
+  );
+};

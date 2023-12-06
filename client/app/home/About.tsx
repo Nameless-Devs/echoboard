04476dbcc3530e4708cc2c3758c2302898ea6b70
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:19:20 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/app/home/About.tsx
+++ client/app/home/About.tsx
@@ -1,95 +1,105 @@
-"use client"
-import { Box, Button, Grid, Typography } from '@mui/material';
-import React from 'react';
+"use client";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import React from "react";
 import PhonePicture from "../image/Phone_example.png";
 import Image from "next/image";
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import { YOUTUBE_LINK } from '@/service/config';
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { YOUTUBE_LINK } from "@/service/config";
 
 export const About = () => {
-    return (
-        <>
+  return (
+    <>
+      <Box
+        id="about"
+        sx={{
+          background:
+            "linear-gradient(135deg, hsla(215, 91%, 18%, 1) 2%, hsla(187, 61%, 28%, 1) 100%)",
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: "center",
+            padding: "2rem 0",
+            fontSize: { md: "2.5rem" },
+            color: "white",
+          }}
+        >
+          About
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={7}>
             <Box
-                id="about"
+              display={"flex"}
+              flexDirection={"column"}
+              sx={{
+                textAlign: "left",
+                margin: { xs: "0 1.5rem", md: "0 0 0 5rem" },
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              <Typography
+                variant="h6"
                 sx={{
-                    background: "linear-gradient(135deg, hsla(215, 91%, 18%, 1) 2%, hsla(187, 61%, 28%, 1) 100%)"
-                }}>
-                <Typography
-                    variant="h5"
-                    sx={{
-                        textAlign: 'center',
-                        padding: '2rem 0',
-                        fontSize: { md: '2.5rem' },
-                        color: "white",
-                    }}
-                >
-                    About
-                </Typography>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={7}>
-                        <Box
-                            display={'flex'}
-                            flexDirection={"column"}
-                            sx={{
-                                textAlign: "left",
-                                margin: { xs: "0 1.5rem", md: "0 0 0 5rem" },
-                                alignItems: "center",
-                                justifyContent: "center",
-                                height: "100%"
-
-                            }}>
-                            <Typography variant='h6'
-                                sx={{
-                                    color: "white",
-                                }}>
-                                EchoBoard is a platform designed to address the challenge of effectively sharing and solving problems within the workplace.
-                            </Typography>
-                            <Typography variant='h6'
-                                sx={{
-                                    color: "white",
-                                    marginTop: "1.5rem",
-                                }}>
-                                It aims to bridge the gap between employees and upper management by providing a platform for employees to voice their concerns,
-                                propose solutions, and collaborate on addressing issues that impact the organisation.
-                            </Typography>
-                            <Button
-                                onClick={() => window.open(YOUTUBE_LINK, "_blank")}
-                                component="label"
-                                size="large"
-                                color="error"
-                                variant="contained"
-                                startIcon={<YouTubeIcon />}
-                                sx={{
-                                    margin: { xs: '2rem', md: '3rem' },
-                                }}>
-                                Watch video
-                            </Button>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12} md={5}>
-                        <Box display={{ xs: "none", md: "flex" }} justifyContent={"center"}>
-                            <Image
-                                src={PhonePicture}
-                                alt="Picture of mobile phone with EchoBoard application"
-                                width={250}
-                                style={{
-                                    marginBottom: "5rem"
-                                }}
-                            />
-                        </Box>
-                        <Box display={{ xs: "flex", md: "none" }} justifyContent={"center"}>
-                            <Image
-                                src={PhonePicture}
-                                alt="Picture of mobile phone with EchoBoard application"
-                                width={200}
-                                style={{
-                                    marginBottom: "3rem"
-                                }}
-                            />
-                        </Box>
-                    </Grid>
-                </Grid>
+                  color: "white",
+                }}
+              >
+                EchoBoard is a platform designed to address the challenge of
+                effectively sharing and solving problems within the workplace.
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "white",
+                  marginTop: "1.5rem",
+                }}
+              >
+                It aims to bridge the gap between employees and upper management
+                by providing a platform for employees to voice their concerns,
+                propose solutions, and collaborate on addressing issues that
+                impact the organisation.
+              </Typography>
+              <Button
+                onClick={() => window.open(YOUTUBE_LINK, "_blank")}
+                component="label"
+                size="large"
+                color="error"
+                variant="contained"
+                startIcon={<YouTubeIcon />}
+                sx={{
+                  margin: { xs: "2rem", md: "3rem" },
+                }}
+              >
+                Watch video
+              </Button>
             </Box>
-        </>
-    )
-}
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <Box display={{ xs: "none", md: "flex" }} justifyContent={"center"}>
+              <Image
+                src={PhonePicture}
+                alt="Picture of mobile phone with EchoBoard application"
+                width={250}
+                style={{
+                  marginBottom: "5rem",
+                }}
+              />
+            </Box>
+            <Box display={{ xs: "flex", md: "none" }} justifyContent={"center"}>
+              <Image
+                src={PhonePicture}
+                alt="Picture of mobile phone with EchoBoard application"
+                width={200}
+                style={{
+                  marginBottom: "3rem",
+                }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};

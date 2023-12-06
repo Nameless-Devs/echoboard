7ec6dce3e8b7a6fc2ede36551e77fb0d273728a6
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:20:38 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/components/UserPage/UserPageComponents/DeleteConfirmationWindow.tsx
+++ client/components/UserPage/UserPageComponents/DeleteConfirmationWindow.tsx
@@ -1,84 +1,80 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
 };
 
 type DeleteConfirmationWindowProp = {
-    open: boolean;
-    handleClose: () => void;
-    contentType: string;
-    content: string;
-    id: string;
-    handleDelete: (id: string) => Promise<void>;
-}
+  open: boolean;
+  handleClose: () => void;
+  contentType: string;
+  content: string;
+  id: string;
+  handleDelete: (id: string) => Promise<void>;
+};
 
 const DeleteConfirmationWindow: React.FC<DeleteConfirmationWindowProp> = ({
-    open,
-    handleClose,
-    contentType,
-    content,
-    id,
-    handleDelete,
+  open,
+  handleClose,
+  contentType,
+  content,
+  id,
+  handleDelete,
 }) => {
+  const queryClient = useQueryClient();
 
-    const queryClient = useQueryClient();
+  const handleDeleteAction = useMutation(handleDelete, {
+    onSettled: () => {
+      handleClose();
+      queryClient.invalidateQueries(["userInfo"]);
+      queryClient.refetchQueries(["userInfo"]);
+    },
+  });
 
-    const handleDeleteAction = useMutation(handleDelete, {
-        onSettled: () => {
-            handleClose();
-            queryClient.invalidateQueries(['userInfo']);
-            queryClient.refetchQueries(['userInfo']);
-        },
-    });
+  const handleDeletePost = async (id: string) => {
+    handleDeleteAction.mutate(id);
+  };
 
-    const handleDeletePost = async (id: string) => {
-        handleDeleteAction.mutate(id);
-    };
-
-    return (
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="delete-comfirmation-modal"
-            aria-describedby="delete-confirmation-description"
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="delete-comfirmation-modal"
+      aria-describedby="delete-confirmation-description"
+    >
+      <Box sx={style}>
+        <Typography id="delete-comfirmation-modal" variant="h6" component="h2">
+          Confirm
+        </Typography>
+        <Typography id="delete-confirmation-description" sx={{ mt: 2 }}>
+          Are you sure you want to delete this {contentType}?
+        </Typography>
+        <Typography variant="h5">{content}</Typography>
+        <Typography>You will not be able to reverse this process.</Typography>
+        <Button
+          onClick={() => {
+            handleDeletePost(id);
+          }}
         >
-            <Box sx={style}>
-                <Typography id="delete-comfirmation-modal" variant="h6" component="h2">
-                    Confirm
-                </Typography>
-                <Typography id="delete-confirmation-description" sx={{ mt: 2 }}>
-                    Are you sure you want to delete this {contentType}?
-                </Typography>
-                <Typography variant='h5'>
-                    {content}
-                </Typography>
-                <Typography>
-                    You will not be able to reverse this process.
-                </Typography>
-                <Button onClick={() => {
-                    handleDeletePost(id)
-                }
-                }>
-                    Yes
-                </Button>
-                <Button onClick={handleClose}>No</Button>
-            </Box>
-        </Modal>
-    )
-}
+          Yes
+        </Button>
+        <Button onClick={handleClose}>No</Button>
+      </Box>
+    </Modal>
+  );
+};
 
-export default DeleteConfirmationWindow
+export default DeleteConfirmationWindow;

ae630021eecf357805f92b8015c161405d70b4b2
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:21:56 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import * as React from "react";
-import "../styles/LandingPage.css"
+import "../styles/LandingPage.css";
 import NavBar from "./NavBar";
 import Main from "./Main";
 import { FeatureList } from "./FeatureList";
@@ -8,13 +8,16 @@ import { About } from "./About";
 import { ExtraFeaturesList } from "./ExtraFeaturesList";
 import Footer from "./Footer";
 
-const pages = ['Home', 'Features', 'About', 'Contact'];
+const pages = ["Home", "Features", "About", "Contact"];
 const home = () => {
   return (
     <>
       <Box className="main__background-picture">
         <NavBar />
-        <Main title="EchoBoard" description="Where Your Voice Amplifies Change!" />
+        <Main
+          title="EchoBoard"
+          description="Where Your Voice Amplifies Change!"
+        />
       </Box>
       <FeatureList />
       <About />

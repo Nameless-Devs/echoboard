eb6ddea79518c9f96028b521392221a84ca24b35
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:21:25 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/service/GetStatusInfo.ts
+++ client/service/GetStatusInfo.ts
@@ -1,15 +1,27 @@
 import { StatusInfo } from "./Types";
 
 export const getStatusInfo = (status: string): StatusInfo => {
-    const statusMappings: Record<string, StatusInfo> = {
-        SOLUTION_IN_REVIEW: { formattedStatus: "Solution in review", color: "info" },
-        VOLUNTEERS_REQUIRED: { formattedStatus: "Volunteers required", color: "warning" },
-        IMPLEMENTATION_IN_PROGRESS: { formattedStatus: "Implementation in progress", color: "secondary" },
-        SOLVED: { formattedStatus: "Solved", color: "success" },
-        FAILED: { formattedStatus: "Failed", color: "error" },
-    };
+  const statusMappings: Record<string, StatusInfo> = {
+    SOLUTION_IN_REVIEW: {
+      formattedStatus: "Solution in review",
+      color: "info",
+    },
+    VOLUNTEERS_REQUIRED: {
+      formattedStatus: "Volunteers required",
+      color: "warning",
+    },
+    IMPLEMENTATION_IN_PROGRESS: {
+      formattedStatus: "Implementation in progress",
+      color: "secondary",
+    },
+    SOLVED: { formattedStatus: "Solved", color: "success" },
+    FAILED: { formattedStatus: "Failed", color: "error" },
+  };
 
-    const defaultStatusInfo: StatusInfo = { formattedStatus: "Unknown", color: "default" };
+  const defaultStatusInfo: StatusInfo = {
+    formattedStatus: "Unknown",
+    color: "default",
+  };
 
-    return statusMappings[status] || defaultStatusInfo;
+  return statusMappings[status] || defaultStatusInfo;
 };

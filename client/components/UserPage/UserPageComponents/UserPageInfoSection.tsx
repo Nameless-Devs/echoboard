e743ff8d97fea31841501cb594099166108132cd
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-06 15:23:09 +0100

    Refactor: Improve Code Quality. - Corrected spelling errors. - Removed unnecessary import statements. - Eliminated unused variables. - Deleted extraneous comments and white spaces for a cleaner layout. - Standardized quotation marks for consistency.

--- client/components/UserPage/UserPageComponents/UserPageInfoSection.tsx
+++ client/components/UserPage/UserPageComponents/UserPageInfoSection.tsx
@@ -1,56 +1,67 @@
-import { calculateNumberOfVolunteering, calulateNumberOfSolvedSolution } from '@/service/HelperFunction';
-import { UserResponseData } from '@/service/Types'
-import { Box, Avatar, Typography, Chip } from '@mui/material'
-import React from 'react'
+import {
+  calculateNumberOfVolunteering,
+  calculateNumberOfSolvedSolution,
+} from "@/service/HelperFunction";
+import { UserResponseData } from "@/service/Types";
+import { Box, Avatar, Typography, Chip } from "@mui/material";
+import React from "react";
 
 type UserPageInfoSectionProp = {
-    user: UserResponseData;
-}
+  user: UserResponseData;
+};
 
-export const UserPageInfoSection: React.FC<UserPageInfoSectionProp> = ({user}) => {
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                gap: {xs: "1rem", md: "2rem"},
-                marginTop: "2rem",
-            }}
+export const UserPageInfoSection: React.FC<UserPageInfoSectionProp> = ({
+  user,
+}) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        gap: { xs: "1rem", md: "2rem" },
+        marginTop: "2rem",
+      }}
+    >
+      <Avatar
+        alt={user.name + "avatar"}
+        src={user.picture}
+        sx={{
+          width: { xs: "7rem", md: "10rem" },
+          height: { xs: "7rem", md: "10rem" },
+          marginLeft: { xs: "1rem", md: "" },
+        }}
+      />
+      <Box sx={{ alignSelf: "center" }}>
+        <Typography
+          variant="h3"
+          color="textPrimary"
+          sx={{
+            fontSize: { xs: "2rem", md: "3rem" },
+          }}
         >
-            <Avatar
-                alt={user.name + "avatar"}
-                src={user.picture}
-                sx={{
-                    width: {xs: "7rem", md: "10rem"},
-                    height: {xs: "7rem", md: "10rem"},
-                    marginLeft: {xs: "1rem", md: ""},
-                }}
-            />
-            <Box sx={{ alignSelf: "center" }}>
-                <Typography 
-                variant="h3" 
-                color="textPrimary"
-                sx={{
-                    fontSize: {xs: "2rem", md: "3rem"}
-                }}
-                >
-                    {user.name}
-                </Typography>
-                <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: {xs: "space-around" , md: "center"},
-                        gap: { xs: "0.5rem", md: "1rem"},
-                        mr: { xs: "1rem", md: "0"}
-                    }}
-                >
-                    <Chip
-                        label={"Problems solved: " + calulateNumberOfSolvedSolution(user.echoBoardSolutions)}
-                        color="success"
-                    />
-                    <Chip label={"Volunteered: " + calculateNumberOfVolunteering(user)} color="warning" />
-                </Box>
-            </Box>
+          {user.name}
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: { xs: "space-around", md: "center" },
+            gap: { xs: "0.5rem", md: "1rem" },
+            mr: { xs: "1rem", md: "0" },
+          }}
+        >
+          <Chip
+            label={
+              "Problems solved: " +
+              calculateNumberOfSolvedSolution(user.echoBoardSolutions)
+            }
+            color="success"
+          />
+          <Chip
+            label={"Volunteered: " + calculateNumberOfVolunteering(user)}
+            color="warning"
+          />
         </Box>
-    )
-}
+      </Box>
+    </Box>
+  );
+};

c45cc23b80ffe57f83453e2eca5e9569e31010c9
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-12-12 15:34:13 +0100

    Updated developers info file to use downloaded avatars

--- client/service/DevelopersInfo.ts
+++ client/service/DevelopersInfo.ts
@@ -1,24 +1,26 @@
+import Anastasia from "../app/image/Devs avatars/Anastasia.png";
+
 export const developers = [
     { name: 'Anastasia Kurayshevich', role: 'Full-stack Developer',
-        avatar: 'https://media.licdn.com/dms/image/D4D03AQHI1MQPvVOh2g/profile-displayphoto-shrink_200_200/0/1694881637326?e=1707350400&v=beta&t=hq4KuZHf46BIZ29rg95Qq4w8R4iwrI-F1bNJaDvlf54',
+        avatar: "Anastasia.png",
         info: "Creative Java developer with a background in the restaurant industry. Enjoys problem-solving and continuous learning. Outside work, passionate about pottery, healthy cooking, and various sports.",
         linkedin: "https://www.linkedin.com/in/anastasia-kurayshevich/",
         github: "https://github.com/AnastasiaKurayshevich"
     },
     { name: 'Nate Tklay Arafayne', role: 'Full-stack Developer',
-        avatar: 'https://media.licdn.com/dms/image/D4D03AQHArFTOOuO0lg/profile-displayphoto-shrink_800_800/0/1690207852437?e=1707350400&v=beta&t=q7h_-b5RQ5Ei0LOibDqBO8QtadvhQt972Qo5cQ1ArZA',
+        avatar: "Nate.png",
         info: "Former military professional turned software engineer with a love for solving complex problems. Thrives on continuous learning and embraces challenges within the dynamic field of technology.",
         linkedin: "https://www.linkedin.com/in/nate-tklay-arafayne-20898925a/",
         github: "https://github.com/NateAra"
     },
     { name: 'Ibrahim Iqbal', role: 'Full-stack Developer',
-        avatar: 'https://media.licdn.com/dms/image/D4D03AQGLAkaNDEHyWQ/profile-displayphoto-shrink_800_800/0/1673879320515?e=1707350400&v=beta&t=nTWcCLx_9zqh-QuKFDJFyyjJUwZvMrx2Pw0hQC1gWis',
+        avatar: "Ibrahim.jpeg",
         info: "Dedicated Java developer with a passion for coding and backend development. Thrives on challenges and innovation. Seeking to bring expertise and dedication to your company.",
         linkedin: "https://www.linkedin.com/in/ibrahim-iqbal-dib59/",
         github: "https://github.com/dIB59"
     },
     { name: 'Ariel Shaka', role: 'Full-stack Developer',
-        avatar: 'https://media.licdn.com/dms/image/D4D03AQHI_nDfF57s9w/profile-displayphoto-shrink_200_200/0/1674828495314?e=1707350400&v=beta&t=MFxdEpqEoY_lns_cgw1JhsbHT1pkxc94FOe23bkmidE',
+        avatar: "Ariel.jpeg",
         info: "Full-stack Java developer with a knack for developing innovative solutions. Enjoys collaboration across teams and focuses on proficiency and agility in creating high-quality products.",
         linkedin: "https://www.linkedin.com/in/arielshaka/",
         github: "https://github.com/ArielShaka"

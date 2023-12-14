ea7ca9927c5dd1f5f7c8858a0783d8e499001d3a
NateAra <nate.arafayne@appliedtechnology.se>
2023-12-14 10:06:09 +0100

    Replaced error with a new custom error

--- client/service/Functions.tsx
+++ client/service/Functions.tsx
@@ -298,14 +298,8 @@ export async function editEchoBoard(
       body: JSON.stringify(echoBoard),
       credentials: "include",
     });
-
-    if (response.ok) {
-      return response;
-    } else {
-      throw new Error(`HTTP Error! Status: ${response.status}`);
-    }
   } catch (error) {
-    throw new Error("Fetch error: " + error);
+    throw new EditingEchoBoardError();
   }
 }
 

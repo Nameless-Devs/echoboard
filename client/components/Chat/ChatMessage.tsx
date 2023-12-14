f2499143970bdf849eb965511a21697a84d1b210
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-12-14 16:06:11 +0100

    Added conditional rendering to group messages from the same user within the time limit

--- client/components/Chat/ChatMessage.tsx
+++ client/components/Chat/ChatMessage.tsx
@@ -2,6 +2,7 @@ import { formatTimestamp } from "@/service/TimeConverterForMessages";
 import { Message } from "@/service/Types";
 import { Avatar, ListItem, ListItemText, Typography } from "@mui/material";
 import React from "react";
+import "../../app/styles/Chat.css";
 
 type ChatMessageProps = {
   index: number;
@@ -9,14 +10,21 @@ type ChatMessageProps = {
   messages: Message[];
 };
 
-export const ChatMessage: React.FC<ChatMessageProps> = ({ index, msg }) => {
+const TIME_LIMIT = 3 * 60 * 1000; // 3 minutes in milliseconds
+
+export const ChatMessage: React.FC<ChatMessageProps> = ({ index, msg, messages }) => {
+  const isWithinTimeLimit =
+    index > 0 &&
+    new Date(msg.timestamp).getTime() - new Date(messages[index - 1].timestamp).getTime() <= TIME_LIMIT;
+
   return (
-    <ListItem key={index} className="message-display">
-      <Avatar src={msg.picture} style={{ marginRight: "15px" }} />
+    <ListItem key={index} className={`message-display ${isWithinTimeLimit ? 'consecutive-message' : ''}`}>
+      {!isWithinTimeLimit && <Avatar src={msg.picture} style={{ marginRight: "15px" }} />}
+
       <ListItemText
         primary={
           <Typography variant="body2">
-            {msg.sender} {formatTimestamp(msg.timestamp)}
+            {!isWithinTimeLimit && msg.sender} {formatTimestamp(msg.timestamp)}
           </Typography>
         }
         secondary={
@@ -24,8 +32,7 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({ index, msg }) => {
             {msg.content}
           </Typography>
         }
-        
-      ></ListItemText>
+      />
     </ListItem>
   );
 };

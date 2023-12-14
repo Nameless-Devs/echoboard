6d03c2c10c85381ded66b1d4a92073047c3893f3
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-12-14 15:32:02 +0100

    Added some rendering logic into the component

--- client/components/Chat/ChatMessageByUser.tsx
+++ client/components/Chat/ChatMessageByUser.tsx
@@ -1,7 +1,29 @@
+import { formatTimestamp } from '@/service/TimeConverterForMessages';
+import { Message } from '@/service/Types';
+import { ListItem, Avatar, ListItemText, Typography } from '@mui/material';
 import React from 'react'
 
-export const ChatMessageByUser = () => {
-  return (
-    <div>ChatMessageByUser</div>
-  )
-}
+type ChatMessageProps = {
+    index: number;
+    msg: Message;
+  };
+  
+export const ChatMessageByUser: React.FC<ChatMessageProps> = ({ index, msg }) => {
+    return (
+      <ListItem key={index} >
+        <ListItemText
+          primary={
+            <Typography variant="body2">
+              {msg.sender} {formatTimestamp(msg.timestamp)}
+            </Typography>
+          }
+          secondary={
+            <Typography variant="body1">
+              {msg.content}
+            </Typography>
+          }
+          
+        ></ListItemText>
+      </ListItem>
+    );
+  };

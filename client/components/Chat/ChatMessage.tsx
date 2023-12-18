e25839deff6b8f0640ff938ee04a324a761d68d7
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-12-18 14:49:25 +0100

    Refactored the code to only have the first message condition

--- client/components/Chat/ChatMessage.tsx
+++ client/components/Chat/ChatMessage.tsx
@@ -1,6 +1,6 @@
 import { formatTimestamp } from "@/service/TimeConverterForMessages";
 import { Message } from "@/service/Types";
-import { Avatar, Grid, ListItem, Typography } from "@mui/material";
+import { Avatar, Box, Grid, ListItem, Typography } from "@mui/material";
 import React from "react";
 import "../../app/styles/Chat.css";
 
@@ -19,42 +19,54 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({ index, msg, messages }
   const isWithinTimeLimit = index > 0 && timeDifference <= TIME_LIMIT;
 
   const isFirstMessage = !isWithinTimeLimit || index === 0;
-  const isLastMessage =
-  (index === messages.length - 1 && isWithinTimeLimit) || (!isWithinTimeLimit && index > 0);
-
+  // const isLastMessage =
+  //   (index === messages.length - 1 && isWithinTimeLimit) || (!isWithinTimeLimit && index >= 0);
+  // console.log(`Index: ${index}, isFirstMessage: ${isFirstMessage}, isLastMessage: ${isLastMessage}`);
 
   return (
     <ListItem key={index} className={`message-display ${isWithinTimeLimit ? 'consecutive-message' : ''}`}>
       <Grid container>
         <Grid item xs={1}>
-        {(isLastMessage || (messages.length === 1 && isFirstMessage)) && <Avatar src={msg.picture} />}
+
+        { isFirstMessage && <Avatar src={msg.picture} />}
+          {/* {(isLastMessage || (messages.length === 1 && isFirstMessage)) && <Avatar src={msg.picture} />} */}
         </Grid>
         <Grid item xs={11}>
           {isFirstMessage && (
             <>
-              <Typography variant="body2">
-                {msg.sender}
-              </Typography>
-              <Typography variant="body1">
-                {msg.content}
-              </Typography>
+              <Box sx={{
+                backgroundColor: "#c1c4c7",
+                width: "fit-content",
+                blockSize: "fit-content",
+                display: "block",
+                padding: ".3rem 1rem",
+                borderRadius: "1.5rem"
+              }}>
+                <Typography variant="body2">
+                  {msg.sender}
+                </Typography>
+                <Typography variant="body1">
+                  {msg.content}
+                </Typography>
+                <Typography>{formatTimestamp(msg.timestamp)} i am here</Typography>
+              </Box>
             </>
           )}
 
-          {!isFirstMessage && !isLastMessage && (
+          {!isFirstMessage  && (
             <Typography variant="body1">
               {msg.content}
             </Typography>
           )}
 
-          {isLastMessage && (
+          {/* {isLastMessage && !isFirstMessage && (
             <>
-             <Typography variant="body1">
-              {msg.content}
-            </Typography>
+            <Typography variant="body1">
+                {msg.content}
+              </Typography>
               <Typography>{formatTimestamp(msg.timestamp)}</Typography>
             </>
-          )}
+          )} */}
         </Grid>
       </Grid>
     </ListItem>

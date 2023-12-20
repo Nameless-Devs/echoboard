1794b8986dd38b518b94286aa9835d54e362a289
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-12-20 11:06:49 +0100

    Added new component for left grid

---
+++ client/components/Chat/LeftGrid.tsx
@@ -0,0 +1,26 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+import ChatRoomList from './ChatRoomList'
+import { ChatRoomTextInfo } from './ChatRoomTextInfo'
+import { ChatRoomResponse } from '@/service/Types'
+
+type LeftGridProps = {
+    chatRooms: ChatRoomResponse[];
+    selectedIndex: number;
+    handleChatRoomChange: (chatRoom: ChatRoomResponse) => void; 
+    setSelectedIndex: (index: number) => void;
+}
+
+export const LeftGrid: React.FC<LeftGridProps> = ({ 
+    chatRooms, 
+    selectedIndex, 
+    handleChatRoomChange, 
+    setSelectedIndex, 
+}) => {
+    return (
+        <Grid item xs={3} sx={{ height: "92vh", overflowY: 'auto', backgroundColor: "#faf9f6", borderRight: "3px solid #c1c4c7" }}>
+            {chatRooms && <ChatRoomList chatRooms={chatRooms} selectedIndex={selectedIndex} onSelectChatRoom={handleChatRoomChange} setSelectedIndex={setSelectedIndex} />}
+            {chatRooms?.length === 0 ? <ChatRoomTextInfo /> : <></>}
+        </Grid>
+    )
+}

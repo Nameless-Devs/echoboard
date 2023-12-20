639db083241f5194c702310287893d4033b46227
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-12-20 10:36:17 +0100

    Added rendering logic

--- client/components/Chat/ChatPageTextInfo.tsx
+++ client/components/Chat/ChatPageTextInfo.tsx
@@ -1,7 +1,18 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 export const ChatPageTextInfo = () => {
-  return (
-    <div>ChatPageTextInfo</div>
-  )
+    return (
+        <Box sx={{
+            textAlign: "center",
+            padding: "2rem 1rem",
+        }}>
+            <Typography variant="h6" color="textSecondary">
+                Welcom to the Chat page where you can discuss the solution implementations with your fellow volunteers.
+            </Typography>
+            <Typography variant="h6" color="textSecondary">
+                Select the chat room and start talking!
+            </Typography>
+        </Box>
+    )
 }

b5a610cb4a15674b542dcb35cfbffd0faa7c36ff
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-21 10:02:08 +0100

    updated imports

--- src/main/java/se/salt/echoboard/websocket/WebSocketConfig.java
+++ src/main/java/se/salt/echoboard/websocket/WebSocketConfig.java
@@ -19,12 +19,14 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.util.WebUtils;
+import se.salt.echoboard.multitenancy.context.TenantContext;
 import se.salt.echoboard.security.config.WebsiteProperties;
 
 import java.util.Map;

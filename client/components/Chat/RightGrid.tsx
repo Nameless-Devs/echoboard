0b0f039a3832283e3ff69641d129137ddf20bae1
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-12-21 11:22:31 +0100

    Added arrow to go back and some styling

--- client/components/Chat/RightGrid.tsx
+++ client/components/Chat/RightGrid.tsx
@@ -1,4 +1,4 @@
-import { Grid, Box } from '@mui/material'
+import { Grid, Box, Button } from '@mui/material'
 import React from 'react'
 import { ChatMessage } from './ChatMessage'
 import { ChatMessageByUser } from './ChatMessageByUser'
@@ -7,6 +7,7 @@ import { SendMessageInputField } from './SendMessageInputField'
 import { Message, SolutionResponseData, UserResponseData } from '@/service/Types'
 import { useScrollToLatestMessage } from '@/hooks/useScrollToLatestMessage'
 import { ChatPageTextInfo } from './ChatPageTextInfo'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 type RightGridProps = {
     selectedChatRoomId: number | undefined;
@@ -14,7 +15,7 @@ type RightGridProps = {
     messages: Message[];
     input: string;
     handleKeyPress: (event: React.KeyboardEvent<HTMLDivElement>) => void;
-    handleMessageInput: (event: React.ChangeEvent<HTMLInputElement>) => void; 
+    handleMessageInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
     handleSendMessage: () => void;
     user: UserResponseData;
 }
@@ -32,94 +33,105 @@ export const RightGrid: React.FC<RightGridProps> = ({
     const scrollToLatestMessage = useScrollToLatestMessage(messages);
     return (
         <>
-        <Grid item xs={0} md={9} sx={{ 
-            height: "100%", 
-            backgroundColor: "#FAF9F7",
-            display: {xs: "none", md: "block"},
-             }}>
-            {/*Top Right*/}
-            <Grid item xs={12} sx={{ height: "85%" }}>
-                {!selectedChatRoomId && <ChatPageTextInfo />}
-                <Grid item xs={12}>
-                    {solution && <ChatSolutionInfo solution={solution} />}
+            <Grid item xs={0} md={9} sx={{
+                height: "100%",
+                backgroundColor: "#FAF9F7",
+                display: { xs: "none", md: "block" },
+            }}>
+                {/*Top Right*/}
+                <Grid item xs={12} sx={{ height: "85%" }}>
+                    {!selectedChatRoomId && <ChatPageTextInfo />}
+                    <Grid item xs={12}>
+                        {solution && <ChatSolutionInfo solution={solution} />}
+                    </Grid>
+                    <Grid item xs={12} sx={{ maxHeight: "62vh", overflow: "auto" }}>
+                        <Box>
+                            {messages.map((msg, index) => (
+                                msg.subject === user.subject ? (
+                                    <ChatMessageByUser key={index} index={index} msg={msg} />
+                                ) : (
+                                    <ChatMessage key={index} index={index} msg={msg} messages={messages} />
+                                )
+                            ))}
+                            <div ref={scrollToLatestMessage} />
+                        </Box>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sx={{ maxHeight: "62vh", overflow: "auto" }}>
-                    <Box>
-                        {messages.map((msg, index) => (
-                            msg.subject === user.subject ? (
-                                <ChatMessageByUser key={index} index={index} msg={msg} />
-                            ) : (
-                                <ChatMessage key={index} index={index} msg={msg} messages={messages} />
-                            )
-                        ))}
-                        <div ref={scrollToLatestMessage} />
-                    </Box>
-                </Grid>
-            </Grid>
-            {/*Bottom Right*/}
-            <Grid
-                item
-                xs={12}
-                sx={{
-                    height: "15%",
-                    outline: "10px blue",
-                    backgroundColor: "rgb(250, 249, 246)",
-                    padding: "1rem",
-                }}
-            >
-                {selectedChatRoomId &&
-                    <SendMessageInputField
-                        input={input}
-                        handleKeyPress={handleKeyPress}
-                        handleMessageInput={handleMessageInput}
-                        handleSendMessage={handleSendMessage}
-                    />}
-            </Grid>
-        </Grid>
-        <Grid item xs={12} md={0} sx={{ 
-            height: "100%", 
-            backgroundColor: "#FAF9F7",
-            display: {xs: selectedChatRoomId !== undefined ? "block" : "none", md: "none" },
-             }}>
-            {/*Top Right*/}
-            <Grid item xs={12} sx={{ height: "85%" }}>
-                {!selectedChatRoomId && <ChatPageTextInfo />}
-                <Grid item xs={12}>
-                    {solution && <ChatSolutionInfo solution={solution} />}
-                </Grid>
-                <Grid item xs={12} sx={{ maxHeight: "62vh", overflow: "auto" }}>
-                    <Box>
-                        {messages.map((msg, index) => (
-                            msg.subject === user.subject ? (
-                                <ChatMessageByUser key={index} index={index} msg={msg} />
-                            ) : (
-                                <ChatMessage key={index} index={index} msg={msg} messages={messages} />
-                            )
-                        ))}
-                        <div ref={scrollToLatestMessage} />
-                    </Box>
+                {/*Bottom Right*/}
+                <Grid
+                    item
+                    xs={12}
+                    sx={{
+                        height: "15%",
+                        outline: "10px blue",
+                        backgroundColor: "rgb(250, 249, 246)",
+                        padding: "1rem",
+                    }}
+                >
+                    {selectedChatRoomId &&
+                        <SendMessageInputField
+                            input={input}
+                            handleKeyPress={handleKeyPress}
+                            handleMessageInput={handleMessageInput}
+                            handleSendMessage={handleSendMessage}
+                        />}
                 </Grid>
             </Grid>
-            {/*Bottom Right*/}
-            <Grid
-                item
-                xs={12}
-                sx={{
-                    height: "15%",
-                    outline: "10px blue",
-                    backgroundColor: "rgb(250, 249, 246)",
-                    padding: "1rem",
-                }}
-            >
-                {selectedChatRoomId &&
-                    <SendMessageInputField
-                        input={input}
-                        handleKeyPress={handleKeyPress}
-                        handleMessageInput={handleMessageInput}
-                        handleSendMessage={handleSendMessage}
-                    />}
+
+            <Grid item xs={12} md={0} sx={{
+                height: "100%",
+                backgroundColor: "#FAF9F7",
+                display: { xs: selectedChatRoomId !== undefined ? "block" : "none", md: "none" },
+            }}>
+                {/*Top Right*/}
+                <Grid item xs={12} sx={{ height: "85%" }}>
+                    {!selectedChatRoomId && <ChatPageTextInfo />}
+                    <Grid item xs={12}>
+                        <Box sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            backgroundColor: "white",
+                            borderBottom: "3px solid #c1c4c7",
+                        }}>
+                            <Button sx={{ padding: "2.5rem 0px", }}>
+                                <ArrowBackIosIcon />
+                            </Button>
+                            {solution && <ChatSolutionInfo solution={solution} />}
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sx={{ maxHeight: "62vh", overflow: "auto" }}>
+                        <Box>
+                            {messages.map((msg, index) => (
+                                msg.subject === user.subject ? (
+                                    <ChatMessageByUser key={index} index={index} msg={msg} />
+                                ) : (
+                                    <ChatMessage key={index} index={index} msg={msg} messages={messages} />
+                                )
+                            ))}
+                            <div ref={scrollToLatestMessage} />
+                        </Box>
+                    </Grid>
+                </Grid>
+                {/*Bottom Right*/}
+                <Grid
+                    item
+                    xs={12}
+                    sx={{
+                        height: "15%",
+                        outline: "10px blue",
+                        backgroundColor: "rgb(250, 249, 246)",
+                        padding: "1rem",
+                    }}
+                >
+                    {selectedChatRoomId &&
+                        <SendMessageInputField
+                            input={input}
+                            handleKeyPress={handleKeyPress}
+                            handleMessageInput={handleMessageInput}
+                            handleSendMessage={handleSendMessage}
+                        />}
+                </Grid>
             </Grid>
-        </Grid>
         </>
     )
 }
